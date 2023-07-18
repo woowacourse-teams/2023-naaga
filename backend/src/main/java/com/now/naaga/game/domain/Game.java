7a454f5392285b9e8082663c91ea93459d9e1420
kokodak <kokodakadokok@gmail.com>
2023-07-18 15:49:20 +0900

    feat: Game 엔티티 생성
    Co-authored-by: dooboocookie <bkh3030@gmail.com>
    Co-authored-by: chaewon121 <cw121@naver.com>
    Co-authored-by: zillionme <beatus96@naver.com>

---
+++ backend/src/main/java/com/now/naaga/game/domain/Game.java
@@ -0,0 +1,60 @@
+package com.now.naaga.game.domain;
+
+import com.now.naaga.member.domain.Member;
+import com.now.naaga.place.domain.Place;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.Objects;
+
+@Entity
+public class Game {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private GameStatus gameStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    @Override
+    public boolean equals(final Object o) {
+
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Game game = (Game) o;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "id=" + id +
+                ", gameStatus=" + gameStatus +
+                ", memberId=" + member.getId() +
+                ", placeId=" + place.getId() +
+                '}';
+    }
+}
---
+++ backend/src/main/java/com/now/naaga/game/domain/GameStatus.java
@@ -0,0 +1,7 @@
+package com.now.naaga.game.domain;
+
+public enum GameStatus {
+
+    IN_PROGRESSING,
+    DONE,
+}
