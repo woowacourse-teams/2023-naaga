a565f7ee57ec189247ea75c25509c3a1f9e7b28b
kokodak <kokodakadokok@gmail.com>
2023-07-18 14:21:32 +0900

    feat: 프로젝트 초기 세팅

---
+++ backend/src/main/java/com/now/naaga/NaagaApplication.java
@@ -0,0 +1,13 @@
+package com.now.naaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaagaApplication.class, args);
+	}
+
+}
