fc7b7b423273a56247fd1c85ac6c3cc2120e1d7d
kokodak <kokodakadokok@gmail.com>
2023-07-18 15:48:06 +0900

    feat: Member 엔티티 생성
    Co-authored-by: dooboocookie <bkh3030@gmail.com>
    Co-authored-by: chaewon121 <cw121@naver.com>
    Co-authored-by: zillionme <beatus96@naver.com>

---
+++ backend/src/main/java/com/now/naaga/member/domain/Member.java
@@ -0,0 +1,60 @@
+package com.now.naaga.member.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Member {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    protected Member() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Member member = (Member) o;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
