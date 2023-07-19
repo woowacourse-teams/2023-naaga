34d83a6db46556195ed39fb590e8d27b271fbc07
briandr97 <briandr@naver.com>
2023-07-20 02:30:40 +0900

    feat: 액티비티에서 사진 아이콘 버튼 클릭시 다이얼로그를 띄우는 로직 구현

--- android/app/src/main/java/com/now/naaga/presentation/onadventure/OnAdventureActivity.kt
+++ android/app/src/main/java/com/now/naaga/presentation/onadventure/OnAdventureActivity.kt
@@ -2,6 +2,8 @@ package com.now.naaga.presentation.onadventure
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.LocationTrackingMode
@@ -14,6 +16,7 @@ import com.now.domain.model.Coordinate
 import com.now.naaga.R
 import com.now.naaga.data.repository.MockDestinationRepository
 import com.now.naaga.databinding.ActivityOnAdventureBinding
+import com.now.naaga.presentation.beginadventure.DestinationPhotoDialog
 
 class OnAdventureActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityOnAdventureBinding
@@ -25,13 +28,12 @@ class OnAdventureActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityOnAdventureBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initViewModel()
         setMapView()
-
+        clickPhotoIcon()
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
 
         binding.viewModel = viewModel
@@ -79,10 +81,23 @@ class OnAdventureActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
+    private fun clickPhotoIcon() {
+        binding.ivOnAdventurePhoto.setOnClickListener {
+            val fragment: Fragment? = supportFragmentManager.findFragmentByTag(DESTINATION_PHOTO)
+            if (fragment == null) {
+                DestinationPhotoDialog.makeDialog("")
+                    .show(supportFragmentManager, DESTINATION_PHOTO)
+            } else {
+                (fragment as DialogFragment).dialog?.show()
+            }
+        }
+    }
+
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
         private const val DESTINATION_LATITUDE = 37.514907
         private const val DESTINATION_LONGITUDE = 127.103198
         private val DESTINATION_COORDINATE = Coordinate(DESTINATION_LATITUDE, DESTINATION_LONGITUDE)
+        private const val DESTINATION_PHOTO = "DESTINATION_PHOTO"
     }
 }
