f35e77670cc602b067f52a47a8f954cea16f17b3
krrong <khk9664@naver.com>
2023-07-19 17:07:04 +0900

    feat: 위치 권한 요청 기능 구현 (#27)
    * chore: repository 패키지 위치 이동
    
    * feat: 모험시작 액티비티 생성
    
    - 필요한 위치권한 Manifest에 등록
    - 위치권한 요청
    
    * design: UI관련 resource 추가
    
    * feat: 게임 시작 기능 액티비티 추가 및 권한 요청 기능 구현
    
    * design: 문자열 strings 리소스화
    
    * design: 배경 및 버튼 색상 변경

---
+++ android/app/src/main/java/com/now/naaga/presentation/beginadventure/BeginAdventureActivity.kt
@@ -0,0 +1,31 @@
+package com.now.naaga.presentation.beginadventure
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.now.naaga.databinding.ActivityBeginAdventureBinding
+
+class BeginAdventureActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBeginAdventureBinding
+
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityBeginAdventureBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        locationPermissionRequest.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+
+        binding.btnBeginAdventureButton.setOnClickListener {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+                LocationDialogFragment().show(supportFragmentManager, "location")
+            }
+        }
+    }
+}
---
+++ android/app/src/main/java/com/now/naaga/presentation/beginadventure/LocationDialogFragment.kt
@@ -0,0 +1,69 @@
+package com.now.naaga.presentation.beginadventure
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+import com.now.naaga.databinding.DialogLocationPermissionBinding
+import kotlin.math.roundToInt
+
+class LocationDialogFragment : DialogFragment() {
+    private lateinit var binding: DialogLocationPermissionBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = DialogLocationPermissionBinding.inflate(layoutInflater)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setFragmentWidth()
+        binding.btnDialogLocationSetting.setOnClickListener { goSetting() }
+    }
+
+    private fun setFragmentWidth() {
+        dialog?.window?.setLayout(getWidth(), convertDpToPx(400))
+    }
+
+    private fun getWidth(): Int {
+        val windowManager: WindowManager =
+            requireContext().getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            (windowManager.currentWindowMetrics.bounds.width() * WIDTH_PERCENTAGE).toInt()
+        } else {
+            (windowManager.defaultDisplay.width * WIDTH_PERCENTAGE).toInt()
+        }
+    }
+
+    private fun convertDpToPx(dp: Int): Int {
+        val density = requireContext().resources.displayMetrics.density
+        return (dp.toFloat() * density).roundToInt()
+    }
+
+    private fun goSetting() {
+        val appDetailsIntent = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.parse("package:${requireContext().packageName}"),
+        ).addCategory(Intent.CATEGORY_DEFAULT)
+        startActivity(appDetailsIntent)
+    }
+
+    companion object {
+        const val WIDTH_PERCENTAGE = 0.83
+    }
+}
