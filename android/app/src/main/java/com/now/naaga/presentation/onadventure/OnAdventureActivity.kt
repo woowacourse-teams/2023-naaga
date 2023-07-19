05d5d03b125b4d04e33af7a4f75bb361a340b9a2
krrong <khk9664@naver.com>
2023-07-20 01:14:31 +0900

    feat: 목적지와 현재위치 사이 거리를 계산하여 사용자에게 보여주는 기능 구현

--- android/app/src/main/java/com/now/naaga/presentation/onadventure/OnAdventureActivity.kt
+++ android/app/src/main/java/com/now/naaga/presentation/onadventure/OnAdventureActivity.kt
@@ -2,30 +2,50 @@ package com.now.naaga.presentation.onadventure
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.util.FusedLocationSource
+import com.now.domain.model.Coordinate
 import com.now.naaga.R
+import com.now.naaga.data.repository.MockDestinationRepository
+import com.now.naaga.databinding.ActivityOnAdventureBinding
 
 class OnAdventureActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var binding: ActivityOnAdventureBinding
     private lateinit var mapView: MapFragment
     private lateinit var naverMap: NaverMap
 
     private lateinit var locationSource: FusedLocationSource
 
+    private lateinit var viewModel: OnAdventureViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_adventure)
 
+        binding = ActivityOnAdventureBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViewModel()
         setMapView()
+
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+    }
+
+    private fun initViewModel() {
+        val repository = MockDestinationRepository()
+        val factory = OnAdventureFactory(repository)
+        viewModel = ViewModelProvider(this, factory)[OnAdventureViewModel::class.java]
     }
 
     private fun setMapView() {
         val fragmentManager = supportFragmentManager
-        val mapFragment = fragmentManager.findFragmentById(R.id.fcv_main_map) as MapFragment
+        val mapFragment = fragmentManager.findFragmentById(R.id.fcv_onAdventure_map) as MapFragment
         mapFragment.getMapAsync(this)
         mapView = mapFragment
     }
@@ -34,16 +54,20 @@ class OnAdventureActivity : AppCompatActivity(), OnMapReadyCallback {
         naverMap = map
         enableLocationButton()
         setFollowMode()
-    }
 
-    private fun setFollowMode() {
-        naverMap.locationSource = locationSource
-        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        naverMap.addOnLocationChangeListener { location ->
+            viewModel.calculateDistance(Coordinate(location.latitude, location.longitude))
+        }
     }
 
     private fun enableLocationButton() {
         val uiSetting = naverMap.uiSettings
         uiSetting.isLocationButtonEnabled = true
+    }
+
+    private fun setFollowMode() {
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
     }
 
     companion object {
--- android/app/src/main/java/com/now/naaga/presentation/onadventure/OnAdventureViewModel.kt
+++ android/app/src/main/java/com/now/naaga/presentation/onadventure/OnAdventureViewModel.kt
@@ -3,6 +3,7 @@ package com.now.naaga.presentation.onadventure
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.now.domain.model.Coordinate
 import com.now.domain.model.Destination
 import com.now.domain.repository.DestinationRepository
 
@@ -14,7 +15,18 @@ class OnAdventureViewModel(
     val destination: LiveData<Destination>
         get() = _destination
 
-    fun getDestination() {
+    private val _distance = MutableLiveData(0)
+    val distance: LiveData<Int> get() = _distance
+
+    init {
+        getDestination()
+    }
+
+    private fun getDestination() {
         _destination.value = destinationRepository.getDestination(1)
+    }
+
+    fun calculateDistance(coordinate: Coordinate) {
+        _distance.value = destination.value?.getDistance(coordinate)
     }
 }
