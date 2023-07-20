5614d562f4aa58ebb93df4b6ff4ed9e0aaa463c0
kokodak <kokodakadokok@gmail.com>
2023-07-20 10:09:39 +0900

    feat: 수정된 API 명세에 맞게 컨트롤러 수정

--- backend/src/main/java/com/now/naaga/game/presentation/GameController.java
+++ backend/src/main/java/com/now/naaga/game/presentation/GameController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/games")
 @RestController
 public class GameController {
 
