7da63dc7af36da40961182890547d01621819840
kokodak <kokodakadokok@gmail.com>
2023-07-20 10:09:45 +0900

    feat: 수정된 API 명세에 맞게 컨트롤러 수정

--- backend/src/main/java/com/now/naaga/game/presentation/GameController.java
+++ backend/src/main/java/com/now/naaga/game/presentation/GameController.java
@@ -2,12 +2,22 @@ package com.now.naaga.game.presentation;
 
 import com.now.naaga.auth.annotation.Auth;
 import com.now.naaga.game.application.GameService;
-import com.now.naaga.game.presentation.dto.PlaceResponse;
+import com.now.naaga.game.domain.Game;
+import com.now.naaga.game.presentation.dto.GameResponse;
+import com.now.naaga.game.presentation.dto.GameStatusResponse;
 import com.now.naaga.member.dto.MemberCommand;
-import com.now.naaga.place.domain.Place;
 import com.now.naaga.place.domain.Position;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/games")
@@ -20,9 +30,29 @@ public class GameController {
         this.gameService = gameService;
     }
 
-    @GetMapping("/destinations")
-    public PlaceResponse findDestination(@Auth MemberCommand memberCommand, @ModelAttribute final Position position) {
-        final Place place = gameService.recommendPlaceByPosition(memberCommand, position);
-        return PlaceResponse.from(place);
+    @PostMapping
+    public ResponseEntity<Void> createGame(@Auth final MemberCommand memberCommand, @RequestBody final Position position) {
+        final Game game = gameService.createGame(memberCommand, position);
+        return ResponseEntity.created(URI.create("/games/" + game.getId())).build();
+    }
+
+    @PatchMapping("/{gameId}")
+    public ResponseEntity<GameStatusResponse> updateGameStatus(@Auth final MemberCommand memberCommand, @PathVariable final Long gameId, @RequestBody final Position position) {
+        return null;
+    }
+
+    @GetMapping("/{gameId}")
+    public ResponseEntity<GameResponse> findGame(@Auth final MemberCommand memberCommand, @PathVariable final Long gameId) {
+        final Game game = gameService.findGame(memberCommand, gameId);
+        return ResponseEntity.ok(GameResponse.from(game));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GameResponse>> findGamesByGameStatus(@Auth final MemberCommand memberCommand, @RequestParam final String gameStatus) {
+        final List<Game> games = gameService.findGamesByStatus(memberCommand, gameStatus);
+        final List<GameResponse> gameResponses = games.stream()
+                .map(GameResponse::from)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(gameResponses);
     }
 }
