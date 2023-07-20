968a93159261118a6df218167ada083edd560030
krrong <khk9664@naver.com>
2023-07-20 12:50:13 +0900

    feat: Dto와 domain 클래스간의 mapper 구현

--- android/domain/src/main/java/com/now/domain/model/AdventureStatus.kt
+++ android/domain/src/main/java/com/now/domain/model/AdventureStatus.kt
@@ -3,4 +3,15 @@ package com.now.domain.model
 enum class AdventureStatus {
     DONE,
     IN_PROGRESS,
+    ;
+
+    companion object {
+        fun getStatus(status: String): AdventureStatus {
+            return when (status) {
+                DONE.name -> DONE
+                IN_PROGRESS.name -> IN_PROGRESS
+                else -> throw IllegalArgumentException()
+            }
+        }
+    }
 }
