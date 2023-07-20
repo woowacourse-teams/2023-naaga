bd7e04150a74a685546caaca434c9903c3201961
kokodak <kokodakadokok@gmail.com>
2023-07-20 10:08:46 +0900

    feat: 게임에 대한 응답 DTO 생성

---
+++ backend/src/main/java/com/now/naaga/game/presentation/dto/GameResponse.java
@@ -0,0 +1,32 @@
+package com.now.naaga.game.presentation.dto;
+
+import com.now.naaga.game.domain.Game;
+
+public class GameResponse {
+
+    private final Long id;
+    private final PlaceResponse place;
+    private final String gameStatus;
+
+    private GameResponse(final Long id, final PlaceResponse place, final String gameStatus) {
+        this.id = id;
+        this.place = place;
+        this.gameStatus = gameStatus;
+    }
+
+    public static GameResponse from(final Game game) {
+        return new GameResponse(game.getId(), PlaceResponse.from(game.getPlace()), game.getGameStatus().toString());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public PlaceResponse getPlace() {
+        return place;
+    }
+
+    public String getGameStatus() {
+        return gameStatus;
+    }
+}
