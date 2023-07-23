1bd207f0aacb7bb2ba3cab69e91d0279013515c8
briandr97 <briandr@naver.com>
2023-07-24 02:44:43 +0900

    feat: OnAdventureActivity가 열리는 두 가지 경우에 따라 모험을 지정
    - 이미 모험이 진행 중이면 이전 화면으로부터 모험을 전달 받고 모험을 서버에 요청하지 않는다.
    - 진행 중인 모험이 없다면 서버로부터 모험을 요청한다.

--- android/app/src/main/java/com/now/naaga/presentation/onadventure/OnAdventureViewModel.kt
+++ android/app/src/main/java/com/now/naaga/presentation/onadventure/OnAdventureViewModel.kt
@@ -3,6 +3,7 @@ package com.now.naaga.presentation.onadventure
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.now.domain.model.Adventure
 import com.now.domain.model.AdventureStatus
 import com.now.domain.model.Coordinate
 import com.now.domain.model.Destination
@@ -50,6 +51,11 @@ class OnAdventureViewModel(
 
     fun checkArrived(coordinate: Coordinate) {
         _isArrived.value = destination.value?.isArrived(coordinate)
+    }
+
+    fun setAdventure(adventure: Adventure) {
+        _adventureId.value = adventure.id
+        _destination.value = adventure.destination
     }
 
     fun beginAdventure(coordinate: Coordinate) {
