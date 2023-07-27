b814964bb0bcfd128cb2b61cba2cba7e0feefef5
krrong <khk9664@naver.com>
2023-07-27 21:50:39 +0900

    feat: UploadActivity 구현

---
+++ android/app/src/main/java/com/now/naaga/presentation/upload/UploadActivity.kt
@@ -0,0 +1,12 @@
+package com.now.naaga.presentation.upload
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.now.naaga.R
+
+class UploadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload)
+    }
+}
