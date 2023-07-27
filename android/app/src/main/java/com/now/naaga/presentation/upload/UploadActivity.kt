ca81d8e724aecd11db7f65c7918d258a09e551c1
krrong <khk9664@naver.com>
2023-07-27 21:56:46 +0900

    feat: UploadActivity의 인텐트를 가져오는 함수 구현

--- android/app/src/main/java/com/now/naaga/presentation/upload/UploadActivity.kt
+++ android/app/src/main/java/com/now/naaga/presentation/upload/UploadActivity.kt
@@ -1,5 +1,7 @@
 package com.now.naaga.presentation.upload
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.now.naaga.R
@@ -8,5 +10,11 @@ class UploadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
+    }
+
+    companion object {
+        fun getIntent(context: Context): Intent {
+            return Intent(context, UploadActivity::class.java)
+        }
     }
 }
