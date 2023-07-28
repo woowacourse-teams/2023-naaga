778be98cd6b8276bde0d373d3d54d841165f6e98
kokodak <kokodakadokok@gmail.com>
2023-07-28 21:30:20 +0900

    feat: GameResult 도메인 생성
    Co-authored-by: dooboocookie <bkh3030@gmail.com>

---
+++ backend/src/main/java/com/now/naaga/game/repository/GameResultRepository.java
@@ -0,0 +1,8 @@
+package com.now.naaga.game.repository;
+
+import com.now.naaga.game.domain.GameResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameResultRepository extends JpaRepository<GameResult, Long> {
+
+}
