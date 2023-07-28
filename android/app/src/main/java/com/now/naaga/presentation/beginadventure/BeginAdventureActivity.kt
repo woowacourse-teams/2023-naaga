68bd1f98f83f39b8b281297af22a28415a518f50
k_dragonm <78788847+briandr97@users.noreply.github.com>
2023-07-28 17:34:01 +0900

    [AN] feat: registerForActivityResult 분기처리 기능 구현 (#72)
    * design: 대락적인 위치 권한만 허용됐을 시 보여줄 문구 string 리소스로 추가
    
    * feat: Dialog 내에서 권한을 체크하여 대략적인 위치 권한이 있는 상태와 전혀 없는 상태를 구분하여 다른 문구를 보여주도록 구현
    
    * feat: BeginAdventureActivity 권한 코드 수정
    
    - 유저가 권한 요청을 허용 및 거절 했을 시 토스트 메시지로 즉각적인 피드백을 주도록 구현
    - 함수 분리
    - 다이얼로그 태그 상수화

--- android/app/src/main/java/com/now/naaga/presentation/beginadventure/BeginAdventureActivity.kt
+++ android/app/src/main/java/com/now/naaga/presentation/beginadventure/BeginAdventureActivity.kt
@@ -1,8 +1,10 @@
 package com.now.naaga.presentation.beginadventure
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.now.naaga.databinding.ActivityBeginAdventureBinding
@@ -11,18 +13,45 @@ import com.now.naaga.presentation.onadventure.OnAdventureActivity
 class BeginAdventureActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBeginAdventureBinding
 
-    private val locationPermissionRequest = registerForActivityResult(
-        ActivityResultContracts.RequestPermission(),
-    ) { }
+    private val locationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // precise location access granted
+                    Toast.makeText(this, "위치 권한 요청이 허용되었습니다", Toast.LENGTH_SHORT).show()
+                }
+
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // only approximate location access granted.
+                    Toast.makeText(this, "대략적인 위치 권한만 허용되었습니다", Toast.LENGTH_SHORT).show()
+                }
+
+                else -> {
+                    // all kind of access denied
+                    Toast.makeText(this, "위치 권한 요청이 거절되었습니다", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityBeginAdventureBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        locationPermissionRequest.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        requestLocationPermission()
+        setClickListeners()
+    }
 
+    private fun requestLocationPermission() {
+        locationPermissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            ),
+        )
+    }
+
+    private fun setClickListeners() {
         binding.btnBeginAdventureButton.setOnClickListener {
             if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
                 LocationPermissionDialog().show(supportFragmentManager, "location")
@@ -30,5 +59,18 @@ class BeginAdventureActivity : AppCompatActivity() {
                 startActivity(OnAdventureActivity.getIntent(this))
             }
         }
+        checkPermissionAndBeginAdventure()
+    }
+
+    private fun checkPermissionAndBeginAdventure() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+            LocationPermissionDialog().show(supportFragmentManager, LOCATION_DIALOG_TAG)
+        } else {
+            startActivity(Intent(this, OnAdventureActivity::class.java))
+        }
+    }
+
+    companion object {
+        private const val LOCATION_DIALOG_TAG = "LOCATION"
     }
 }
