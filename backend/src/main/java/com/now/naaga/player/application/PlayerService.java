e80714aa27c5f9eb877f4223596a7c23fd30f26a
dooboocookie <bkh3030@gmail.com>
2023-07-30 11:04:35 +0900

    feat: 장소 저장하는 기능 구현

--- backend/src/main/java/com/now/naaga/player/application/PlayerService.java
+++ backend/src/main/java/com/now/naaga/player/application/PlayerService.java
@@ -2,7 +2,9 @@ package com.now.naaga.player.application;
 
 import com.now.naaga.member.application.dto.MemberCommand;
 import com.now.naaga.player.domain.Player;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PlayerService {
 
     public Player findPlayerByMemberCommand(final MemberCommand memberCommand) {
