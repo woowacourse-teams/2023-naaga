dc3943bb044c44694b2ee0571a92d8ee8074af28
chaewon121 <cw121@naver.com>
2023-07-30 19:25:01 +0900

    feat: 맴버 아이디로 플레이어 찾는 기능 구현

--- backend/src/main/java/com/now/naaga/player/persistence/repository/PlayerRepository.java
+++ backend/src/main/java/com/now/naaga/player/persistence/repository/PlayerRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
+    Player findByMemberId(Long memberId);
 }
