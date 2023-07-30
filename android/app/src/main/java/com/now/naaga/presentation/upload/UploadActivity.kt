3cf9e64f4bb4b67f9c0d7824c423616c83e8af5d
krrong <khk9664@naver.com>
2023-07-31 00:20:39 +0900

    feat: 카메라 권한 요청 기능 구현 (#92)
    * design: 카메라 권한 관련 아이콘 추가
    
    * feat: 카메라 권한 추가
    
    * feat: dataBinding 사용하도록 xml 변경
    
    * feat: 카메라 위치권한을 요구하는 다이얼로그 생성
    
    * feat: 카메라 권한이 없는 경우 권한 요청 다이얼로그를 띄우는 기능 구현
    
    * chore: 레이아웃 id 컨벤션 유지
    
    * refactor: 다이얼로그의 tag를 다이얼로그 클래스에 위치하도록 통일
    
    * refactor: 매직넘버 상수화
    
    * rename: drawable 파일명 변경

--- android/app/src/main/java/com/now/naaga/presentation/upload/UploadActivity.kt
+++ android/app/src/main/java/com/now/naaga/presentation/upload/UploadActivity.kt
@@ -1,18 +1,41 @@
 package com.now.naaga.presentation.upload
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.now.naaga.R
+import com.now.naaga.databinding.ActivityUploadBinding
+import com.now.naaga.presentation.upload.CameraPermissionDialog.Companion.TAG_CAMERA_DIALOG
 
 class UploadActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUploadBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload)
+
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        checkPermission()
+
+        binding.btnUploadSubmit.setOnClickListener {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                CameraPermissionDialog().show(supportFragmentManager, TAG_CAMERA_DIALOG)
+            }
+        }
+    }
+
+    private fun checkPermission() {
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), REQUEST_CODE_CAMERA)
+        }
     }
 
     companion object {
+        private const val REQUEST_CODE_CAMERA = 1000
+
         fun getIntent(context: Context): Intent {
             return Intent(context, UploadActivity::class.java)
         }
