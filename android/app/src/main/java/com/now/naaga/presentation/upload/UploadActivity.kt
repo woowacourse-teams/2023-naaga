8b940c3096bbb5edd207510c9068c8cb7c172f43
krrong <khk9664@naver.com>
2023-07-30 19:04:11 +0900

    feat: 카메라 권한이 없는 경우 권한 요청 다이얼로그를 띄우는 기능 구현

--- android/app/src/main/java/com/now/naaga/presentation/upload/UploadActivity.kt
+++ android/app/src/main/java/com/now/naaga/presentation/upload/UploadActivity.kt
@@ -1,15 +1,35 @@
 package com.now.naaga.presentation.upload
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.now.naaga.R
+import com.now.naaga.databinding.ActivityUploadBinding
 
 class UploadActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUploadBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload)
+
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        checkPermission()
+
+        binding.btnSubmit.setOnClickListener {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                CameraPermissionDialog().show(supportFragmentManager, "1000")
+            }
+        }
+    }
+
+    private fun checkPermission() {
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), 1000)
+        }
     }
 
     companion object {
