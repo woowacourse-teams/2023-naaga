7c683fb6e715522a13876d79955f80dd9d4a9e31
hyunji1203 <hj27720@gmail.com>
2023-07-31 17:31:06 +0900

    refactor: Mock 데이터 생성 방법 수정

--- android/app/src/main/java/com/now/naaga/presentation/adventurehistory/AdventureHistoryViewModel.kt
+++ android/app/src/main/java/com/now/naaga/presentation/adventurehistory/AdventureHistoryViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.now.domain.model.AdventureResult
-import com.now.domain.model.AdventureResultType.FAIL
 import com.now.domain.model.AdventureResultType.SUCCESS
 import com.now.domain.model.Coordinate
 import com.now.domain.model.Place
@@ -20,7 +19,7 @@ class AdventureHistoryViewModel : ViewModel() {
     }
 
     companion object {
-        val MOCK_ADVENTURE_HISTORY_DATA = listOf(
+        val MOCK_ADVENTURE_HISTORY_DATA = List(4) {
             AdventureResult(
                 id = 1,
                 gameId = 1,
@@ -40,47 +39,7 @@ class AdventureHistoryViewModel : ViewModel() {
                 raisedRank = 2,
                 beginTime = LocalDateTime.of(2023, 8, 1, 13, 30, 0),
                 endTime = LocalDateTime.of(2023, 8, 1, 14, 0, 0),
-            ),
-            AdventureResult(
-                id = 1,
-                gameId = 1,
-                destination = Place(
-                    id = 1,
-                    name = "선정릉 돌담길",
-                    coordinate = Coordinate(35.3094, 127.21389),
-                    image = "",
-                    description = "test",
-                ),
-                resultType = SUCCESS,
-                score = 130,
-                playTime = LocalTime.of(13, 30, 0),
-                distance = 300,
-                hintUses = 3,
-                tryCount = 2,
-                raisedRank = 2,
-                beginTime = LocalDateTime.of(2023, 8, 1, 13, 30, 0),
-                endTime = LocalDateTime.of(2023, 8, 1, 14, 0, 0),
-            ),
-            AdventureResult(
-                id = 1,
-                gameId = 1,
-                destination = Place(
-                    id = 1,
-                    name = "선정릉 돌담길",
-                    coordinate = Coordinate(35.3094, 127.21389),
-                    image = "",
-                    description = "test",
-                ),
-                resultType = FAIL,
-                score = 130,
-                playTime = LocalTime.of(13, 30, 0),
-                distance = 300,
-                hintUses = 3,
-                tryCount = 2,
-                raisedRank = 2,
-                beginTime = LocalDateTime.of(2023, 8, 1, 13, 30, 0),
-                endTime = LocalDateTime.of(2023, 8, 1, 14, 0, 0),
-            ),
-        )
+            )
+        }
     }
 }
