d9565007a9c70b569d6e10acf0aeca5189dd4f2e
briandr97 <briandr@naver.com>
2023-07-31 14:15:47 +0900

    feat: 도메인 수정

--- android/domain/src/main/java/com/now/domain/model/Rank.kt
+++ android/domain/src/main/java/com/now/domain/model/Rank.kt
@@ -3,5 +3,5 @@ package com.now.domain.model
 data class Rank(
     val player: Player,
     val rank: Int,
-    val percent: Int,
+    val percent: Double,
 )
--- android/domain/src/main/java/com/now/domain/model/RemainingTryCount.kt
+++ android/domain/src/main/java/com/now/domain/model/RemainingTryCount.kt
@@ -1,16 +1,16 @@
 package com.now.domain.model
 
 @JvmInline
-value class RestTryCount(private val value: Int) {
+value class RemainingTryCount(private val value: Int) {
     init {
         require(value >= 0) { "잔여 시도 횟수는 0보다 작을 수 없습니다." }
     }
 
     fun toInt(): Int = value
 
-    operator fun minus(value: Int): RestTryCount {
+    operator fun minus(value: Int): RemainingTryCount {
         var result = this.value - value
         if (result < 0) result = 0
-        return RestTryCount(result)
+        return RemainingTryCount(result)
     }
 }
