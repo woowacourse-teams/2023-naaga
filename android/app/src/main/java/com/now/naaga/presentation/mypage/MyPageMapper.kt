55e93937d3026ea26cbaeb9926b6a53a95e1b958
briandr97 <briandr@naver.com>
2023-08-01 02:33:34 +0900

    feat: 도메인 모델을 UI모델로 변경할 매퍼 함수 생성

---
+++ android/app/src/main/java/com/now/naaga/presentation/mypage/MyPageMapper.kt
@@ -0,0 +1,48 @@
+package com.now.naaga.presentation.mypage
+
+import android.content.Context
+import com.now.domain.model.Place
+import com.now.domain.model.Statistic
+import com.now.naaga.R
+
+fun Statistic.toUiModel(context: Context): List<MyPageStatisticsUiModel> {
+    return listOf(
+        MyPageStatisticsUiModel(
+            adventureCount,
+            context.getString(R.string.mypage_count),
+            context.getString(R.string.mypage_total_adventure),
+        ),
+        MyPageStatisticsUiModel(
+            successCount,
+            context.getString(R.string.mypage_count),
+            context.getString(R.string.mypage_adventure_success),
+        ),
+        MyPageStatisticsUiModel(
+            failureCount,
+            context.getString(R.string.mypage_count),
+            context.getString(R.string.mypage_adventure_failure),
+        ),
+        MyPageStatisticsUiModel(
+            totalPlayTime,
+            context.getString(R.string.mypage_minute),
+            context.getString(R.string.mypage_total_play_time),
+        ),
+        MyPageStatisticsUiModel(
+            totalDistance,
+            context.getString(R.string.mypage_meter),
+            context.getString(R.string.mypage_total_adventure_distance),
+        ),
+        MyPageStatisticsUiModel(
+            totalHintUses,
+            context.getString(R.string.mypage_number),
+            context.getString(R.string.mypage_total_hint_uses),
+        ),
+    )
+}
+
+fun Place.toUiModel(): MyPagePlaceUiModel {
+    return MyPagePlaceUiModel(
+        image,
+        description,
+    )
+}
