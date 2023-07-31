117fb6ed809b176043722ef9f9fdb5fcaf56dbde
hyunji <83613106+hyunji1203@users.noreply.github.com>
2023-07-31 17:57:25 +0900

    feat: RankRepository 생성 (#109)
    * refactor: Service 명 수정
    
    * feat: RankRepository Interface 구현
    
    * feat: PlayerDto를 도메인 객체로 바꿔주는 도메인 mapper 생성
    
    * feat: RankDto를 도메인 객체로 바꿔주는 도메인 mapper 생성
    
    * feat: 서버와 Rank 데이터를 주고 받기 위해 팔요한 rank service 객체 생성
    
    * feat: RankRepository 구현체 생성
    
    * refactor: 함수 인자 표시

--- android/app/src/main/java/com/now/naaga/data/remote/retrofit/ServicePool.kt
+++ android/app/src/main/java/com/now/naaga/data/remote/retrofit/ServicePool.kt
@@ -2,7 +2,9 @@ package com.now.naaga.data.remote.retrofit
 
 import com.now.naaga.data.remote.retrofit.RetrofitFactory.retrofit
 import com.now.naaga.data.remote.retrofit.service.AdventureService
+import com.now.naaga.data.remote.retrofit.service.RankService
 
 object ServicePool {
     val adventureService = retrofit.create(AdventureService::class.java)
+    val rankService = retrofit.create(RankService::class.java)
 }
