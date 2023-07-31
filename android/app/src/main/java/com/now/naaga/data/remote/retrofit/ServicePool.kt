61832b411ee65ad3434793a6c4dafcad36d5a2bc
hyunji <83613106+hyunji1203@users.noreply.github.com>
2023-07-31 18:04:26 +0900

    feat: StatisticsRepository 생성 (#110)
    * rename: 파일명 수정
    
    * feat: StatisticsRepository 인터페이스 생성
    
    * feat: 서버와의 Statistics 관련 통신을 위한 service 객체 생성
    
    * feat: StatisticsDto 객체를 도메인 객체로 바꿔주는 Mapper 생성
    
    * feat: StatisticsRepository 구현체 생성
    
    * refactor: 함수 인자 표시

--- android/app/src/main/java/com/now/naaga/data/remote/retrofit/ServicePool.kt
+++ android/app/src/main/java/com/now/naaga/data/remote/retrofit/ServicePool.kt
@@ -3,8 +3,10 @@ package com.now.naaga.data.remote.retrofit
 import com.now.naaga.data.remote.retrofit.RetrofitFactory.retrofit
 import com.now.naaga.data.remote.retrofit.service.AdventureService
 import com.now.naaga.data.remote.retrofit.service.RankService
+import com.now.naaga.data.remote.retrofit.service.StatisticsService
 
 object ServicePool {
     val adventureService = retrofit.create(AdventureService::class.java)
     val rankService = retrofit.create(RankService::class.java)
+    val statisticsService = retrofit.create(StatisticsService::class.java)
 }
--- android/app/src/main/java/com/now/naaga/data/remote/retrofit/service/StatisticsService.kt
+++ android/app/src/main/java/com/now/naaga/data/remote/retrofit/service/StatisticsService.kt
@@ -1,10 +1,10 @@
 package com.now.naaga.data.remote.retrofit.service
 
-import com.now.naaga.data.remote.dto.third.StatisticDto
+import com.now.naaga.data.remote.dto.third.StatisticsDto
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface StatisticsService {
     @GET("/statistics/my")
-    fun getMyStatistics(): Call<StatisticDto>
+    fun getMyStatistics(): Call<StatisticsDto>
 }
