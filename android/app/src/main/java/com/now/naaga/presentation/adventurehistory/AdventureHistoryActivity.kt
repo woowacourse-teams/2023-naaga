c80cefd9b9794e32b7adf2d7415bf976290fa690
hyunji1203 <hj27720@gmail.com>
2023-07-31 16:12:26 +0900

    feat: AdventureHistoryActivity에 모험 기록을 보여줄 리사이클러뷰 구현

--- android/app/src/main/java/com/now/naaga/presentation/adventurehistory/AdventureHistoryActivity.kt
+++ android/app/src/main/java/com/now/naaga/presentation/adventurehistory/AdventureHistoryActivity.kt
@@ -5,17 +5,22 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.now.domain.model.AdventureResult
 import com.now.naaga.databinding.ActivityAdventureHistoryBinding
+import com.now.naaga.presentation.adventurehistory.recyclerview.AdventureHistoryAdapter
 
 class AdventureHistoryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAdventureHistoryBinding
     private lateinit var viewModel: AdventureHistoryViewModel
+
+    private val historyAdapter = AdventureHistoryAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAdventureHistoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initViewModel()
+        initRecyclerView()
         viewModel.fetchHistories()
         startObserving()
     }
@@ -25,9 +30,22 @@ class AdventureHistoryActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
     }
 
+    private fun initRecyclerView() {
+        binding.rvAdventureHistoryVisitedPlaces.apply {
+            adapter = historyAdapter
+            itemAnimator = null
+            setHasFixedSize(true)
+        }
+    }
+
     private fun startObserving() {
         viewModel.places.observe(this) { places ->
+            updateHistory(places)
         }
+    }
+
+    private fun updateHistory(places: List<AdventureResult>) {
+        historyAdapter.submitList(places)
     }
 
     companion object {
