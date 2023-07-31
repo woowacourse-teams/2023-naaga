95cfe8d86bff4e6a2475b563226773b38eb7d11b
briandr97 <briandr@naver.com>
2023-07-31 16:11:01 +0900

    feat: 레이아웃이 두개로 나눠짐에 따라 어댑터 수정중

--- android/app/src/main/java/com/now/naaga/presentation/mypage/MyPageAdapter.kt
+++ android/app/src/main/java/com/now/naaga/presentation/mypage/MyPageAdapter.kt
@@ -1,55 +1,53 @@
 package com.now.naaga.presentation.mypage
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.now.naaga.databinding.RvMypageItemBinding
+import com.now.naaga.databinding.RvMypageItemPlaceBinding
+import com.now.naaga.databinding.RvMypageItemStatisticsBinding
 
 class MyPageAdapter(
     private val contents: List<MyPageItemUiModel>,
-) : RecyclerView.Adapter<MyPageAdapter.MyPageViewHolder>() {
+) : RecyclerView.Adapter<MyPageAdapter.MyPageStatisticsViewHolder>() {
 
-    class MyPageViewHolder(private val binding: RvMypageItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(data: MyPageItemUiModel) {
-            when (data) {
-                is MyPagePlaceUiModel -> bindPlaceModel(data)
-                is MyPageStatisticsUiModel -> bindStatisticsModel(data)
-            }
-        }
-
-        private fun bindPlaceModel(data: MyPagePlaceUiModel) {
-            binding.ivMypageItem.clipToOutline = true
+    class MyPagePlaceViewHolder(private val binding: RvMypageItemPlaceBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: MyPagePlaceUiModel) {
+            binding.model = data
             Glide.with(binding.ivMypageItem).load(data.image).into(binding.ivMypageItem)
-            binding.tvMypageItemDescription.text = data.description
-            binding.clMypageItemValue.visibility = View.INVISIBLE
-        }
-
-        private fun bindStatisticsModel(data: MyPageStatisticsUiModel) {
-            binding.tvMapygeItemValue.text = data.value.toString()
-            binding.tvMypageItemUnit.text = data.unit
-            binding.tvMypageItemDescription.text = data.description
-            binding.ivMypageItem.visibility = View.INVISIBLE
         }
 
         companion object {
-            fun getView(parent: ViewGroup): RvMypageItemBinding {
+            fun getView(parent: ViewGroup): RvMypageItemPlaceBinding {
                 val layoutInflater: LayoutInflater = LayoutInflater.from(parent.context)
-                return RvMypageItemBinding.inflate(layoutInflater, parent, false)
+                return RvMypageItemPlaceBinding.inflate(layoutInflater, parent, false)
             }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPageViewHolder {
-        return MyPageViewHolder(MyPageViewHolder.getView(parent))
+    class MyPageStatisticsViewHolder(private val binding: RvMypageItemStatisticsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: MyPageStatisticsUiModel) {
+            binding.model = data
+        }
+
+        companion object {
+            fun getView(parent: ViewGroup): RvMypageItemStatisticsBinding {
+                val layoutInflater: LayoutInflater = LayoutInflater.from(parent.context)
+                return RvMypageItemStatisticsBinding.inflate(layoutInflater, parent, false)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPageStatisticsViewHolder {
+        return MyPageStatisticsViewHolder(MyPageStatisticsViewHolder.getView(parent))
     }
 
     override fun getItemCount(): Int {
         return contents.size
     }
 
-    override fun onBindViewHolder(holder: MyPageViewHolder, position: Int) {
-        holder.bind(contents[position])
+    override fun onBindViewHolder(holder: MyPageStatisticsViewHolder, position: Int) {
+        holder.bind(contents[position] as MyPageStatisticsUiModel)
     }
 }
