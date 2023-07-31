db2e1e4defa16ac1f482dd52865c5e412fb3839f
krrong <khk9664@naver.com>
2023-07-31 15:19:47 +0900

    feat: 장소 path 레트로핏 서비스 구현 (#103)

---
+++ android/app/src/main/java/com/now/naaga/data/remote/retrofit/service/PlaceService.kt
@@ -0,0 +1,31 @@
+package com.now.naaga.data.remote.retrofit.service
+
+import com.now.naaga.data.remote.dto.third.PlaceDto
+import okhttp3.MultipartBody
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface PlaceService {
+    @GET("/places")
+    fun getMyPlace(
+        @Query("sort-by") sort: String,
+        @Query("order") order: String,
+    ): Call<List<PlaceDto>>
+
+    @GET("/places/{placeId}")
+    fun getPlace(
+        @Path("placeId") placeId: Long,
+    ): Call<PlaceDto>
+
+    @Multipart
+    @POST("/places")
+    fun submitPlace(
+        @Part placeDto: PlaceDto,
+        @Part file: MultipartBody.Part,
+    ): Call<PlaceDto>
+}
