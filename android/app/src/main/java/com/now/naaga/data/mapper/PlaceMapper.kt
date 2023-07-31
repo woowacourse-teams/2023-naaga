00471867f2ecf838fb4a7bdab2afe64038037043
krrong <khk9664@naver.com>
2023-08-01 03:25:07 +0900

    feat: PlaceRepository 생성 (#118)
    * feat: 장소 repository interface 생성
    
    * feat: 서비스풀에 장소 서비스 추가
    
    * feat: place mapper 생성
    
    * feat: 장소 repository 구현체 생성
    
    * refactor: 오류처리 변경에 따른 repository 코드 수정

--- android/app/src/main/java/com/now/naaga/data/mapper/PlaceMapper.kt
+++ android/app/src/main/java/com/now/naaga/data/mapper/PlaceMapper.kt
@@ -3,6 +3,16 @@ package com.now.naaga.data.mapper
 import com.now.domain.model.Place
 import com.now.naaga.data.remote.dto.third.PlaceDto
 
+fun Place.toDto(): PlaceDto {
+    return PlaceDto(
+        id = id,
+        name = name,
+        coordinate = coordinate.toDto(),
+        imageUrl = image,
+        description = description,
+    )
+}
+
 fun PlaceDto.toDomain(): Place {
     return Place(
         id = id,
