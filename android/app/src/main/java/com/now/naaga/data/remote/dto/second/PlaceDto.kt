223dfa5b41b5242b8ba7f4154d77aa505121b6a8
k_dragonm <78788847+briandr97@users.noreply.github.com>
2023-07-31 13:44:13 +0900

    feat: 명세서 기준으로 Dto들 생성 (#89)
    * feat: 명세서 기준으로 Dto들 생성
    
    * feat: 변경된 API 명세에 맞춰 Dto 변경

---
+++ android/app/src/main/java/com/now/naaga/data/remote/dto/second/PlaceDto.kt
@@ -0,0 +1,13 @@
+package com.now.naaga.data.remote.dto.second
+
+import com.now.naaga.data.remote.dto.CoordinateDto
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PlaceDto(
+    val id: Long,
+    val name: String,
+    val coordinate: CoordinateDto,
+    val imageUrl: String,
+    val description: String,
+)
