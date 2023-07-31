7622c2c7ce77dc4e2e224605a425b0aadb19caa9
hyunji1203 <hj27720@gmail.com>
2023-07-31 17:05:21 +0900

    feat: 서버와의 Statistics 관련 통신을 위한 service 객체 생성

--- android/app/src/main/java/com/now/naaga/data/remote/retrofit/ServicePool.kt
+++ android/app/src/main/java/com/now/naaga/data/remote/retrofit/ServicePool.kt
@@ -2,7 +2,10 @@ package com.now.naaga.data.remote.retrofit
 
 import com.now.naaga.data.remote.retrofit.RetrofitFactory.retrofit
 import com.now.naaga.data.remote.retrofit.service.AdventureService
+import com.now.naaga.data.remote.retrofit.service.StatisticsService
+import retrofit2.create
 
 object ServicePool {
     val adventureService = retrofit.create(AdventureService::class.java)
+    val statisticsService = retrofit.create(StatisticsService::class.java)
 }
