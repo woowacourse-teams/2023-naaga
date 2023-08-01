a1dae7edd57d687f6447e19b9d79bd761d84c985
briandr97 <briandr@naver.com>
2023-08-02 01:57:24 +0900

    feat: 도메인이 추가 및 변경됨에 따라 UiModel과 Mapper를 생성

--- android/app/src/main/java/com/now/naaga/presentation/uimodel/mapper/AdventureMapper.kt
+++ android/app/src/main/java/com/now/naaga/presentation/uimodel/mapper/AdventureMapper.kt
@@ -1,7 +1,10 @@
 package com.now.naaga.presentation.uimodel.mapper
 
 import com.now.domain.model.Adventure
+import com.now.domain.model.Game
+import com.now.domain.model.RemainingTryCount
 import com.now.naaga.presentation.uimodel.model.AdventureUiModel
+import com.now.naaga.presentation.uimodel.model.GameUiModel
 
 fun Adventure.toUi(): AdventureUiModel {
     return AdventureUiModel(
@@ -16,5 +19,31 @@ fun AdventureUiModel.toDomain(): Adventure {
         id = id,
         destination = destination.toDomain(),
         adventureStatus = adventureStatus,
+    )
+}
+
+fun Game.toUi(): GameUiModel {
+    return GameUiModel(
+        id = id,
+        createdAt = createdAt,
+        startCoordinate = startCoordinate.toUi(),
+        destination = destination.toUi(),
+        player = player.toUi(),
+        adventureStatus = adventureStatus,
+        remainingTryCount = remainingTryCount.toInt(),
+        hints = hints.map { it.toUi() },
+    )
+}
+
+fun GameUiModel.toDomain(): Game {
+    return Game(
+        id = id,
+        createdAt = createdAt,
+        startCoordinate = startCoordinate.toDomain(),
+        destination = destination.toDomain(),
+        player = player.toDomain(),
+        adventureStatus = adventureStatus,
+        remainingTryCount = RemainingTryCount(remainingTryCount),
+        hints = hints.map { it.toDomain() },
     )
 }
