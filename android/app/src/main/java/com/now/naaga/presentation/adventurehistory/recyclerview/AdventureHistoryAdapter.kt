bc71d42e48e66c6ca041766ec5704042e3aab146
briandr97 <briandr@naver.com>
2023-08-01 14:55:21 +0900

    refactor: 뷰 생성 위치 변경, 함수명 변경

--- android/app/src/main/java/com/now/naaga/presentation/adventurehistory/recyclerview/AdventureHistoryAdapter.kt
+++ android/app/src/main/java/com/now/naaga/presentation/adventurehistory/recyclerview/AdventureHistoryAdapter.kt
@@ -1,17 +1,13 @@
 package com.now.naaga.presentation.adventurehistory.recyclerview
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import com.now.domain.model.AdventureResult
-import com.now.naaga.databinding.ItemHistoryBinding
 
 class AdventureHistoryAdapter : ListAdapter<AdventureResult, AdventureHistoryViewHolder>(historyDiff) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdventureHistoryViewHolder {
-        val binding =
-            ItemHistoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return AdventureHistoryViewHolder(binding)
+        return AdventureHistoryViewHolder(AdventureHistoryViewHolder.getView(parent))
     }
 
     override fun onBindViewHolder(holder: AdventureHistoryViewHolder, position: Int) {
--- android/app/src/main/java/com/now/naaga/presentation/adventurehistory/recyclerview/AdventureHistoryViewHolder.kt
+++ android/app/src/main/java/com/now/naaga/presentation/adventurehistory/recyclerview/AdventureHistoryViewHolder.kt
@@ -1,21 +1,21 @@
 package com.now.naaga.presentation.adventurehistory.recyclerview
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.now.domain.model.AdventureResult
 import com.now.domain.model.AdventureResultType
 import com.now.naaga.R
 import com.now.naaga.databinding.ItemHistoryBinding
 
-class AdventureHistoryViewHolder(
-    private val binding: ItemHistoryBinding,
-) : RecyclerView.ViewHolder(binding.root) {
+class AdventureHistoryViewHolder(private val binding: ItemHistoryBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(adventureResult: AdventureResult) {
         binding.adventureResult = adventureResult
-        setPhoto(adventureResult.resultType)
+        setSuccessOrFailure(adventureResult.resultType)
         setName(adventureResult)
     }
 
-    private fun setPhoto(resultType: AdventureResultType) {
+    private fun setSuccessOrFailure(resultType: AdventureResultType) {
         when (resultType) {
             AdventureResultType.SUCCESS -> binding.ivAdventureHistoryStamp.setImageResource(R.drawable.ic_success)
             AdventureResultType.FAIL -> binding.ivAdventureHistoryStamp.setImageResource(R.drawable.ic_fail)
@@ -33,5 +33,10 @@ class AdventureHistoryViewHolder(
 
     companion object {
         private const val DESTINATION_NAME_IN_FAILURE_CASE = "????"
+
+        fun getView(parent: ViewGroup): ItemHistoryBinding {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            return ItemHistoryBinding.inflate(layoutInflater, parent, false)
+        }
     }
 }
