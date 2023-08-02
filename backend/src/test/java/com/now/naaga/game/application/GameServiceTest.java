1f5bd4073e3ce96166e40e71fcef4a8aa0b07bfa
zillionme <beatus96@naver.com>
2023-08-02 20:49:20 +0900

    fix: 서비스 테스트 트랜잭션 추가

--- backend/src/test/java/com/now/naaga/game/application/GameServiceTest.java
+++ backend/src/test/java/com/now/naaga/game/application/GameServiceTest.java
@@ -2,10 +2,12 @@ package com.now.naaga.game.application;
 
 import static com.now.naaga.game.domain.EndType.ARRIVED;
 import static com.now.naaga.game.domain.GameStatus.IN_PROGRESS;
-import static com.now.naaga.game.fixture.GameTestFixture.GS25_방이도곡점_좌표;
-import static com.now.naaga.game.fixture.GameTestFixture.PLAYER;
-import static com.now.naaga.game.fixture.GameTestFixture.잠실_루터회관;
-import static com.now.naaga.game.fixture.GameTestFixture.잠실_루터회관_정문_근처_좌표;
+import static com.now.naaga.game.fixture.GameFixture.GAME_IN_PROGRESS;
+import static com.now.naaga.game.fixture.MemberFixture.MEMBER_CHAE;
+import static com.now.naaga.game.fixture.PlaceFixture.잠실_루터회관;
+import static com.now.naaga.game.fixture.PlayerFixture.PLAYER;
+import static com.now.naaga.game.fixture.PositionFixture.GS25_방이도곡점_좌표;
+import static com.now.naaga.game.fixture.PositionFixture.잠실_루터회관_정문_근처_좌표;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -15,11 +17,10 @@ import com.now.naaga.game.application.dto.EndGameCommand;
 import com.now.naaga.game.domain.Game;
 import com.now.naaga.game.domain.GameResult;
 import com.now.naaga.game.domain.ScorePolicy;
-import com.now.naaga.game.presentation.dto.EndGameRequest;
 import com.now.naaga.game.repository.GameRepository;
 import com.now.naaga.game.repository.GameResultRepository;
 import com.now.naaga.player.application.PlayerService;
-import com.now.naaga.player.presentation.dto.PlayerRequest;
+import com.now.naaga.player.domain.Player;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Optional;
@@ -31,10 +32,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @Sql("/truncate.sql")
+@Transactional
 @SpringBootTest
 class GameServiceTest {
     
@@ -53,19 +56,18 @@ class GameServiceTest {
     private GameResultRepository gameResultRepository;
     
     @Test
-    void 종료된_게임의_결과를_저장한다() {
+    void 종료된_게임의_결과를_저장한다() throws InterruptedException {
         // Given
-        EndGameCommand endGameCommand = new EndGameCommand(2l, ARRIVED, 잠실_루터회관_정문_근처_좌표,1l);
-        
-        
-        LocalDateTime startTime = LocalDateTime.of(2023, 7, 31, 12, 00, 30);
-        Game game = new Game(1l, IN_PROGRESS, PLAYER, 잠실_루터회관, GS25_방이도곡점_좌표, 3, Collections.emptyList(), startTime, null);
+        EndGameCommand endGameCommand = new EndGameCommand(1l, ARRIVED, 잠실_루터회관_정문_근처_좌표,2l);
+        Player player = PLAYER("chae", MEMBER_CHAE());
+        Game game = new Game(player, 잠실_루터회관(player), GS25_방이도곡점_좌표);
         
         // Mock 객체 설정
         when(gameRepository.findById(anyLong())).thenReturn(Optional.of(game));
-        when(playerService.findPlayerById(2l)).thenReturn(PLAYER);
+        when(playerService.findPlayerById(anyLong())).thenReturn(player);
         
         // When
+        Thread.sleep(1000);
         gameService.endGame(endGameCommand);
         
         // Then
