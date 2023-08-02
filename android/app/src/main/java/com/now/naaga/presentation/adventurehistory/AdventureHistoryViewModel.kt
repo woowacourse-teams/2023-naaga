cbabbc1a21585ecceee3bcf22f1c532835d95777
k_dragonm <78788847+briandr97@users.noreply.github.com>
2023-08-02 17:48:54 +0900

    feat: 모험 기록 뷰 기능 구현 및 서버 통신 구현 (#151)
    * design: 모험 기록 뷰 디자인 변경
    
    * design: 모험 기록 뷰 리사이클러뷰 아이템 디자인 변경
    
    * refactor: 뷰 생성 위치 변경, 함수명 변경
    
    * feat: 아이템에 사진 넣는 로직 추가
    
    * feat: 뷰모델 팩토리 생성, 로컬 목데이터에서 가져오던 데이터를 서버로부터 가져오도록 변경
    
    * feat: manifest exported 변경
    
    * feat: 이미지 뷰 corner radius 적용
    
    * design: 색상 변경 및 그림자 적용
    
    * design: 마진값 변경

--- android/app/src/main/java/com/now/naaga/presentation/adventurehistory/AdventureHistoryViewModel.kt
+++ android/app/src/main/java/com/now/naaga/presentation/adventurehistory/AdventureHistoryViewModel.kt
@@ -3,42 +3,31 @@ package com.now.naaga.presentation.adventurehistory
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.now.domain.model.AdventureResult
-import com.now.domain.model.AdventureResultType.SUCCESS
-import com.now.domain.model.Coordinate
-import com.now.domain.model.Place
-import java.time.LocalDateTime
-import java.time.LocalTime
+import com.now.domain.model.OrderType
+import com.now.domain.model.SortType
+import com.now.domain.repository.AdventureRepository2
+import com.now.naaga.data.repository.ThirdDemoAdventureRepository
 
-class AdventureHistoryViewModel : ViewModel() {
-    private val _places = MutableLiveData<List<AdventureResult>>()
-    val places: LiveData<List<AdventureResult>> = _places
+class AdventureHistoryViewModel(private val adventureRepository2: AdventureRepository2) : ViewModel() {
+    private val _adventureResults = MutableLiveData<List<AdventureResult>>()
+    val adventureResults: LiveData<List<AdventureResult>> = _adventureResults
 
     fun fetchHistories() {
-        _places.value = MOCK_ADVENTURE_HISTORY_DATA
+        adventureRepository2.fetchMyAdventureResults(SortType.TIME, OrderType.DESCENDING) { result ->
+            result.onSuccess { _adventureResults.value = it }
+        }
     }
 
     companion object {
-        val MOCK_ADVENTURE_HISTORY_DATA = List(4) {
-            AdventureResult(
-                id = 1,
-                gameId = 1,
-                destination = Place(
-                    id = 1,
-                    name = "선정릉 돌담길",
-                    coordinate = Coordinate(35.3094, 127.21389),
-                    image = "",
-                    description = "test",
-                ),
-                resultType = SUCCESS,
-                score = 130,
-                playTime = LocalTime.of(13, 30, 0),
-                distance = 300,
-                hintUses = 3,
-                tryCount = 2,
-                beginTime = LocalDateTime.of(2023, 8, 1, 13, 30, 0),
-                endTime = LocalDateTime.of(2023, 8, 1, 14, 0, 0),
-            )
+        val Factory = AdventureHistoryFactory(ThirdDemoAdventureRepository())
+
+        class AdventureHistoryFactory(private val adventureRepository2: AdventureRepository2) :
+            ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return AdventureHistoryViewModel(adventureRepository2) as T
+            }
         }
     }
 }
