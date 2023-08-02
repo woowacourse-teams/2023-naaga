a7513a8c296f36fa174d6a14658312088a4b78dd
kokodak <kokodakadokok@gmail.com>
2023-08-02 16:04:02 +0900

    feat: 힌트 id를 통해 힌트를 조회하는 기능 구현

--- backend/src/main/java/com/now/naaga/game/application/HintService.java
+++ backend/src/main/java/com/now/naaga/game/application/HintService.java
@@ -1,12 +1,15 @@
 package com.now.naaga.game.application;
 
+import static com.now.naaga.game.exception.GameExceptionType.HINTS_EXHAUSTED;
+import static com.now.naaga.game.exception.GameExceptionType.HINT_NOT_EXIST_IN_GAME;
+
 import com.now.naaga.game.application.dto.CreateHintCommand;
 import com.now.naaga.game.application.dto.FindGameByIdCommand;
+import com.now.naaga.game.application.dto.FindHintByIdCommand;
 import com.now.naaga.game.domain.Direction;
 import com.now.naaga.game.domain.Game;
 import com.now.naaga.game.domain.Hint;
 import com.now.naaga.game.exception.GameException;
-import com.now.naaga.game.exception.GameExceptionType;
 import com.now.naaga.game.repository.HintRepository;
 import com.now.naaga.place.domain.Place;
 import com.now.naaga.place.domain.Position;
@@ -29,12 +32,21 @@ public class HintService {
     public Hint createHint(final CreateHintCommand command) {
         final Game game = gameService.findGameById(new FindGameByIdCommand(command.gameId(), command.playerId()));
         if (!game.canUseMoreHint()) {
-            throw new GameException(GameExceptionType.HINTS_EXHAUSTED);
+            throw new GameException(HINTS_EXHAUSTED);
         }
         final Position coordinate = command.coordinate();
         final Place place = game.getPlace();
         final Direction direction = Direction.calculate(coordinate, place.getPosition());
         final Hint hint = new Hint(coordinate, direction, game);
         return hintRepository.save(hint);
+    }
+
+    public Hint findHintById(final FindHintByIdCommand command) {
+        final Game game = gameService.findGame(new FindGameByIdCommand(command.gameId(), command.playerId()));
+        return game.getHints()
+                .stream()
+                .filter(hint -> hint.getId().equals(command.hintId()))
+                .findAny()
+                .orElseThrow(() -> new GameException(HINT_NOT_EXIST_IN_GAME));
     }
 }
