6b138c22826ffa9b3cd784e531c0e209fb04d6b8
zillionme <100172683+zillionme@users.noreply.github.com>
2023-08-02 21:42:49 +0900

    feat: 게임 종료 API 및 게임 조회 테스트 구현 (#147)
    * test: 게임 종료 기능 테스트
    
    * feat: 게임 종료 기능 기본 구조 추가
    
    * feat: 게임 종료 기능 구현
    
    * test: 게임 점수 계산 기능 테스트
    
    * feat: 게임 점수 계산 기능 구현
    
    * test: 게임 점수 계산 테스트 수정
    
    * test: 게임 종료 및 게임 결과 저장 서비스 테스트
    
    * feat: 게임 종료 서비스 및 dto 구현
    
    * test: 게임 종료 api 테스트
    
    * feat: 게임 종료 api 구현
    
    * test: 게임 종료 api 테스트 수정
    
    * test: sql파일 수정 및 게임 종료 api 테스트 추가
    
    * feat: 모험 결과 DTO와 도메인 객체의 raisedRank 삭제
    
    * feat: 랭크 변동 상황을 알려주는 뷰 삭제
    
    * feat: 인자 변경에 따른 수정 사항 적용
    
    * test: 게임 종료 api 예외 처리 테스트 추가
    
    * refactor: Game 클래스 필요없는 메서드 제거 및 리팩터링
    
    * feat: 모험 기록 뷰 기능 구현 및 서버 통신 구현 (#151)
    
    * design: 모험 기록 뷰 디자인 변경
    
    * design: 모험 기록 뷰 리사이클러뷰 아이템 디자인 변경
    
    * refactor: 뷰 생성 위치 변경, 함수명 변경
    
    * feat: 아이템에 사진 넣는 로직 추가
    
    * feat: 뷰모델 팩토리 생성, 로컬 목데이터에서 가져오던 데이터를 서버로부터 가져오도록 변경
    
    * feat: manifest exported 변경
    
    * feat: 이미지 뷰 corner radius 적용
    
    * design: 색상 변경 및 그림자 적용
    
    * design: 마진값 변경
    
    * [AN] feat: MyPage 기능 구현 (#146)
    
    * feat: 게임 결과 조회 기능 구현 (#152)
    
    * feat: 게임 아이디를 통해 게임결과를 조회한다
    
    * feat: 결과 반환을 위한 responseDTO 생성
    
    * refactor: 게임 결과를 위한 도메인 수정
    
    * feat: 게임 결과 반환 기능 구현 완료
    
    * style: 코드 컨벤션에 맞게 수정
    
    * style: 코드 컨벤션에 맞게 수정
    
    * refactor: 게임 도메인의 오류 리팩토링
    
    * refactor: final 추가
    
    * refactor: 게임 전체 결과 가져오는 로직 수정
    
    * refactor: 테스트 fixture 클래스 생성
    
    * refactor: 테스트 fixture 적용
    
    * fix: 레포지토리 테스트 truncate.sql 추가
    
    * fix: 서비스 테스트 트랜잭션 추가
    
    * test: 게임 식별자로 게임 조회 테스트 추가
    
    * chore: 식별자로 게임 조회 메서드명 변경
    
    * chore: 머지 컨플릭트 해결
    
    ---------
    
    Co-authored-by: hyunji1203 <hj27720@gmail.com>
    Co-authored-by: k_dragonm <78788847+briandr97@users.noreply.github.com>
    Co-authored-by: krrong <khk9664@naver.com>
    Co-authored-by: chaewon121 <80631952+chaewon121@users.noreply.github.com>

---
+++ backend/src/test/java/com/now/naaga/game/fixture/MemberFixture.java
@@ -0,0 +1,13 @@
+package com.now.naaga.game.fixture;
+
+import com.now.naaga.member.domain.Member;
+
+public class MemberFixture {
+    
+    public static Member MEMBER_IRYE() {
+        return new Member("irye@gmail.com", "0104");
+    }
+    public static Member MEMBER_CHAE() {
+        return new Member("chae@gmail.com", "0121");
+    }
+}
