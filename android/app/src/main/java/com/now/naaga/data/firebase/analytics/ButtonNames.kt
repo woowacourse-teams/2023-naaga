20c6dcb2439b805f71fc16937212b753bd7275d9
krrong <khk9664@naver.com>
2023-08-04 04:17:28 +0900

    feat: BeginAdventureActivity firebase용 버튼 로그 추가

--- android/app/src/main/java/com/now/naaga/data/firebase/analytics/ButtonNames.kt
+++ android/app/src/main/java/com/now/naaga/data/firebase/analytics/ButtonNames.kt
@@ -1,6 +1,7 @@
 package com.now.naaga.data.firebase.analytics
 
-const val BUTTON = "BUTTON"
-
 // BeginActivity
-const val BEGIN_END_ADVENTURE = "END_ADVENTURE"
+const val BEGIN_BEGIN_ADVENTURE = "BEGIN_ADVENTURE"
+const val BEGIN_GO_RANK = "GO_RANK"
+const val BEGIN_GO_UPLOAD = "GO_UPLOAD"
+const val BEGIN_GO_MYPAGE = "GO_MYPAGE"
