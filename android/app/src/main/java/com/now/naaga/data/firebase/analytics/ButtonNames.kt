00b283a9a2fd15f7bfde83b9eb1ccfb447ff7da7
krrong <khk9664@naver.com>
2023-08-04 04:43:21 +0900

    feat: AdventureResultActivity firebase용 버튼 로그 추가

--- android/app/src/main/java/com/now/naaga/data/firebase/analytics/ButtonNames.kt
+++ android/app/src/main/java/com/now/naaga/data/firebase/analytics/ButtonNames.kt
@@ -1,6 +1,6 @@
 package com.now.naaga.data.firebase.analytics
 
-// BeginActivity
+// BeginAdventureActivity
 const val BEGIN_BEGIN_ADVENTURE = "BEGIN_ADVENTURE"
 const val BEGIN_GO_RANK = "GO_RANK"
 const val BEGIN_GO_UPLOAD = "GO_UPLOAD"
@@ -8,3 +8,6 @@ const val BEGIN_GO_MYPAGE = "GO_MYPAGE"
 
 // LocationPermissionDialog
 const val LOCATION_PERMISSION_OPEN_SETTING = "OPEN_SETTING"
+
+// AdventureResultActivity
+const val RESULT_RESULT_RETURN = "RESULT_RETURN"
