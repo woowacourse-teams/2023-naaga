cb31a0edbf32774cc0b81bb9e1c3de88df54695c
krrong <khk9664@naver.com>
2023-08-04 04:30:15 +0900

    feat: LocationPermissionDialog firebase용 버튼 로그 추가

--- android/app/src/main/java/com/now/naaga/data/firebase/analytics/ButtonNames.kt
+++ android/app/src/main/java/com/now/naaga/data/firebase/analytics/ButtonNames.kt
@@ -5,3 +5,6 @@ const val BEGIN_BEGIN_ADVENTURE = "BEGIN_ADVENTURE"
 const val BEGIN_GO_RANK = "GO_RANK"
 const val BEGIN_GO_UPLOAD = "GO_UPLOAD"
 const val BEGIN_GO_MYPAGE = "GO_MYPAGE"
+
+// LocationPermissionDialog
+const val LOCATION_PERMISSION_OPEN_SETTING = "OPEN_SETTING"
