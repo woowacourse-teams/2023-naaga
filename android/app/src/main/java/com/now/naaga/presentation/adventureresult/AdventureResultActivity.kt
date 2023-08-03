5b716e1304fd3b59b36ca3f1d3f11c0f4aac49ab
briandr97 <briandr@naver.com>
2023-08-03 22:36:28 +0900

    feat: 받아올 값의 타입과 보여줄 뷰의 형태 변화를 반영한 코드 수정

--- android/app/src/main/java/com/now/naaga/presentation/adventureresult/AdventureResultActivity.kt
+++ android/app/src/main/java/com/now/naaga/presentation/adventureresult/AdventureResultActivity.kt
@@ -15,8 +15,6 @@ import com.now.naaga.data.repository.DefaultAdventureRepository
 import com.now.naaga.data.repository.DefaultRankRepository
 import com.now.naaga.databinding.ActivityAdventureResultBinding
 import com.now.naaga.presentation.beginadventure.BeginAdventureActivity
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
 
 class AdventureResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAdventureResultBinding
@@ -50,7 +48,6 @@ class AdventureResultActivity : AppCompatActivity() {
         viewModel.adventureResult.observe(this) { adventureResult ->
             setResultType(adventureResult)
             setPhoto(adventureResult.destination.image)
-            setPlayTime(adventureResult.playTime)
         }
 
         viewModel.errorMessage.observe(this) { errorMessage ->
@@ -88,11 +85,6 @@ class AdventureResultActivity : AppCompatActivity() {
             .into(binding.ivAdventureResultPhoto)
     }
 
-    private fun setPlayTime(playTime: LocalTime) {
-        val timeFormatter = DateTimeFormatter.ofPattern(TIME_FORMATTER_PATTERN)
-        binding.tvAdventureResultPlayTime.text = playTime.format(timeFormatter)
-    }
-
     private fun setClickListeners() {
         binding.btnAdventureResultReturn.setOnClickListener {
             startActivity(BeginAdventureActivity.getIntent(this))
@@ -102,7 +94,6 @@ class AdventureResultActivity : AppCompatActivity() {
     companion object {
         private const val GAME_ID = "GAME_ID"
         private const val MESSAGE_IN_RESULT_TYPE_NONE = "네트워크에 문제가 생겼습니다."
-        private const val TIME_FORMATTER_PATTERN = "HH:mm:ss"
 
         fun getIntentWithGameId(context: Context, gameId: Long): Intent {
             return Intent(context, AdventureResultActivity::class.java).apply {
