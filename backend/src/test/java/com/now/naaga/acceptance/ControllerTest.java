c7667b4851df5d8a3e0b8252e1c05a391690856c
chaewon121 <cw121@naver.com>
2023-08-03 11:07:41 +0900

    test: @Sql조건 수정

--- backend/src/test/java/com/now/naaga/acceptance/ControllerTest.java
+++ backend/src/test/java/com/now/naaga/acceptance/ControllerTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 
-@Sql(value = "/truncate.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@Sql("/truncate.sql")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class ControllerTest {
 
