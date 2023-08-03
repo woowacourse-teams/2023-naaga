bc0b41c110190df71d806a0ff41b5ba4297f92cc
kokodak <kokodakadokok@gmail.com>
2023-08-03 13:41:53 +0900

    feat: Location 헤더로부터 id를 추출하는 기능 구현

--- backend/src/test/java/com/now/naaga/common/CommonControllerTest.java
+++ backend/src/test/java/com/now/naaga/common/CommonControllerTest.java
@@ -1,6 +1,8 @@
 package com.now.naaga.common;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -15,5 +17,10 @@ public abstract class CommonControllerTest {
 
     protected void setUp() {
         RestAssured.port = port;
+    }
+
+    protected Long getIdFromLocationHeader(ExtractableResponse<Response> extractableResponse) {
+        String[] split = extractableResponse.header("Location").split("/");
+        return Long.parseLong(split[split.length - 1]);
     }
 }
