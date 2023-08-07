4850d921f12aa05f6790da5b02692b5030774319
briandr97 <briandr@naver.com>
2023-08-07 16:46:06 +0900

    feat: 다른 라이브 데이터를 받아서 한번만 호출할 수 있도록 효율적으로 변경
     - 기존에는 DiposableLiveData를 만들고 이 데이터에 setValue까지 직접 해줘야했는데 이제는 다른 라이브데이터를 받아서 DisposableLiveData까지 setValue해주지 않아도 된다.

--- android/app/src/main/java/com/now/naaga/presentation/onadventure/DisposableLiveData.kt
+++ android/app/src/main/java/com/now/naaga/presentation/onadventure/DisposableLiveData.kt
@@ -1,22 +1,21 @@
 package com.now.naaga.presentation.onadventure
 
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import java.util.concurrent.atomic.AtomicBoolean
 
-class DisposableLiveData<T> : MutableLiveData<T>() {
+class DisposableLiveData<T>(private val m: LiveData<T>) : LiveData<T>() {
     private var isFirst = AtomicBoolean(true)
 
     override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        m.observe(owner) {
+            setValue(it)
+        }
         super.observe(owner) { t: T ->
             if (isFirst.compareAndSet(true, false)) {
                 observer.onChanged(t)
             }
         }
-    }
-
-    override fun setValue(value: T) {
-        super.setValue(value)
     }
 }
