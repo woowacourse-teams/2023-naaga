729f16764518b5076da4571978dd1480e9ace31a
kokodak <kokodakadokok@gmail.com>
2023-08-07 21:49:33 +0900

    fix: @Value 어노테이션 삭제

--- backend/src/main/java/com/now/naaga/common/infrastructure/MultipartFileManager.java
+++ backend/src/main/java/com/now/naaga/common/infrastructure/MultipartFileManager.java
@@ -12,7 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 @Component
 public class MultipartFileManager implements FileManager<MultipartFile> {
 
-    @Value("${multipartFile.directory.path}")
     private String saveDirectory;
 
     @Override
