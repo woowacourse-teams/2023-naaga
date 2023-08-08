5c2443936f6c20589e00a816dd20a9c5da68dd05
hyunji1203 <hj27720@gmail.com>
2023-08-08 20:32:17 +0900

    feat: Dto, Domain 변환 mapper 추가

---
+++ android/app/src/main/java/com/now/naaga/data/mapper/AuthMapper.kt
@@ -0,0 +1,24 @@
+package com.now.naaga.data.mapper
+
+import com.now.domain.model.PlatformAuth
+import com.now.domain.model.AuthPlatformType
+import com.now.domain.model.NaagaAuth
+import com.now.naaga.data.remote.dto.NaagaAuthDto
+import com.now.naaga.data.remote.dto.PlatformAuthDto
+
+fun PlatformAuthDto.toDomain(): PlatformAuth {
+    return PlatformAuth(token, AuthPlatformType.findByName(type))
+}
+
+fun PlatformAuth.toDto(): PlatformAuthDto {
+    return PlatformAuthDto(token, type.name)
+}
+
+fun NaagaAuthDto.toDomain(): NaagaAuth {
+    return NaagaAuth(accessToken, refreshToken)
+}
+
+fun NaagaAuth.toDto(): NaagaAuthDto {
+    return NaagaAuthDto(accessToken, refreshToken)
+}
+
