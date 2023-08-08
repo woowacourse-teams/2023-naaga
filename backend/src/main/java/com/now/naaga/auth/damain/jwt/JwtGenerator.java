21f7cdfd3320cc411385189d86f652d26318467c
dooboocookie <bkh3030@gmail.com>
2023-08-08 17:21:45 +0900

    feat: JWT 생성 로직

---
+++ backend/src/main/java/com/now/naaga/auth/damain/jwt/JwtGenerator.java
@@ -0,0 +1,32 @@
+package com.now.naaga.auth.damain.jwt;
+
+import com.now.naaga.auth.damain.AuthTokens;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtGenerator {
+
+    private static final String BEARER_TYPE = "Bearer";
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 2;  // 2일
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+
+    private final JwtProvider jwtProvider;
+
+    public JwtGenerator(final JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    public AuthTokens generate(Long memberId) {
+        final long now = (new Date()).getTime();
+        final Date accessTokenExpiredAt = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+        final Date refreshTokenExpiredAt = new Date(now + REFRESH_TOKEN_EXPIRE_TIME);
+
+        final String subject = memberId.toString();
+        final String accessToken = jwtProvider.generate(subject, accessTokenExpiredAt);
+        final String refreshToken = jwtProvider.generate(subject, refreshTokenExpiredAt);
+
+        return new AuthTokens(accessToken, refreshToken);
+    }
+}
---
+++ backend/src/main/java/com/now/naaga/auth/damain/jwt/JwtProvider.java
@@ -0,0 +1,30 @@
+package com.now.naaga.auth.damain.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    private final Key key;
+
+    public JwtProvider(@Value("${jwt.secret-key}") final String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generate(String subject, Date expiredAt) {
+        return Jwts.builder()
+                .setSubject(subject)
+                .setExpiration(expiredAt)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+}
