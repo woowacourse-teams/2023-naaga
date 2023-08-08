0523b8d3c5865a8d07ddd40f61bc3b11f66fe215
dooboocookie <bkh3030@gmail.com>
2023-08-08 17:24:42 +0900

    feat: 로그인 관련 요청 추가

---
+++ backend/src/main/java/com/now/naaga/auth/presentation/dto/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.now.naaga.auth.presentation.dto;
+
+import com.now.naaga.auth.damain.AuthTokens;
+
+public record AuthResponse(String accessToken,
+                           String refreshToken) {
+
+    public static AuthResponse from(final AuthTokens authTokens) {
+        return new AuthResponse(
+                authTokens.getAccessToken(),
+                authTokens.getRefreshToken()
+        );
+    }
+}
