998fb0622c3d3db467f0711c6f9e5a9f4070b3d0
krrong <khk9664@naver.com>
2023-08-09 22:14:13 +0900

    feat: 통신 오류시 DataThrowable을 사용하는 코드 스니펫에 예외 핸들링 추가

--- android/app/src/main/java/com/now/naaga/data/remote/retrofit/RetrofitUtil.kt
+++ android/app/src/main/java/com/now/naaga/data/remote/retrofit/RetrofitUtil.kt
@@ -2,6 +2,7 @@ package com.now.naaga.data.remote.retrofit
 
 import com.now.naaga.data.throwable.DataThrowable.AuthorizationThrowable
 import com.now.naaga.data.throwable.DataThrowable.GameThrowable
+import com.now.naaga.data.throwable.DataThrowable.IllegalStateThrowable
 import com.now.naaga.data.throwable.DataThrowable.PlaceThrowable
 import com.now.naaga.data.throwable.DataThrowable.PlayerThrowable
 import com.now.naaga.data.throwable.DataThrowable.UniversalThrowable
@@ -14,6 +15,10 @@ fun <T> Response<T>.codeIn400s(): Boolean {
     return this.code() in 400..499
 }
 
+fun <T> Response<T>.codeIn500s(): Boolean {
+    return this.code() in 500..599
+}
+
 fun <T> Call<T>.fetchResponse(
     onSuccess: (T) -> Unit,
     onFailure: (Throwable) -> Unit,
@@ -22,10 +27,12 @@ fun <T> Call<T>.fetchResponse(
         object : Callback<T> {
             override fun onResponse(call: Call<T>, response: Response<T>) {
                 if (response.isSuccessful) {
-                    // TODO : body가 없는 경우 뱉어낼 에러 정하기 & 500번 에러도 고민
-                    // TODO : 실패로 던져주고 처리는 뷰에서 진행한다.
-                    val body = response.body() ?: return
+                    val body = response.body() ?: return onFailure(IllegalStateThrowable())
                     onSuccess(body)
+                }
+
+                if (response.codeIn500s()) {
+                    return onFailure(IllegalStateThrowable())
                 }
 
                 if (response.codeIn400s()) {
@@ -35,22 +42,27 @@ fun <T> Call<T>.fetchResponse(
                     val message = jsonObject?.getString("message") ?: ""
 
                     when (code) {
-                        in 100 until 200 -> {
+                        in 100..199 -> {
                             onFailure(AuthorizationThrowable(code, message))
                         }
-                        in 200 until 300 -> {
+
+                        in 200..299 -> {
                             onFailure(UniversalThrowable(code, message))
                         }
-                        in 300 until 400 -> {
+
+                        in 300..399 -> {
                             onFailure(PlayerThrowable(code, message))
                         }
-                        in 400 until 500 -> {
+
+                        in 400..499 -> {
                             onFailure(GameThrowable(code, message))
                         }
-                        in 500 until 600 -> {
+
+                        in 500..599 -> {
                             onFailure(PlaceThrowable(code, message))
                         }
-                        in 900 until 1000 -> {
+
+                        in 900..999 -> {
                             onFailure(UniversalThrowable(code, message))
                         }
                     }
@@ -58,7 +70,7 @@ fun <T> Call<T>.fetchResponse(
             }
 
             override fun onFailure(call: Call<T>, t: Throwable) {
-                onFailure(t)
+                onFailure(IllegalStateThrowable())
             }
         },
     )
