a6366dda759c73d9fcb5c52a28aba51f413d07a8
krrong <khk9664@naver.com>
2023-08-09 15:03:22 +0900

    feat: 통신 오류시 DataThrowable을 사용하는 코드 스니펫 작성

--- android/app/src/main/java/com/now/naaga/data/remote/retrofit/RetrofitUtil.kt
+++ android/app/src/main/java/com/now/naaga/data/remote/retrofit/RetrofitUtil.kt
@@ -7,6 +7,12 @@ import com.now.naaga.data.NaagaThrowable
 import com.now.naaga.data.NaagaThrowable.NaagaUnknownError
 import com.now.naaga.data.NaagaThrowable.ServerConnectFailure
 import com.now.naaga.data.remote.dto.FailureDto
+import com.now.naaga.data.throwable.DataThrowable.AuthorizationThrowable
+import com.now.naaga.data.throwable.DataThrowable.GameThrowable
+import com.now.naaga.data.throwable.DataThrowable.PlaceThrowable
+import com.now.naaga.data.throwable.DataThrowable.PlayerThrowable
+import com.now.naaga.data.throwable.DataThrowable.UniversalThrowable
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -29,6 +35,56 @@ fun <T> Response<T>.getFailureDto(): FailureDto {
     val jsonObject: JsonObject = JsonParser.parseString(errorString).asJsonObject
     val failureDto: FailureDto = Gson().fromJson(jsonObject, FailureDto::class.java)
     return failureDto
+}
+
+fun <T> Call<T>.fetchResponse(
+    onSuccess: (T) -> Unit,
+    onFailure: (Throwable) -> Unit,
+) {
+    enqueue(
+        object : Callback<T> {
+            override fun onResponse(call: Call<T>, response: Response<T>) {
+                if (response.isSuccessful) {
+                    // TODO : body가 없는 경우 뱉어낼 에러 정하기 & 500번 에러도 고민
+                    // TODO : 실패로 던져주고 처리는 뷰에서 진행한다.
+                    val body = response.body() ?: return
+                    onSuccess(body)
+                }
+
+                if (response.code() == 400) {
+                    val errorResponse = response.errorBody()?.string()
+                    val jsonObject = errorResponse?.let { JSONObject(it) }
+                    val code = jsonObject?.getInt("code") ?: 0
+                    val message = jsonObject?.getString("message") ?: ""
+
+                    when (code) {
+                        in 100 until 200 -> {
+                            onFailure(AuthorizationThrowable(code, message))
+                        }
+                        in 200 until 300 -> {
+                            onFailure(UniversalThrowable(code, message))
+                        }
+                        in 300 until 400 -> {
+                            onFailure(PlayerThrowable(code, message))
+                        }
+                        in 400 until 500 -> {
+                            onFailure(GameThrowable(code, message))
+                        }
+                        in 500 until 600 -> {
+                            onFailure(PlaceThrowable(code, message))
+                        }
+                        in 900 until 1000 -> {
+                            onFailure(UniversalThrowable(code, message))
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<T>, t: Throwable) {
+                onFailure(t)
+            }
+        },
+    )
 }
 
 fun <T> Call<T>.fetchNaagaNullableResponse(
