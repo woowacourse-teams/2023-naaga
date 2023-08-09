8edde361d8a63f1feeb1c1ec2b0611f209060bc4
briandr97 <briandr@naver.com>
2023-08-10 03:54:45 +0900

    feat: AuthRepository 수정, 데이터 레이어에서 토큰을 저장할 수 있도록 변경

--- android/app/src/main/java/com/now/naaga/data/repository/DefaultAuthRepository.kt
+++ android/app/src/main/java/com/now/naaga/data/repository/DefaultAuthRepository.kt
@@ -1,9 +1,8 @@
 package com.now.naaga.data.repository
 
-import com.now.domain.model.NaagaAuth
 import com.now.domain.model.PlatformAuth
 import com.now.domain.repository.AuthRepository
-import com.now.naaga.data.mapper.toDomain
+import com.now.naaga.NaagaApplication
 import com.now.naaga.data.mapper.toDto
 import com.now.naaga.data.remote.retrofit.ServicePool
 import com.now.naaga.data.remote.retrofit.fetchNaagaResponse
@@ -11,11 +10,15 @@ import com.now.naaga.data.remote.retrofit.fetchNaagaResponse
 class DefaultAuthRepository : AuthRepository {
     override fun getToken(
         platformAuth: PlatformAuth,
-        callback: (Result<NaagaAuth>) -> Unit,
+        callback: (Result<Boolean>) -> Unit,
     ) {
         val call = ServicePool.authService.requestToken(platformAuth.toDto())
         call.fetchNaagaResponse(
-            onSuccess = { callback(Result.success(it.toDomain())) },
+            onSuccess = { naagaAuthDto ->
+                NaagaApplication.authDataSource.setAccessToken(naagaAuthDto.accessToken)
+                NaagaApplication.authDataSource.setRefreshToken(naagaAuthDto.refreshToken)
+                callback(Result.success(true))
+            },
             onFailure = { callback(Result.failure(it)) },
         )
     }
