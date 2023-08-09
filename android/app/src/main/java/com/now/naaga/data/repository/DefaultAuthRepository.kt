3f44d684956bf93cd9a26493437cda78fa754eb2
briandr97 <briandr@naver.com>
2023-08-10 05:53:04 +0900

    refactor: DefaultAuthRepository에서 dataSource를 외부에서 주입받도록 변경

--- android/app/src/main/java/com/now/naaga/data/repository/DefaultAuthRepository.kt
+++ android/app/src/main/java/com/now/naaga/data/repository/DefaultAuthRepository.kt
@@ -2,21 +2,21 @@ package com.now.naaga.data.repository
 
 import com.now.domain.model.PlatformAuth
 import com.now.domain.repository.AuthRepository
-import com.now.naaga.NaagaApplication
+import com.now.naaga.data.local.AuthDataSource
 import com.now.naaga.data.mapper.toDto
 import com.now.naaga.data.remote.retrofit.ServicePool
 import com.now.naaga.data.remote.retrofit.fetchNaagaResponse
 
-class DefaultAuthRepository : AuthRepository {
+class DefaultAuthRepository(private val authDataSource: AuthDataSource) : AuthRepository {
     override fun getToken(
         platformAuth: PlatformAuth,
         callback: (Result<Boolean>) -> Unit,
     ) {
-        val call = ServicePool.authService.requestToken(platformAuth.toDto())
+        val call = ServicePool.authService.requestAuth(platformAuth.toDto())
         call.fetchNaagaResponse(
             onSuccess = { naagaAuthDto ->
-                NaagaApplication.authDataSource.setAccessToken(naagaAuthDto.accessToken)
-                NaagaApplication.authDataSource.setRefreshToken(naagaAuthDto.refreshToken)
+                authDataSource.setAccessToken(naagaAuthDto.accessToken)
+                authDataSource.setRefreshToken(naagaAuthDto.refreshToken)
                 callback(Result.success(true))
             },
             onFailure = { callback(Result.failure(it)) },
