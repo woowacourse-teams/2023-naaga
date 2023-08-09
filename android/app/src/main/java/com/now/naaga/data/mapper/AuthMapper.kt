32a321a4e30e58e64d72ceca288d6c524d851155
briandr97 <briandr@naver.com>
2023-08-10 03:45:15 +0900

    feat: 도메인에서 엑세스 토큰과 리프레시 토큰에 대한 내용은 필요가 없다고 생각되어 삭제

--- android/app/src/main/java/com/now/naaga/data/mapper/AuthMapper.kt
+++ android/app/src/main/java/com/now/naaga/data/mapper/AuthMapper.kt
@@ -1,9 +1,7 @@
 package com.now.naaga.data.mapper
 
 import com.now.domain.model.AuthPlatformType
-import com.now.domain.model.NaagaAuth
 import com.now.domain.model.PlatformAuth
-import com.now.naaga.data.remote.dto.NaagaAuthDto
 import com.now.naaga.data.remote.dto.PlatformAuthDto
 
 fun PlatformAuthDto.toDomain(): PlatformAuth {
@@ -12,12 +10,4 @@ fun PlatformAuthDto.toDomain(): PlatformAuth {
 
 fun PlatformAuth.toDto(): PlatformAuthDto {
     return PlatformAuthDto(token, type.name)
-}
-
-fun NaagaAuthDto.toDomain(): NaagaAuth {
-    return NaagaAuth(accessToken, refreshToken)
-}
-
-fun NaagaAuth.toDto(): NaagaAuthDto {
-    return NaagaAuthDto(accessToken, refreshToken)
 }
