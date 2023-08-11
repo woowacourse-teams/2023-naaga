b551e21327358d842ebe0c12b195f0324c1c665c
briandr97 <briandr@naver.com>
2023-08-11 15:26:02 +0900

    feat: 다크모드를 사용해도 다크테마가 나오지 않도록 변경

--- android/app/src/main/java/com/now/naaga/NaagaApplication.kt
+++ android/app/src/main/java/com/now/naaga/NaagaApplication.kt
@@ -1,6 +1,7 @@
 package com.now.naaga
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.kakao.sdk.common.KakaoSdk
 import com.now.naaga.data.local.AuthDataSource
 import com.now.naaga.data.local.DefaultAuthDataSource
@@ -11,6 +12,11 @@ class NaagaApplication : Application() {
         super.onCreate()
         initKakaoSdk()
         initDataSources()
+        disableDarkMode()
+    }
+
+    private fun disableDarkMode() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 
     private fun initKakaoSdk() {
