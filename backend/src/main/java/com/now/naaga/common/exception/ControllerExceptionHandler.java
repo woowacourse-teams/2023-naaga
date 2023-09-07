cea0006cf038533aa45961c107eed8e001b480a5
chaewon121 <cw121@naver.com>
2023-09-07 20:08:21 +0900

    feat: exceptionHandler 수정

--- backend/src/main/java/com/now/naaga/common/exception/ControllerExceptionHandler.java
+++ backend/src/main/java/com/now/naaga/common/exception/ControllerExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.now.naaga.common.exception;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -11,43 +11,45 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
+    public ControllerExceptionHandler() {
+    }
+
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ExceptionResponse> handleBaseException(final BaseException e) {
         final BaseExceptionType baseExceptionType = e.exceptionType();
         final ExceptionResponse exceptionResponse = new ExceptionResponse(baseExceptionType.errorCode(), baseExceptionType.errorMessage());
-        log.warn("error = {}", exceptionResponse, e);
+
+        MDC.put("jsonResponse", exceptionResponse.toString());
         return ResponseEntity.status(baseExceptionType.httpStatus()).body(exceptionResponse);
     }
 
     @ExceptionHandler({
             HttpMessageNotReadableException.class,
             MethodArgumentNotValidException.class,
-            HttpMediaTypeNotSupportedException.class
-    })
+            HttpMediaTypeNotSupportedException.class})
     public ResponseEntity<ExceptionResponse> handleTypeMismatchException(final Exception e) {
         final CommonExceptionType commonExceptionType = CommonExceptionType.INVALID_REQUEST_BODY;
         final ExceptionResponse exceptionResponse = new ExceptionResponse(commonExceptionType.errorCode(), commonExceptionType.errorMessage());
-        log.warn("error = {}", exceptionResponse, e);
+
+        log.warn("error = {}", exceptionResponse);
+
         return ResponseEntity.status(commonExceptionType.httpStatus()).body(exceptionResponse);
     }
 
     @ExceptionHandler(InternalException.class)
-    public ResponseEntity<ExceptionResponse> handleInternalException(final InternalException e){
+    public ResponseEntity<ExceptionResponse> handleInternalException(final InternalException e) {
         final BaseExceptionType internalExceptionType = e.exceptionType();
 
-//        log.error("errorCode = {} \n message = {}",
-//                internalExceptionType.errorCode(),
-//                internalExceptionType.errorMessage());
-
-        log.error("errorCode = {} \n message = {} \n error = {}",
-                internalExceptionType.errorCode(),
-                internalExceptionType.errorMessage(),
-                 e.getMessage() , e);
+        log.error("error = {}\nstacktrace = {}",
+                e.toString(), getStackTraceAsString(e));
 
         final ExceptionResponse exceptionResponse = new ExceptionResponse(10000, "예기치 못한 오류입니다");
         return ResponseEntity.status(internalExceptionType.httpStatus())
@@ -55,12 +57,21 @@ public class ControllerExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ExceptionResponse> handleException(final Exception e){
-        log.error("error = {}"+ e.getMessage() , e);
+    public ResponseEntity<ExceptionResponse> handleException(final Exception e) {
+
+        log.error("error = {}\nstacktrace = {}",
+                e.toString(), getStackTraceAsString(e));
 
         final ExceptionResponse exceptionResponse = new ExceptionResponse(10000, "예기치 못한 오류입니다");
         e.printStackTrace();
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(exceptionResponse);
+    }
+
+    private String getStackTraceAsString(Exception e) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        return sw.toString();
     }
 }
