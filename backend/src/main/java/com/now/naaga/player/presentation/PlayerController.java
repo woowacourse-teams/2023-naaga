0be774ebf061fe7531d60d7ea1c6006f521e31c1
chaewon121 <cw121@naver.com>
2023-09-14 01:10:11 +0900

    refactor: 추가 메서드 삭제

--- backend/src/main/java/com/now/naaga/player/presentation/PlayerController.java
+++ backend/src/main/java/com/now/naaga/player/presentation/PlayerController.java
@@ -2,17 +2,15 @@ package com.now.naaga.player.presentation;
 
 import com.now.naaga.auth.presentation.annotation.Auth;
 import com.now.naaga.common.exception.CommonException;
-import com.now.naaga.member.domain.Member;
-import com.now.naaga.member.persistence.repository.MemberRepository;
 import com.now.naaga.player.application.PlayerService;
-import com.now.naaga.player.domain.Player;
 import com.now.naaga.player.domain.Rank;
-import com.now.naaga.player.persistence.repository.PlayerRepository;
 import com.now.naaga.player.presentation.dto.PlayerRequest;
 import com.now.naaga.player.presentation.dto.RankResponse;
-import com.now.naaga.score.domain.Score;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,13 +23,8 @@ public class PlayerController {
 
     private final PlayerService playerService;
 
-    private final MemberRepository memberRepository;
-    private final PlayerRepository playerRepository;
-
-    public PlayerController(final PlayerService playerService, final MemberRepository memberRepository, final PlayerRepository playerRepository) {
+    public PlayerController(final PlayerService playerService) {
         this.playerService = playerService;
-        this.memberRepository = memberRepository;
-        this.playerRepository = playerRepository;
     }
 
     @GetMapping("/my")
@@ -45,7 +38,7 @@ public class PlayerController {
     public ResponseEntity<List<RankResponse>> findAllRank(@RequestParam(name = "sort-by") final String sortBy,
                                                           @RequestParam(name = "order") final String order) {
         if (!sortBy.equalsIgnoreCase("RANK") || !order.equalsIgnoreCase("ASCENDING")) {
-            System.out.println("sortBy: "+sortBy);
+            System.out.println("sortBy: " + sortBy);
             throw new CommonException(INVALID_REQUEST_PARAMETERS);
         }
 
@@ -56,11 +49,4 @@ public class PlayerController {
         return ResponseEntity.ok(rankResponseList);
     }
 
-    @PostMapping("/save")
-    public void insertMember(@RequestParam(name = "email") final String email) {
-        final Member member = new Member(email);
-        Member member2 = memberRepository.save(member);
-        final Player player = new Player("채채",new Score(0),member2);
-        playerRepository.save(player);
-    }
 }
