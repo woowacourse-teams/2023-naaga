a9f424a94cdec437842de8946dd80893c95306b3
hyunji <83613106+hyunji1203@users.noreply.github.com>
2023-09-15 07:27:14 +0900

    refactor: Login 뷰에서 진행하는 네트워크 작업 코루틴으로 변경 (#328)
    * feat: service와 repository에 suspend 추가
    
    * refactor: repository 내 코루틴 사용
    
    * refactor: repository 생성 시 Dispatcher 전해주도록 수정
    
    * refactor: 코루틴 사용
    
    * refactor: 불필요한 인자 제거
    
    * refactor: 불필요한 로그 제거
    
    * refactor: 불필요한 import 제거

--- android/app/src/main/java/com/now/naaga/presentation/login/LoginViewModel.kt
+++ android/app/src/main/java/com/now/naaga/presentation/login/LoginViewModel.kt
@@ -3,10 +3,12 @@ package com.now.naaga.presentation.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.now.domain.model.AuthPlatformType
 import com.now.domain.model.PlatformAuth
 import com.now.domain.repository.AuthRepository
 import com.now.naaga.data.throwable.DataThrowable
+import kotlinx.coroutines.launch
 
 class LoginViewModel(
     private val authRepository: AuthRepository,
@@ -18,10 +20,14 @@ class LoginViewModel(
     val throwable: LiveData<DataThrowable> = _throwable
 
     fun signIn(token: String, platformType: AuthPlatformType) {
-        authRepository.getToken(PlatformAuth(token, platformType)) { result ->
-            result
-                .onSuccess { _isLoginSucceed.value = it }
-                .onFailure { _throwable.value = it as DataThrowable }
+        viewModelScope.launch {
+            runCatching {
+                authRepository.getToken(PlatformAuth(token, platformType))
+            }.onSuccess { status ->
+                _isLoginSucceed.value = status
+            }.onFailure {
+                _throwable.value = it as DataThrowable
+            }
         }
     }
 }
