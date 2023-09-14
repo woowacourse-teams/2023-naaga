a0b45f0ffc780a645d4af92f76f042f9dab11fe5
briandr97 <briandr@naver.com>
2023-09-14 17:35:49 +0900

    feat: 토큰 refresh 요청을 위한 레포지토리 로직 작성

--- android/app/src/main/java/com/now/naaga/data/repository/DefaultAuthRepository.kt
+++ android/app/src/main/java/com/now/naaga/data/repository/DefaultAuthRepository.kt
@@ -4,8 +4,10 @@ import com.now.domain.model.PlatformAuth
 import com.now.domain.repository.AuthRepository
 import com.now.naaga.data.local.AuthDataSource
 import com.now.naaga.data.mapper.toDto
+import com.now.naaga.data.remote.dto.RefreshTokenDto
 import com.now.naaga.data.remote.retrofit.ServicePool
 import com.now.naaga.data.remote.retrofit.fetchResponse
+import com.now.naaga.data.throwable.DataThrowable
 
 class DefaultAuthRepository(private val authDataSource: AuthDataSource) : AuthRepository {
     override fun getToken(
@@ -13,6 +15,20 @@ class DefaultAuthRepository(private val authDataSource: AuthDataSource) : AuthRe
         callback: (Result<Boolean>) -> Unit,
     ) {
         val call = ServicePool.authService.requestAuth(platformAuth.toDto())
+        call.fetchResponse(
+            onSuccess = { naagaAuthDto ->
+                authDataSource.setAccessToken(naagaAuthDto.accessToken)
+                authDataSource.setRefreshToken(naagaAuthDto.refreshToken)
+                callback(Result.success(true))
+            },
+            onFailure = { callback(Result.failure(it)) },
+        )
+    }
+
+    override fun refreshToken(callback: (Result<Boolean>) -> Unit) {
+        val refreshToken: String = authDataSource.getRefreshToken()
+            ?: return callback(Result.failure(DataThrowable.AuthorizationThrowable(101, "로컬에 리프레시 토큰이 없습니다.")))
+        val call = ServicePool.authService.refreshAuth(RefreshTokenDto(refreshToken))
         call.fetchResponse(
             onSuccess = { naagaAuthDto ->
                 authDataSource.setAccessToken(naagaAuthDto.accessToken)
