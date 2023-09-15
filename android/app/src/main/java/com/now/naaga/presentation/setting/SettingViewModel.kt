9b4d4399a963fb7d241a1b2b630e5f9904c37112
krrong <khk9664@naver.com>
2023-09-16 00:50:29 +0900

    feat: 로그인 상태, 에러를 관리하는 라이브데이터 추가

--- android/app/src/main/java/com/now/naaga/presentation/setting/SettingViewModel.kt
+++ android/app/src/main/java/com/now/naaga/presentation/setting/SettingViewModel.kt
@@ -1,22 +1,40 @@
 package com.now.naaga.presentation.setting
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.now.domain.repository.AuthRepository
 import com.now.naaga.NaagaApplication.DependencyContainer.authDataSource
 import com.now.naaga.data.repository.DefaultAuthRepository
+import com.now.naaga.data.throwable.DataThrowable
+import com.now.naaga.data.throwable.DataThrowable.AuthorizationThrowable
 import kotlinx.coroutines.launch
 
 class SettingViewModel(private val authRepository: AuthRepository) : ViewModel() {
+    private val _isLoggedIn = MutableLiveData(true)
+    val isLoggedIn: LiveData<Boolean> = _isLoggedIn
+
+    private val _throwable = MutableLiveData<DataThrowable>()
+    val throwable: LiveData<DataThrowable> = _throwable
 
     fun logout() {
         viewModelScope.launch {
             runCatching {
                 authRepository.logout()
             }.onSuccess {
+                _isLoggedIn.value = false
             }.onFailure {
+                setError(it as DataThrowable)
             }
+        }
+    }
+
+    private fun setError(throwable: DataThrowable) {
+        when (throwable) {
+            is AuthorizationThrowable -> _throwable.value = throwable
+            else -> {}
         }
     }
 
