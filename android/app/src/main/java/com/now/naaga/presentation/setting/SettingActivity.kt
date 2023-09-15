c8c99eed90f158c556b985b05e757d8f7d6d189d
krrong <khk9664@naver.com>
2023-09-16 00:50:41 +0900

    feat: 뷰모델 데이터 구독 및 적절한 처리 로직 구현

--- android/app/src/main/java/com/now/naaga/presentation/setting/SettingActivity.kt
+++ android/app/src/main/java/com/now/naaga/presentation/setting/SettingActivity.kt
@@ -3,8 +3,10 @@ package com.now.naaga.presentation.setting
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.now.naaga.R
 import com.now.naaga.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
@@ -16,12 +18,29 @@ class SettingActivity : AppCompatActivity() {
         binding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initViewModel()
+        subscribe()
         setClickListeners()
     }
 
     private fun initViewModel() {
         viewModel = ViewModelProvider(this, SettingViewModel.Factory)[SettingViewModel::class.java]
         binding.lifecycleOwner = this
+    }
+
+    private fun subscribe() {
+        viewModel.isLoggedIn.observe(this) { isLoggedIn ->
+            if (!isLoggedIn) {
+                shortToast(getString(R.string.setting_logout_message))
+                finish()
+            }
+        }
+        viewModel.throwable.observe(this) {
+            shortToast(it.message.toString())
+        }
+    }
+
+    private fun shortToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     private fun setClickListeners() {
