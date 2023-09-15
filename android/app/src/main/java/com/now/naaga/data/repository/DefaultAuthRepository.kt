bb4f3acf49ae1d7352678fb53bed65f6478ddbaa
hyunji1203 <hj27720@gmail.com>
2023-09-15 22:14:49 +0900

    feat: 회원 탈퇴 기능 추기

--- android/app/src/main/java/com/now/naaga/data/repository/DefaultAuthRepository.kt
+++ android/app/src/main/java/com/now/naaga/data/repository/DefaultAuthRepository.kt
@@ -1,11 +1,13 @@
 package com.now.naaga.data.repository
 
+import android.util.Log
 import com.now.domain.model.PlatformAuth
 import com.now.domain.repository.AuthRepository
 import com.now.naaga.data.local.AuthDataSource
 import com.now.naaga.data.mapper.toDto
 import com.now.naaga.data.remote.retrofit.ServicePool.authService
 import com.now.naaga.util.getValueOrThrow
+import com.now.naaga.util.unlinkWithKakao
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -24,6 +26,23 @@ class DefaultAuthRepository(
                 authDataSource.setRefreshToken(naagaAuthDto.refreshToken)
                 return@withContext true
             }
+            return@withContext false
+        }
+    }
+
+    override suspend fun withdrawalMember(): Boolean {
+        return withContext(dispatcher) {
+            runCatching {
+                authService.withdrawalMember()
+                Log.d("test", "회원탈퇴 성공")
+                return@withContext true
+            }.onSuccess {
+                unlinkWithKakao()
+                Log.d("test", "카카오 회원탈퇴 성공")
+            }.onFailure {
+                Log.d("test", "${it.message}, ${it}")
+            }
+            Log.d("test", "회원탈퇴 실패")
             return@withContext false
         }
     }
