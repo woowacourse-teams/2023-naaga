6a084535123167b8c22b77dc884a428c2552e39a
hyunji1203 <hj27720@gmail.com>
2023-09-15 10:38:38 +0900

    feat: Setting 뷰 구현

---
+++ android/app/src/main/java/com/now/naaga/presentation/setting/SettingActivity.kt
@@ -0,0 +1,31 @@
+package com.now.naaga.presentation.setting
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.now.naaga.databinding.ActivitySettingBinding
+
+class SettingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingBinding
+    private lateinit var viewModel: SettingViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProvider(this, SettingViewModel.Factory)[SettingViewModel::class.java]
+        binding.lifecycleOwner = this
+    }
+
+    companion object {
+        fun getIntent(context: Context): Intent {
+            return Intent(context, SettingActivity::class.java)
+        }
+    }
+}
