67feac43e7222166aa265b9957b93dd92f4f9ccf
hyunji1203 <hj27720@gmail.com>
2023-09-16 17:43:38 +0900

    feat: 내 랭킹을 조회해오는 메서드 테스트 추가

---
+++ android/app/src/test/java/com/now/naaga/RankViewModelTest.kt
@@ -0,0 +1,99 @@
+package com.now.naaga
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.now.domain.model.Player
+import com.now.domain.model.Rank
+import com.now.domain.repository.RankRepository
+import com.now.naaga.presentation.rank.RankViewModel
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class RankViewModelTest {
+    private lateinit var vm: RankViewModel
+    private lateinit var rankRepository: RankRepository
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private val fakeMyLank = Rank(
+        Player(
+            id = 1,
+            nickname = "뽀또",
+            score = 1000,
+        ),
+        rank = 1,
+        percent = 1,
+    )
+
+    private val fakeRanksList = listOf(
+        Rank(
+            player = Player(
+                id = 1,
+                nickname = "뽀또",
+                score = 1000,
+            ),
+            rank = 1,
+            percent = 1,
+        ),
+        Rank(
+            player = Player(
+                id = 2,
+                nickname = "뽀또2",
+                score = 1100,
+            ),
+            rank = 2,
+            percent = 2,
+        ),
+        Rank(
+            player = Player(
+                id = 3,
+                nickname = "뽀또3",
+                score = 1200,
+            ),
+            rank = 3,
+            percent = 3,
+        ),
+    )
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Before
+    fun setup() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+        rankRepository = mockk()
+        vm = RankViewModel(rankRepository)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `내 랭킹 조회`() {
+        // given
+        coEvery {
+            rankRepository.getMyRank()
+        } coAnswers {
+            fakeMyLank
+        }
+
+        // when
+        vm.fetchMyRank()
+
+        // then
+        assertEquals(vm.myRank.getOrAwaitValue(), fakeMyLank.rank)
+        assertEquals(vm.myName.getOrAwaitValue(), fakeMyLank.player.nickname)
+        assertEquals(vm.myScore.getOrAwaitValue(), fakeMyLank.player.score)
+    }
+}
