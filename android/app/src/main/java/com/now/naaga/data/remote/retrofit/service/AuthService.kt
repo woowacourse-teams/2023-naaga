5c0017f263a89a31189c377a314ce0530023b8ac
hyunji1203 <hj27720@gmail.com>
2023-09-16 22:54:30 +0900

    refactor: 메서드 반환 값이 non-nullable 하도록 수정

--- android/app/src/main/java/com/now/naaga/data/remote/retrofit/service/AuthService.kt
+++ android/app/src/main/java/com/now/naaga/data/remote/retrofit/service/AuthService.kt
@@ -14,5 +14,5 @@ interface AuthService {
     ): Response<NaagaAuthDto>
 
     @DELETE("/auth/unlink")
-    suspend fun withdrawalMember(): Response<Unit>?
+    suspend fun withdrawalMember(): Response<Unit>
 }
