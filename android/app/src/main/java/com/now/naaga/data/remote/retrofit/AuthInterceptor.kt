f0919b006833dcd9b167cad7cb8a8eb44d7c99da
briandr97 <briandr@naver.com>
2023-09-18 15:08:21 +0900

    feat: response의 바디를 한번 확인하면 다시 사용할 수 없는 (closed 이슈)를 해결

--- android/app/src/main/java/com/now/naaga/data/remote/retrofit/AuthInterceptor.kt
+++ android/app/src/main/java/com/now/naaga/data/remote/retrofit/AuthInterceptor.kt
@@ -1,7 +1,6 @@
 package com.now.naaga.data.remote.retrofit
 
 import android.content.Intent
-import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.JsonParser
 import com.now.naaga.BuildConfig
@@ -30,15 +29,18 @@ class AuthInterceptor() : Interceptor {
         }
 
         val headerAddedRequest = chain.request().newBuilder().addHeader(AUTH_KEY, getAccessToken()).build()
-        val response = chain.proceed(headerAddedRequest)
+        val response: Response = chain.proceed(headerAddedRequest)
 
         if (response.code == 401) {
-            val failureDto = response.getDto<FailureDto>()
-            if (failureDto.code == 102) {
-                return reRequest(chain).also { if (it != null) response.close() } ?: return response
-            } else {
+            runCatching {
+                reRequest(chain)
+            }.onSuccess { newResponse: Response? ->
+                if (newResponse != null) {
+                    response.close()
+                    return newResponse
+                }
+            }.onFailure {
                 startLoginActivity()
-                Log.e("[ERROR] Auth Interceptor", "$failureDto")
             }
         }
         return response
@@ -88,7 +90,12 @@ class AuthInterceptor() : Interceptor {
     }
 
     private fun getAccessToken(): String {
-        return requireNotNull(NaagaApplication.authDataSource.getAccessToken()) { NO_ACCESS_TOKEN }
+        val accessToken = NaagaApplication.authDataSource.getAccessToken()
+        if (accessToken == null) {
+            startLoginActivity()
+            return ""
+        }
+        return accessToken
     }
 
     private fun getRefreshToken(): String {
@@ -119,9 +126,6 @@ class AuthInterceptor() : Interceptor {
         const val AUTH_PATH = "auth"
         const val AUTH_REFRESH_PATH = "auth/refresh"
 
-        const val AUTH_ERROR = "%s에서 인증 오류 발생"
-        const val REFRESH_EXPIRED = "리프레시 토큰 만료"
-        const val NO_ACCESS_TOKEN = "엑세스 토큰이 없습니다"
         const val NO_REFRESH_TOKEN = "리프레시 토큰이 없습니다"
         const val REFRESH_FAILURE = "토큰 리프레시 실패"
     }
