8dab436a9a29dcdd394765dc1530c63e628d3fdc
chaewon121 <80631952+chaewon121@users.noreply.github.com>
2023-09-27 18:05:54 +0900

    fix: 트렌젝셔널 추가

--- backend/src/test/java/com/now/naaga/gameresult/application/GameResultServiceTest.java
+++ backend/src/test/java/com/now/naaga/gameresult/application/GameResultServiceTest.java
@@ -21,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
