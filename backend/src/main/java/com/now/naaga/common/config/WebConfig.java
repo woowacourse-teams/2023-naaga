4840f55aaf9eee05942c5957bdf97989615d209e
dooboocookie <bkh3030@gmail.com>
2023-10-01 02:04:37 +0900

    feat: manager 페이지에 대해서 CORS 설정

--- backend/src/main/java/com/now/naaga/common/config/WebConfig.java
+++ backend/src/main/java/com/now/naaga/common/config/WebConfig.java
@@ -7,10 +7,13 @@ import java.util.List;
 
 import com.now.naaga.auth.presentation.argumentresolver.MemberAuthArgumentResolver;
 import com.now.naaga.common.presentation.interceptor.PathMatcherInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.util.PathMatcher;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,6 +27,9 @@ public class WebConfig implements WebMvcConfigurer {
     private final AuthInterceptor authInterceptor;
 
     private final PathMatcher pathMatcher;
+
+    @Value("${manager.origin-url}")
+    private String managerUriPath;
 
     public WebConfig(final PlayerArgumentResolver playerArgumentResolver,
                      final MemberAuthArgumentResolver memberAuthArgumentResolver,
@@ -50,12 +56,23 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludeRequestPattern("/**/*.jpeg")
                 .excludeRequestPattern("/**/*.gif")
                 .excludeRequestPattern("/**/*.ico")
-                .excludeRequestPattern("/ranks");
+                .excludeRequestPattern("/ranks")
+                .excludeRequestPattern("/**", HttpMethod.OPTIONS);
     }
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(playerArgumentResolver);
         resolvers.add(memberAuthArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(managerUriPath)
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3000);
     }
 }
