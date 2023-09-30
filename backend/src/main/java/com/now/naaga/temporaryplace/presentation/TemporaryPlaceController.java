14f156d5a86f6a930d1069d5896547c725ce6434
kokodak <kokodakadokok@gmail.co>
2023-09-30 22:49:06 +0900

    feat: 장소 검수 컨트롤러 구현

---
+++ backend/src/main/java/com/now/naaga/temporaryplace/presentation/TemporaryPlaceController.java
@@ -0,0 +1,9 @@
+package com.now.naaga.temporaryplace.presentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/temporary-places")
+@RestController
+public class TemporaryPlaceController {
+}
