5ca6e74cbaec337fa24e037e3cf0bab2745399d5
dooboocookie <bkh3030@gmail.com>
2023-09-30 22:59:36 +0900

    refactor: 기존 AuthInterceptor 매핑 PathMatcherInterceptor로 변경

--- backend/src/main/java/com/now/naaga/common/config/WebConfig.java
+++ backend/src/main/java/com/now/naaga/common/config/WebConfig.java
@@ -6,8 +6,11 @@ import com.now.naaga.auth.presentation.argumentresolver.PlayerArgumentResolver;
 import java.util.List;
 
 import com.now.naaga.auth.presentation.argumentresolver.MemberAuthArgumentResolver;
+import com.now.naaga.common.presentation.interceptor.PathMatcherInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.PathMatcher;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,25 +18,39 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final PlayerArgumentResolver playerArgumentResolver;
+
     private final MemberAuthArgumentResolver memberAuthArgumentResolver;
+
     private final AuthInterceptor authInterceptor;
+
+    private final PathMatcher pathMatcher;
 
     public WebConfig(final PlayerArgumentResolver playerArgumentResolver,
                      final MemberAuthArgumentResolver memberAuthArgumentResolver,
-                     final AuthInterceptor authInterceptor) {
+                     final AuthInterceptor authInterceptor,
+                     final PathMatcher pathMatcher) {
         this.playerArgumentResolver = playerArgumentResolver;
         this.memberAuthArgumentResolver = memberAuthArgumentResolver;
         this.authInterceptor = authInterceptor;
+        this.pathMatcher = pathMatcher;
     }
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(authInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/h2-console/**")
-                .excludePathPatterns("/auth/**")
-                .excludePathPatterns("/**/*.png", "/**/*.jpg", "/**/*.jpeg", "/**/*.gif", "/**/*.ico")
-                .excludePathPatterns("/ranks");
+        registry.addInterceptor(mapAuthInterceptor());
+    }
+
+    private HandlerInterceptor mapAuthInterceptor() {
+        return new PathMatcherInterceptor(authInterceptor, pathMatcher)
+                .includeRequestPattern("/**")
+                .excludeRequestPattern("/h2-console/**")
+                .excludeRequestPattern("/auth/**")
+                .excludeRequestPattern("/**/*.png")
+                .excludeRequestPattern("/**/*.jpg")
+                .excludeRequestPattern("/**/*.jpeg")
+                .excludeRequestPattern("/**/*.gif")
+                .excludeRequestPattern("/**/*.ico")
+                .excludeRequestPattern("/ranks");
     }
 
     @Override
