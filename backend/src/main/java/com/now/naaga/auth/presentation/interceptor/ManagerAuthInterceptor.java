c884e131d0dcf26ec9ca10491e01acf41eaff0e4
dooboocookie <bkh3030@gmail.com>
2023-10-01 16:58:48 +0900

    refactor: Auth 헤더의 값이 id:password 들어오지 않을 때 검증 추가와 예외 정의

--- backend/src/main/java/com/now/naaga/auth/presentation/interceptor/ManagerAuthInterceptor.java
+++ backend/src/main/java/com/now/naaga/auth/presentation/interceptor/ManagerAuthInterceptor.java
@@ -1,5 +1,7 @@
 package com.now.naaga.auth.presentation.interceptor;
 
+import com.now.naaga.auth.exception.AuthException;
+import com.now.naaga.auth.exception.AuthExceptionType;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,8 +12,12 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import java.util.Base64;
 import java.util.Objects;
 
+import static com.now.naaga.auth.exception.AuthExceptionType.*;
+
 @Component
 public class ManagerAuthInterceptor implements HandlerInterceptor {
+
+    public static final int AUTH_HEADER_INFO_SIZE = 2;
 
     @Value("${manager.id}")
     private String id;
@@ -23,14 +29,29 @@ public class ManagerAuthInterceptor implements HandlerInterceptor {
     public boolean preHandle(final HttpServletRequest request,
                              final HttpServletResponse response,
                              final Object handler) throws Exception {
+        final String[] idAndPassword = extractHeaderInfo(request);
+        return loginForManager(idAndPassword);
+    }
+
+    private String[] extractHeaderInfo(final HttpServletRequest request) {
         final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (header == null) {
+            throw new AuthException(NOT_EXIST_HEADER);
+        }
         final String decodedHeader = new String(Base64.getDecoder().decode(header));
         final String[] idAndPassword = decodedHeader.split(":");
+        if (idAndPassword.length != AUTH_HEADER_INFO_SIZE) {
+            throw new AuthException(INVALID_HEADER);
+        }
+        return idAndPassword;
+    }
+
+    private boolean loginForManager(final String[] idAndPassword) {
         final String inputId = idAndPassword[0].trim();
         final String inputPassword = idAndPassword[1].trim();
         if (Objects.equals(id, inputId) && Objects.equals(password, inputPassword)) {
             return true;
         }
-        throw new IllegalArgumentException("허용되지 않은 관리자입니다");
+        throw new AuthException(INVALID_MANAGER);
     }
 }
