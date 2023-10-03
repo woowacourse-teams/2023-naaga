0c13a93888b660d21ba2dc7fc51c5af03d1f92ef
zillionme <beatus96@naver.com>
2023-10-04 06:35:29 +0900

    feat: 검수할 장소 등록 API 구현

--- backend/src/main/java/com/now/naaga/temporaryplace/presentation/TemporaryPlaceController.java
+++ backend/src/main/java/com/now/naaga/temporaryplace/presentation/TemporaryPlaceController.java
@@ -1,9 +1,40 @@
 package com.now.naaga.temporaryplace.presentation;
 
+import com.now.naaga.auth.presentation.annotation.Auth;
+import com.now.naaga.player.presentation.dto.PlayerRequest;
+import com.now.naaga.temporaryplace.application.TemporaryPlaceService;
+import com.now.naaga.temporaryplace.application.dto.CreateTemporaryPlaceCommand;
+import com.now.naaga.temporaryplace.domain.TemporaryPlace;
+import com.now.naaga.temporaryplace.presentation.dto.CreateTemporaryPlaceRequest;
+import com.now.naaga.temporaryplace.presentation.dto.TemporaryPlaceResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RequestMapping("/temporary-places")
 @RestController
 public class TemporaryPlaceController {
+
+    private final TemporaryPlaceService temporaryPlaceService;
+
+    public TemporaryPlaceController(final TemporaryPlaceService temporaryPlaceService) {
+        this.temporaryPlaceService = temporaryPlaceService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TemporaryPlaceResponse> createTemporaryPlace(@Auth final PlayerRequest playerRequest,
+                                                                       @ModelAttribute final CreateTemporaryPlaceRequest createTemporaryPlaceRequest) {
+        final CreateTemporaryPlaceCommand createTemporaryPlaceCommand = CreateTemporaryPlaceCommand.of(playerRequest, createTemporaryPlaceRequest);
+        final TemporaryPlace temporaryPlace = temporaryPlaceService.createTemporaryPlace(createTemporaryPlaceCommand);
+        final TemporaryPlaceResponse response = TemporaryPlaceResponse.from(temporaryPlace);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .location(URI.create("/temporary-places/" + temporaryPlace.getId()))
+                .body(response);
+    }
 }
---
+++ backend/src/main/java/com/now/naaga/temporaryplace/presentation/dto/TemporaryPlaceResponse.java
@@ -0,0 +1,33 @@
+package com.now.naaga.temporaryplace.presentation.dto;
+
+import com.now.naaga.game.presentation.dto.CoordinateResponse;
+import com.now.naaga.temporaryplace.domain.TemporaryPlace;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record TemporaryPlaceResponse(Long id,
+                                     String name,
+                                     CoordinateResponse coordinate,
+                                     String imageUrl,
+                                     String description
+
+) {
+
+    public static TemporaryPlaceResponse from(final TemporaryPlace savedTemporaryPlace) {
+        CoordinateResponse coordinateResponse = CoordinateResponse.of(savedTemporaryPlace.getPosition());
+        return new TemporaryPlaceResponse(
+                savedTemporaryPlace.getId(),
+                savedTemporaryPlace.getName(),
+                coordinateResponse,
+                savedTemporaryPlace.getImageUrl(),
+                savedTemporaryPlace.getDescription()
+        );
+    }
+
+    public static List<TemporaryPlaceResponse> convertToResponses(final List<TemporaryPlace> temporaryPlaces) {
+        return temporaryPlaces.stream()
+                .map(TemporaryPlaceResponse::from)
+                .collect(Collectors.toList());
+    }
+}
