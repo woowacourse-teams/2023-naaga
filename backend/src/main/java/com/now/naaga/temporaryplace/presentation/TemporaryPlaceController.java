71344d28c1bd4ae250d4d17b734b10740fb57d1d
kokodak <kokodakadokok@gmail.com>
2023-10-04 03:13:32 +0900

    feat: 검수 장소 삭제 API 구현

--- backend/src/main/java/com/now/naaga/temporaryplace/presentation/TemporaryPlaceController.java
+++ backend/src/main/java/com/now/naaga/temporaryplace/presentation/TemporaryPlaceController.java
@@ -1,9 +1,28 @@
 package com.now.naaga.temporaryplace.presentation;
 
+import com.now.naaga.temporaryplace.application.TemporaryPlaceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/temporary-places")
 @RestController
 public class TemporaryPlaceController {
+
+    private final TemporaryPlaceService temporaryPlaceService;
+
+    public TemporaryPlaceController(final TemporaryPlaceService temporaryPlaceService) {
+        this.temporaryPlaceService = temporaryPlaceService;
+    }
+
+    @DeleteMapping("/{temporaryPlaceId}")
+    public ResponseEntity<Void> deleteTemporaryPlace(@PathVariable final Long temporaryPlaceId) {
+        temporaryPlaceService.deleteById(temporaryPlaceId);
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+    }
 }
