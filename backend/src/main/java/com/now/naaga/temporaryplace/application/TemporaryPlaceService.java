7aa49e891c595432918febd0acdbdf98961f21e4
kokodak <kokodakadokok@gmail.com>
2023-10-03 23:29:42 +0900

    feat: 임시용 TemporaryPlaceService 생성

---
+++ backend/src/main/java/com/now/naaga/temporaryplace/application/TemporaryPlaceService.java
@@ -0,0 +1,13 @@
+package com.now.naaga.temporaryplace.application;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+public class TemporaryPlaceService {
+
+    public void deleteById(final Long id) {
+        return;
+    }
+}
