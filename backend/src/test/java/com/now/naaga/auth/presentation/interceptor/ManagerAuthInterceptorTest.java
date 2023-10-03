824bd540b8eea4c5ca57a36109b31ab8a6d795fd
kokodak <kokodakadokok@gmail.com>
2023-10-04 01:10:24 +0900

    fix: 잘못된 base64 디코딩 과정 수정

--- backend/src/test/java/com/now/naaga/auth/presentation/interceptor/ManagerAuthInterceptorTest.java
+++ backend/src/test/java/com/now/naaga/auth/presentation/interceptor/ManagerAuthInterceptorTest.java
@@ -36,12 +36,12 @@ class ManagerAuthInterceptorTest {
     @Test
     void Auth_헤더를_Base64_헤더를_디코딩해서_관리자_로그인을_처리한다() throws Exception {
         // given
-        final String s = "Basic "+id+":"+password;
+        final String s = id + ":" + password;
         final String authHeader = new String(Base64.getEncoder().encode(s.getBytes()));
         final MockHttpServletRequest request = new MockHttpServletRequest();
         final MockHttpServletResponse response = new MockHttpServletResponse();
         final Controller controller = Mockito.mock(Controller.class);
-        request.addHeader("Authorization", authHeader);
+        request.addHeader("Authorization", "Basic " + authHeader);
 
         // when
         final boolean expected = managerAuthInterceptor.preHandle(request, response, controller);
@@ -53,12 +53,12 @@ class ManagerAuthInterceptorTest {
     @Test
     void 잘못된_관리자_정보를_입력하면_예외를_발생한다() throws Exception {
         // given
-        final String s = "Basic wrongId:wrongPW";
+        final String s = "wrongId:wrongPW";
         final String authHeader = new String(Base64.getEncoder().encode(s.getBytes()));
         final MockHttpServletRequest request = new MockHttpServletRequest();
         final MockHttpServletResponse response = new MockHttpServletResponse();
         final Controller controller = Mockito.mock(Controller.class);
-        request.addHeader("Authorization", authHeader);
+        request.addHeader("Authorization", "Basic " + authHeader);
 
         // when & then
         assertThatThrownBy(() -> managerAuthInterceptor.preHandle(request, response, controller)).isInstanceOf(AuthException.class);
