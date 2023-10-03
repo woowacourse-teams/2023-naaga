ca9c631d5fd5b68bbbb17e95d622fbaafb16e423
krrong <khk9664@naver.com>
2023-10-03 23:07:21 +0900

    feat: 장소를 추가한 적 없는 유저에게는 다른 뷰가 보이도록 함

--- android/app/src/main/java/com/now/naaga/presentation/mypage/place/MyPageCustomGrid.kt
+++ android/app/src/main/java/com/now/naaga/presentation/mypage/place/MyPageCustomGrid.kt
@@ -5,22 +5,30 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.now.naaga.databinding.CustomMypageGridBinding
+import com.now.naaga.databinding.CustomMypageGridEmptyBinding
 import com.now.naaga.presentation.uimodel.model.MyPagePlaceUiModel
 
 class MyPageCustomGrid(context: Context, attrs: AttributeSet? = null) : ConstraintLayout(context, attrs) {
     private lateinit var layoutInflater: LayoutInflater
-    private lateinit var binding: CustomMypageGridBinding
+    private lateinit var placeBinding: CustomMypageGridBinding
+    private lateinit var emptyBinding: CustomMypageGridEmptyBinding
 
-    private fun initNotEmptyView() {
+    private fun initPlaceBinding() {
         layoutInflater = LayoutInflater.from(context)
-        binding = CustomMypageGridBinding.inflate(layoutInflater, this, true)
+        placeBinding = CustomMypageGridBinding.inflate(layoutInflater, this, true)
+    }
+
+    private fun initEmptyBinding() {
+        layoutInflater = LayoutInflater.from(context)
+        emptyBinding = CustomMypageGridEmptyBinding.inflate(layoutInflater, this, true)
     }
 
     fun initContent(data: List<MyPagePlaceUiModel>) {
         if (data.isNotEmpty()) {
-            initNotEmptyView()
-            val adapter = MyPagePlaceAdapter(data)
-            binding.rvMypageItemContent.adapter = adapter
+            initPlaceBinding()
+            placeBinding.rvMypageItemContent.adapter = MyPagePlaceAdapter(data)
+        } else {
+            initEmptyBinding()
         }
     }
 }
