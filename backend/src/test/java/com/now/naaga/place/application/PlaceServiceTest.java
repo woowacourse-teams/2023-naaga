23814c18af6365d808a192fdfdd8d821f5768c6d
kokodak <kokodakadokok@gmail.com>
2023-10-04 20:43:41 +0900

    test: 주변 반경 20M 내에 등록된 장소가 있을 때 예외 발생 확인을 위한 테스트 코드 추가

--- backend/src/test/java/com/now/naaga/place/application/PlaceServiceTest.java
+++ backend/src/test/java/com/now/naaga/place/application/PlaceServiceTest.java
@@ -2,13 +2,18 @@ package com.now.naaga.place.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import com.now.naaga.common.builder.PlaceBuilder;
 import com.now.naaga.common.builder.PlayerBuilder;
+import com.now.naaga.common.exception.BaseExceptionType;
 import com.now.naaga.place.application.dto.CreatePlaceCommand;
 import com.now.naaga.place.domain.Place;
 import com.now.naaga.place.domain.Position;
+import com.now.naaga.place.exception.PlaceException;
+import com.now.naaga.place.exception.PlaceExceptionType;
 import com.now.naaga.player.domain.Player;
 import com.now.naaga.temporaryplace.application.TemporaryPlaceService;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -33,6 +38,9 @@ class PlaceServiceTest {
 
     @Autowired
     private PlaceService placeService;
+
+    @Autowired
+    private PlaceBuilder placeBuilder;
 
     @Autowired
     private PlayerBuilder playerBuilder;
@@ -69,5 +77,31 @@ class PlaceServiceTest {
                               .isEqualTo(expected);
             verify(temporaryPlaceService, times(1)).deleteById(temporaryPlaceId);
         });
+    }
+
+    @Test
+    void 장소_등록_시_주변_반경_20M_내에_등록된_장소가_존재한다면_예외가_발생한다() {
+        // given
+        final Player player = playerBuilder.init()
+                                           .build();
+
+        placeBuilder.init()
+                    .position(Position.of(1.234567, 1.234567))
+                    .build();
+
+        final CreatePlaceCommand createPlaceCommand = new CreatePlaceCommand("루터회관",
+                                                                             "이곳은 루터회관이다 알겠냐",
+                                                                             Position.of(1.23456, 1.23456),
+                                                                             "image/url",
+                                                                             player.getId(),
+                                                                             1L);
+
+        // when
+        final BaseExceptionType baseExceptionType = assertThrows(PlaceException.class,
+                                                                 () -> placeService.createPlace(createPlaceCommand)
+                                                                ).exceptionType();
+
+        // then
+        assertThat(baseExceptionType).isEqualTo(PlaceExceptionType.ALREADY_EXIST_NEARBY);
     }
 }
