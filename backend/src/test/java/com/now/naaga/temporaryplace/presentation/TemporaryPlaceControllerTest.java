01ebc2c5f13e3a6fdb2e9525c8c9f0166d05912f
zillionme <beatus96@naver.com>
2023-10-04 12:42:17 +0900

    test: 검수할 장소 목록 조회 기능 테스트

--- backend/src/test/java/com/now/naaga/temporaryplace/presentation/TemporaryPlaceControllerTest.java
+++ backend/src/test/java/com/now/naaga/temporaryplace/presentation/TemporaryPlaceControllerTest.java
@@ -1,5 +1,8 @@
 package com.now.naaga.temporaryplace.presentation;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.now.naaga.auth.domain.AuthToken;
 import com.now.naaga.auth.infrastructure.AuthType;
 import com.now.naaga.auth.infrastructure.jwt.AuthTokenGenerator;
@@ -7,17 +10,14 @@ import com.now.naaga.common.CommonControllerTest;
 import com.now.naaga.common.builder.PlaceBuilder;
 import com.now.naaga.common.builder.PlayerBuilder;
 import com.now.naaga.common.builder.TemporaryPlaceBuilder;
-import com.now.naaga.common.exception.ExceptionResponse;
 import com.now.naaga.common.infrastructure.FileManager;
-import com.now.naaga.place.domain.Place;
 import com.now.naaga.player.domain.Player;
 import com.now.naaga.temporaryplace.domain.TemporaryPlace;
-import com.now.naaga.temporaryplace.exception.TemporaryPlaceException;
-import com.now.naaga.temporaryplace.exception.TemporaryPlaceExceptionType;
 import com.now.naaga.temporaryplace.presentation.dto.TemporaryPlaceResponse;
 import io.restassured.builder.MultiPartSpecBuilder;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -33,10 +33,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.math.RoundingMode;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
-import static com.now.naaga.common.fixture.PlaceFixture.PLACE;
-import static com.now.naaga.common.fixture.PositionFixture.서울_좌표;
-import static com.now.naaga.common.fixture.PositionFixture.잠실_루터회관_정문_좌표;
 import static com.now.naaga.common.fixture.TemporaryPlaceFixture.TEMPORARY_PLACE;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
@@ -60,6 +58,9 @@ class TemporaryPlaceControllerTest extends CommonControllerTest {
     @Autowired
     private AuthTokenGenerator authTokenGenerator;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private FileManager<MultipartFile> fileManager;
 
@@ -69,8 +70,13 @@ class TemporaryPlaceControllerTest extends CommonControllerTest {
     @Value("${manager.password}")
     private String password;
 
+    @BeforeEach
+    void setup() {
+        super.setUp();
+    }
+
     @Test
-    void 검수할_장소_등록_요청을_받으면_추가된_검수할_장소_정보를_응답한다() throws FileNotFoundException {
+    void 검수할_장소_등록_요청을_받으면_201_상태코드와_함께_추가된_검수할_장소_정보를_응답한다() throws FileNotFoundException {
         //given
         final Player player = playerBuilder.init()
                 .build();
@@ -118,56 +124,31 @@ class TemporaryPlaceControllerTest extends CommonControllerTest {
                     .ignoringFields("id", "imageUrl")
                     .isEqualTo(expected);
         });
-
     }
 
     @Test
-    void 검수할_장소_추가_요청_시_주변_20미터_내에_이미_등록된_목적지가_있다면_예외를_응답한다() throws FileNotFoundException {
+    void 검수할_장소_목록_조회_요청을_받으면_200_상태코드와_함께_검수할_장소_목록을_응답한다() throws FileNotFoundException, JsonProcessingException {
         //given
-        final Player player = playerBuilder.init()
+        final TemporaryPlace temporaryPlace = temporaryPlaceBuilder.init()
                 .build();
-        final Place place = placeBuilder.init()
-                .position(잠실_루터회관_정문_좌표)
-                .build();
-        final TemporaryPlace temporaryPlace = TEMPORARY_PLACE();
-
-        final AuthToken generate = authTokenGenerator.generate(player.getMember(), 1L, AuthType.KAKAO);
-        final String accessToken = generate.getAccessToken();
-        when(fileManager.save(any())).thenReturn(new File("/임시경로", "이미지.png"));
 
         //when
         final ExtractableResponse<Response> extract = given()
                 .log().all()
-                .multiPart(new MultiPartSpecBuilder(temporaryPlace.getName()).controlName("name").charset(StandardCharsets.UTF_8).build())
-                .multiPart(new MultiPartSpecBuilder(temporaryPlace.getDescription()).controlName("description").charset(StandardCharsets.UTF_8).build())
-                .multiPart(
-                        new MultiPartSpecBuilder(place.getPosition().getLatitude().setScale(6, RoundingMode.HALF_DOWN).doubleValue()).controlName(
-                                "latitude").charset(StandardCharsets.UTF_8).build())
-                .multiPart(
-                        new MultiPartSpecBuilder(place.getPosition().getLongitude().setScale(6, RoundingMode.HALF_DOWN).doubleValue()).controlName(
-                                "longitude").charset(StandardCharsets.UTF_8).build())
-                .multiPart(new MultiPartSpecBuilder(new FileInputStream(new File("src/test/java/com/now/naaga/temporaryplace/fixture/루터회관.png"))).controlName(
-                                "imageFile").charset(StandardCharsets.UTF_8)
-                        .fileName(
-                                "src/test/java/com/now/naaga/temporaryplace/fixture/루터회관.png")
-                        .mimeType(
-                                "image/png")
-                        .build())
-                .header("Authorization", "Bearer " + accessToken)
+                .auth().preemptive().basic(id, password)
                 .when()
-                .post("/temporary-places")
+                .get("/temporary-places")
                 .then()
                 .log().all()
                 .extract();
-
         final int statusCode = extract.statusCode();
-        final ExceptionResponse actual = extract.as(ExceptionResponse.class);
-        final TemporaryPlaceExceptionType exceptionType = TemporaryPlaceExceptionType.ALREADY_EXIST_NEARBY;
-        final ExceptionResponse expected = new ExceptionResponse(exceptionType.errorCode(), exceptionType.errorMessage());
-
+        final String jsonResponse = extract.body().asString();
+        final List<TemporaryPlaceResponse> actual = objectMapper.readValue(jsonResponse, new TypeReference<List<TemporaryPlaceResponse>>() {
+        });
+        final List<TemporaryPlaceResponse> expected = TemporaryPlaceResponse.convertToResponses(List.of(temporaryPlace));
         //then
         assertSoftly(softAssertions -> {
-            softAssertions.assertThat(statusCode).isEqualTo(HttpStatus.BAD_REQUEST.value());
+            softAssertions.assertThat(statusCode).isEqualTo(HttpStatus.OK.value());
             softAssertions.assertThat(expected)
                     .usingRecursiveComparison()
                     .isEqualTo(actual);
