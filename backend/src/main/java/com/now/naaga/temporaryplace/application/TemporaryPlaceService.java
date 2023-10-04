4fa26c4449e22c94f65c8e5a39f1d1fd636011ab
zillionme <beatus96@naver.com>
2023-10-04 12:40:47 +0900

    refactor: 필요없는 메서드 삭제

--- backend/src/main/java/com/now/naaga/temporaryplace/application/TemporaryPlaceService.java
+++ backend/src/main/java/com/now/naaga/temporaryplace/application/TemporaryPlaceService.java
@@ -6,7 +6,6 @@ import com.now.naaga.player.application.PlayerService;
 import com.now.naaga.player.domain.Player;
 import com.now.naaga.temporaryplace.application.dto.CreateTemporaryPlaceCommand;
 import com.now.naaga.temporaryplace.domain.TemporaryPlace;
-import com.now.naaga.temporaryplace.domain.TemporaryPlaceCheckService;
 import com.now.naaga.temporaryplace.repository.TemporaryPlaceRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,23 +23,18 @@ public class TemporaryPlaceService {
 
     private final PlayerService playerService;
 
-    private final TemporaryPlaceCheckService temporaryPlaceCheckService;
-
     private final FileManager<MultipartFile> fileManager;
 
     public TemporaryPlaceService(final TemporaryPlaceRepository temporaryPlaceRepository,
                                  final PlayerService playerService,
-                                 final TemporaryPlaceCheckService temporaryPlaceCheckService,
                                  final FileManager<MultipartFile> fileManager) {
         this.temporaryPlaceRepository = temporaryPlaceRepository;
         this.playerService = playerService;
-        this.temporaryPlaceCheckService = temporaryPlaceCheckService;
         this.fileManager = fileManager;
     }
 
     public TemporaryPlace createTemporaryPlace(final CreateTemporaryPlaceCommand createTemporaryPlaceCommand) {
         final Position position = createTemporaryPlaceCommand.position();
-        temporaryPlaceCheckService.checkOtherPlaceNearby(position);
         final File uploadPath = fileManager.save(createTemporaryPlaceCommand.imageFile());
         try {
             final Long playerId = createTemporaryPlaceCommand.playerId();
