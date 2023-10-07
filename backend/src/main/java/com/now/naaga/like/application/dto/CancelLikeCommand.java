8866941f9605748b64ece857df55ffb7520f5feb
dooboocookie <bkh3030@gmail.com>
2023-10-07 16:58:06 +0900

    feat: 좋아요 삭제 컨트롤러 추가

--- backend/src/main/java/com/now/naaga/like/application/dto/CancelLikeCommand.java
+++ backend/src/main/java/com/now/naaga/like/application/dto/CancelLikeCommand.java
@@ -1,5 +1,10 @@
 package com.now.naaga.like.application.dto;
 
+import com.now.naaga.player.presentation.dto.PlayerRequest;
+
 public record CancelLikeCommand(Long playerId,
                                 Long placeId) {
+    public static CancelLikeCommand of(final PlayerRequest playerRequest, final Long placeId) {
+        return new CancelLikeCommand(playerRequest.playerId(), placeId);
+    }
 }
