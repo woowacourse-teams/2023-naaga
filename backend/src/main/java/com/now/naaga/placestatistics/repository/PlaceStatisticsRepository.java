1d4f51b563e758b74d0da589a95ea615137d50ed
dooboocookie <bkh3030@gmail.com>
2023-10-09 16:16:09 +0900

    chore: 낙관적 락 테스트 연습

--- backend/src/main/java/com/now/naaga/placestatistics/repository/PlaceStatisticsRepository.java
+++ backend/src/main/java/com/now/naaga/placestatistics/repository/PlaceStatisticsRepository.java
@@ -1,11 +1,18 @@
 package com.now.naaga.placestatistics.repository;
 
 import com.now.naaga.placestatistics.domain.PlaceStatistics;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
 public interface PlaceStatisticsRepository extends JpaRepository<PlaceStatistics, Long> {
 
+    @Lock(LockModeType.OPTIMISTIC)
     Optional<PlaceStatistics> findByPlaceId(Long placeId);
+
+    @Lock(LockModeType.NONE)
+    @Override
+    PlaceStatistics save(PlaceStatistics placeStatistics);
 }
