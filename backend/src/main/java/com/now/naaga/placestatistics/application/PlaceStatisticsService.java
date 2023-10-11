4e400f7bd29faefbcac61377e0ecab8d27f9699f
kokodak <kokodakadokok@gmail.com>
2023-10-11 23:00:51 +0900

    feat: 장소 통계에서 좋아요 수를 올리는 기능 구현

--- backend/src/main/java/com/now/naaga/placestatistics/application/PlaceStatisticsService.java
+++ backend/src/main/java/com/now/naaga/placestatistics/application/PlaceStatisticsService.java
@@ -1,10 +1,13 @@
 package com.now.naaga.placestatistics.application;
 
-import com.now.naaga.placestatistics.domain.PlaceStatistics;
+import com.now.naaga.place.exception.PlaceException;
+import com.now.naaga.place.exception.PlaceExceptionType;
+import com.now.naaga.placestatistics.application.dto.PlusLikeCommand;
 import com.now.naaga.placestatistics.application.dto.SubtractLikeCommand;
+import com.now.naaga.placestatistics.domain.PlaceStatistics;
+import com.now.naaga.placestatistics.exception.PlaceStatisticsException;
 import com.now.naaga.placestatistics.exception.PlaceStatisticsExceptionType;
 import com.now.naaga.placestatistics.repository.PlaceStatisticsRepository;
-import com.now.naaga.placestatistics.exception.PlaceStatisticsException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,11 +21,18 @@ public class PlaceStatisticsService {
         this.placeStatisticsRepository = placeStatisticsRepository;
     }
 
+    public void plusLike(final PlusLikeCommand plusLikeCommand) {
+        final Long placeId = plusLikeCommand.placeId();
+        final PlaceStatistics placeStatistics = placeStatisticsRepository.findByPlaceId(placeId)
+                                                                         .orElseThrow(() -> new PlaceException(PlaceExceptionType.NO_EXIST));
+        placeStatistics.plusLike();
+    }
+
     public void subtractLike(final SubtractLikeCommand subtractLikeCommand) {
         final Long placeId = subtractLikeCommand.placeId();
 
         final PlaceStatistics placeStatistics = placeStatisticsRepository.findByPlaceId(placeId)
-                .orElseThrow(() -> new PlaceStatisticsException(PlaceStatisticsExceptionType.NOT_FOUND));
+                                                                         .orElseThrow(() -> new PlaceStatisticsException(PlaceStatisticsExceptionType.NOT_FOUND));
 
         placeStatistics.subtractLike();
     }
--- backend/src/test/java/com/now/naaga/placestatistics/application/PlaceStatisticsServiceTest.java
+++ backend/src/test/java/com/now/naaga/placestatistics/application/PlaceStatisticsServiceTest.java
@@ -1,7 +1,14 @@
 package com.now.naaga.placestatistics.application;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import com.now.naaga.common.builder.PlaceStatisticsBuilder;
+import com.now.naaga.common.exception.BaseExceptionType;
 import com.now.naaga.place.domain.Place;
+import com.now.naaga.place.exception.PlaceException;
+import com.now.naaga.place.exception.PlaceExceptionType;
+import com.now.naaga.placestatistics.application.dto.PlusLikeCommand;
 import com.now.naaga.placestatistics.application.dto.SubtractLikeCommand;
 import com.now.naaga.placestatistics.domain.PlaceStatistics;
 import com.now.naaga.placestatistics.repository.PlaceStatisticsRepository;
@@ -13,8 +20,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+@SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @Sql("/truncate.sql")
 @ActiveProfiles("test")
@@ -37,12 +43,43 @@ class PlaceStatisticsServiceTest {
     }
 
     @Test
+    void 장소통계에서_좋아요_수_1개를_올린다() {
+        // given
+        final PlaceStatistics placeStatistics = placeStatisticsBuilder.init()
+                                                                      .likeCount(5L)
+                                                                      .build();
+
+        // when
+        placeStatisticsService.plusLike(new PlusLikeCommand(placeStatistics.getPlace().getId()));
+
+        // then
+        final PlaceStatistics actual = placeStatisticsRepository.findByPlaceId(placeStatistics.getPlace().getId()).get();
+        assertThat(actual.getLikeCount()).isEqualTo(6L);
+    }
+
+    @Test
+    void 장소통계의_좋아요_수를_올릴때_장소통계를_찾을_수_없다면_장소_예외가_발생한다() {
+        // given
+        final PlaceStatistics placeStatistics = placeStatisticsBuilder.init()
+                                                                      .build();
+
+        // when
+        final PlusLikeCommand plusLikeCommand = new PlusLikeCommand(placeStatistics.getPlace().getId() + 1);
+        final BaseExceptionType baseExceptionType = assertThrows(PlaceException.class,
+                                                                 () -> placeStatisticsService.plusLike(plusLikeCommand)
+                                                                ).exceptionType();
+
+        // then
+        assertThat(baseExceptionType).isEqualTo(PlaceExceptionType.NO_EXIST);
+    }
+
+    @Test
     void 장소통계_1개를_줄인다() {
         //given
         final long beforeLikeCount = 10L;
         final PlaceStatistics placeStatistics = placeStatisticsBuilder.init()
-                .likeCount(beforeLikeCount)
-                .build();
+                                                                      .likeCount(beforeLikeCount)
+                                                                      .build();
         final Place place = placeStatistics.getPlace();
 
         //when
