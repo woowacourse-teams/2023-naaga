3159c80ac00a048acc87d15ae330c20c189b480d
chaewon121 <cw121@naver.com>
2023-10-13 23:57:23 +0900

    refactor: 요청 파라미터가 잘못된 경우 예외처리

--- backend/src/main/java/com/now/naaga/letter/presentation/LetterController.java
+++ backend/src/main/java/com/now/naaga/letter/presentation/LetterController.java
@@ -1,6 +1,7 @@
 package com.now.naaga.letter.presentation;
 
 import com.now.naaga.auth.presentation.annotation.Auth;
+import com.now.naaga.common.exception.CommonException;
 import com.now.naaga.letter.application.LetterService;
 import com.now.naaga.letter.application.dto.FindNearByLetterCommand;
 import com.now.naaga.letter.domain.Letter;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.now.naaga.common.exception.CommonExceptionType.INVALID_REQUEST_PARAMETERS;
+
 @RequestMapping("/letters")
 @RestController
 public class LetterController {
@@ -30,15 +33,15 @@ public class LetterController {
     public ResponseEntity<List<NearByLetterResponse>> findLetterNearBy(@Auth final PlayerRequest playerRequest,
                                                                        @RequestParam(name = "latitude") final String latitude,
                                                                        @RequestParam(name = "longitude") final String longitude) {
-        //널 체크 예외가 필요한가..!?
-        // 쪽지가 존재하지 않을때는 빈값을 줘야함 -> 필요시 코드 추가 작성
+        if (latitude == null || longitude == null) {
+            throw new CommonException(INVALID_REQUEST_PARAMETERS);
+        }
         final FindNearByLetterCommand findNearByLetterCommand = FindNearByLetterCommand.from(latitude, longitude);
 
         final List<Letter> letters = letterService.findNearByLetters(findNearByLetterCommand);
         final List<NearByLetterResponse> nearByLetterResponses = letters.stream()
                 .map(NearByLetterResponse::from)
                 .collect(Collectors.toList());
-        System.out.println("컨트롤러에서 뽑은 letters: "+letters.size());
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(nearByLetterResponses);
