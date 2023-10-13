e74f0d57d09df83e4fd3e8201af0bbf34b541beb
zillionme <beatus96@naver.com>
2023-10-13 15:01:19 +0900

    feat: 쪽지 등록 API 구현

--- backend/src/main/java/com/now/naaga/letter/presentation/LetterController.java
+++ backend/src/main/java/com/now/naaga/letter/presentation/LetterController.java
@@ -1,9 +1,39 @@
 package com.now.naaga.letter.presentation;
 
+import com.now.naaga.auth.presentation.annotation.Auth;
+import com.now.naaga.letter.application.LetterService;
+import com.now.naaga.letter.domain.Letter;
+import com.now.naaga.letter.presentation.dto.LetterCreateCommand;
+import com.now.naaga.letter.presentation.dto.LetterRequest;
+import com.now.naaga.letter.presentation.dto.LetterResponse;
+import com.now.naaga.player.presentation.dto.PlayerRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RequestMapping("/letters")
 @RestController
 public class LetterController {
+
+    private final LetterService letterService;
+
+    public LetterController(final LetterService letterService) {
+        this.letterService = letterService;
+    }
+
+    @PostMapping
+    public ResponseEntity<LetterResponse> createLetter(@Auth final PlayerRequest playerRequest,
+                                                       @RequestBody final LetterRequest letterRequest) {
+        final LetterCreateCommand letterCreateCommand = LetterCreateCommand.of(playerRequest, letterRequest);
+        final Letter letter = letterService.createLetter(letterCreateCommand);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .location(URI.create("/letters/" + letter.getId()))
+                .body(LetterResponse.of(letter));
+    }
 }
---
+++ backend/src/main/java/com/now/naaga/letter/presentation/dto/LetterResponse.java
@@ -0,0 +1,21 @@
+package com.now.naaga.letter.presentation.dto;
+
+import com.now.naaga.game.presentation.dto.CoordinateResponse;
+import com.now.naaga.letter.domain.Letter;
+import com.now.naaga.player.presentation.dto.PlayerResponse;
+
+public record LetterResponse(Long id,
+                             PlayerResponse player,
+                             CoordinateResponse coordinate,
+                             String message,
+                             String registerDate
+                             ) {
+
+    public static LetterResponse of(final Letter letter) {
+        Long id = letter.getId();
+        PlayerResponse playerResponse = PlayerResponse.from(letter.getRegisteredPlayer());
+        CoordinateResponse coordinateResponse = CoordinateResponse.of(letter.getPosition());
+        String registerDate = letter.getCreatedTime().toString();
+        return new LetterResponse(id, playerResponse, coordinateResponse, letter.getMessage(), registerDate);
+    }
+}
