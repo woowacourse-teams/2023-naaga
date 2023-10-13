6716f235c11f7771294ec9539410700894863546
kokodak <kokodakadokok@gmail.com>
2023-10-14 04:31:17 +0900

    feat: 좋아요 등록 API 구현

--- backend/src/main/java/com/now/naaga/like/presentation/PlaceLikeController.java
+++ backend/src/main/java/com/now/naaga/like/presentation/PlaceLikeController.java
@@ -2,12 +2,19 @@ package com.now.naaga.like.presentation;
 
 import com.now.naaga.auth.presentation.annotation.Auth;
 import com.now.naaga.like.application.PlaceLikeService;
+import com.now.naaga.like.application.dto.ApplyLikeCommand;
 import com.now.naaga.like.application.dto.CancelLikeCommand;
+import com.now.naaga.like.domain.PlaceLike;
+import com.now.naaga.like.presentation.dto.ApplyPlaceLikeRequest;
+import com.now.naaga.like.presentation.dto.PlaceLikeResponse;
 import com.now.naaga.player.presentation.dto.PlayerRequest;
+import java.net.URI;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +26,21 @@ public class PlaceLikeController {
 
     public PlaceLikeController(final PlaceLikeService placeLikeService) {
         this.placeLikeService = placeLikeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PlaceLikeResponse> applyPlaceLike(@Auth PlayerRequest playerRequest,
+                                                            @PathVariable Long placeId,
+                                                            @RequestBody ApplyPlaceLikeRequest applyPlaceLikeRequest) {
+        final ApplyLikeCommand command = ApplyLikeCommand.of(playerRequest,
+                                                             placeId,
+                                                             applyPlaceLikeRequest);
+        final PlaceLike placeLike = placeLikeService.applyLike(command);
+        final PlaceLikeResponse response = PlaceLikeResponse.from(placeLike);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .location(URI.create("/places/" + placeId + "/likes/my"))
+                .body(response);
     }
 
     @DeleteMapping("/my")
--- backend/src/test/java/com/now/naaga/like/presentation/PlaceLikeControllerTest.java
+++ backend/src/test/java/com/now/naaga/like/presentation/PlaceLikeControllerTest.java
@@ -1,5 +1,8 @@
 package com.now.naaga.like.presentation;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+
 import com.now.naaga.auth.domain.AuthToken;
 import com.now.naaga.auth.infrastructure.AuthType;
 import com.now.naaga.auth.infrastructure.jwt.AuthTokenGenerator;
@@ -8,11 +11,12 @@ import com.now.naaga.common.builder.PlaceBuilder;
 import com.now.naaga.common.builder.PlaceLikeBuilder;
 import com.now.naaga.common.builder.PlaceStatisticsBuilder;
 import com.now.naaga.common.builder.PlayerBuilder;
-import com.now.naaga.common.exception.ExceptionResponse;
 import com.now.naaga.like.domain.PlaceLike;
+import com.now.naaga.like.domain.PlaceLikeType;
+import com.now.naaga.like.presentation.dto.ApplyPlaceLikeRequest;
+import com.now.naaga.like.presentation.dto.PlaceLikeResponse;
 import com.now.naaga.member.domain.Member;
 import com.now.naaga.place.domain.Place;
-import com.now.naaga.placestatistics.domain.PlaceStatistics;
 import com.now.naaga.player.domain.Player;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -25,10 +29,7 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
-import static com.now.naaga.like.exception.PlaceLikeExceptionType.NOT_EXIST;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.http.HttpStatus.NO_CONTENT;
+import org.springframework.http.MediaType;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -63,16 +64,63 @@ class PlaceLikeControllerTest extends CommonControllerTest {
     }
 
     @Test
+    void 좋아요_등록이_성공하면_201_응답을_반환한다() {
+        // given
+        final Player player = playerBuilder.init()
+                                           .build();
+
+        final Place place = placeBuilder.init()
+                                        .build();
+
+        placeStatisticsBuilder.init()
+                              .place(place)
+                              .build();
+
+        final ApplyPlaceLikeRequest applyPlaceLikeRequest = new ApplyPlaceLikeRequest(PlaceLikeType.LIKE);
+
+        final Member member = player.getMember();
+        final AuthToken generate = authTokenGenerator.generate(member, member.getId(), AuthType.KAKAO);
+        final String accessToken = generate.getAccessToken();
+
+        // when
+        final ExtractableResponse<Response> extract = RestAssured
+                .given().log().all()
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(applyPlaceLikeRequest)
+                .when()
+                .post("/places/{placeId}/likes", place.getId())
+                .then().log().all()
+                .extract();
+
+        // then
+        final int statusCode = extract.statusCode();
+        final PlaceLikeResponse actual = extract.as(PlaceLikeResponse.class);
+        final PlaceLikeResponse expected = new PlaceLikeResponse(null,
+                                                                 player.getId(),
+                                                                 place.getId(),
+                                                                 PlaceLikeType.LIKE);
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(statusCode).isEqualTo(HttpStatus.CREATED.value());
+            softAssertions.assertThat(actual).usingRecursiveComparison()
+                          .ignoringExpectedNullFields()
+                          .isEqualTo(expected);
+        });
+    }
+
+    @Test
     void 좋아요_삭제를_성공하면_204응답을_한다() {
         //given
         final Place place = placeBuilder.init()
-                .build();
+                                        .build();
+
         final PlaceLike placeLike = placeLikeBuilder.init()
-                .place(place)
-                .build();
+                                                    .place(place)
+                                                    .build();
         placeStatisticsBuilder.init()
-                .place(place)
-                .build();
+                              .place(place)
+                              .build();
+
         final Member member = placeLike.getPlayer().getMember();
         final AuthToken generate = authTokenGenerator.generate(member, member.getId(), AuthType.KAKAO);
         final String accessToken = generate.getAccessToken();
