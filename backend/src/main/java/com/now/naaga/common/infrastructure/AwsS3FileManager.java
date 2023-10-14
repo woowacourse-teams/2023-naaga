771c2bb87572490498a4e4eb31dcb051f29a2992
zillionme <beatus96@naver.com>
2023-10-14 18:22:43 +0900

    feat: 긴급 푸시

--- backend/src/main/java/com/now/naaga/common/infrastructure/AwsS3FileManager.java
+++ backend/src/main/java/com/now/naaga/common/infrastructure/AwsS3FileManager.java
@@ -1,6 +1,7 @@
 package com.now.naaga.common.infrastructure;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.now.naaga.common.exception.CommonException;
@@ -15,52 +16,53 @@ import java.util.UUID;
 
 @Component
 public class AwsS3FileManager {
-
+    
     private static final String FILE_EXTENSION_SEPARATOR = ".";
-
+    
     private final AmazonS3 amazonS3;
-
+    
     private final String bucketName;
-
+    
     public AwsS3FileManager(final AmazonS3 amazonS3,
                             @Value("${cloud.aws.s3.bucket}") final String bucketName) {
         this.amazonS3 = amazonS3;
         this.bucketName = bucketName;
     }
-
+    
     public String uploadFile(final MultipartFile multipartFile) {
         validateFileExists(multipartFile);
         final String fileName = buildFileName(multipartFile.getOriginalFilename());
         final ObjectMetadata objectMetadata = initializeObjectMetadata(multipartFile);
-
+        
         try (final InputStream inputStream = multipartFile.getInputStream()) {
             amazonS3.putObject(new PutObjectRequest(bucketName, fileName, inputStream, objectMetadata));
         } catch (IOException e) {
             throw new IllegalArgumentException();
         }
-
+        
         return amazonS3.getUrl(bucketName, fileName).toString();
     }
-
+    
     private void validateFileExists(final MultipartFile multipartFile) {
         if (multipartFile.isEmpty()) {
             throw new CommonException(CommonExceptionType.MULTIPART_FILE_NOT_EXIST);
         }
     }
-
+    
     private String buildFileName(final String originalFileName) {
         final String extension = originalFileName.substring(originalFileName.lastIndexOf(FILE_EXTENSION_SEPARATOR));
         return UUID.randomUUID() + extension;
     }
-
+    
     private ObjectMetadata initializeObjectMetadata(final MultipartFile multipartFile) {
         final ObjectMetadata objectMetadata = new ObjectMetadata();
         objectMetadata.setContentLength(multipartFile.getSize());
         objectMetadata.setContentType(multipartFile.getContentType());
         return objectMetadata;
     }
-
-    public void deleteFile() {
-
+    
+    public void deleteFile(String imageUrl) {
+        String fileName = imageUrl.substring(imageUrl.lastIndexOf("/"));
+        amazonS3.deleteObject(new DeleteObjectRequest(bucketName, fileName));
     }
 }
--- backend/src/main/java/com/now/naaga/temporaryplace/application/TemporaryPlaceService.java
+++ backend/src/main/java/com/now/naaga/temporaryplace/application/TemporaryPlaceService.java
@@ -6,6 +6,7 @@ import com.now.naaga.player.application.PlayerService;
 import com.now.naaga.player.domain.Player;
 import com.now.naaga.temporaryplace.application.dto.CreateTemporaryPlaceCommand;
 import com.now.naaga.temporaryplace.domain.TemporaryPlace;
+import com.now.naaga.temporaryplace.exception.TemporaryPlaceException;
 import com.now.naaga.temporaryplace.repository.TemporaryPlaceRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,16 +14,18 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Comparator;
 import java.util.List;
 
+import static com.now.naaga.temporaryplace.exception.TemporaryPlaceExceptionType.NOT_EXIST;
+
 @Transactional
 @Service
 public class TemporaryPlaceService {
-
+    
     private final TemporaryPlaceRepository temporaryPlaceRepository;
-
+    
     private final PlayerService playerService;
-
+    
     private final AwsS3FileManager awsS3FileManager;
-
+    
     public TemporaryPlaceService(final TemporaryPlaceRepository temporaryPlaceRepository,
                                  final PlayerService playerService,
                                  final AwsS3FileManager awsS3FileManager) {
@@ -30,11 +33,11 @@ public class TemporaryPlaceService {
         this.playerService = playerService;
         this.awsS3FileManager = awsS3FileManager;
     }
-
+    
     public TemporaryPlace createTemporaryPlace(final CreateTemporaryPlaceCommand createTemporaryPlaceCommand) {
         final Position position = createTemporaryPlaceCommand.position();
         String imageUrl = awsS3FileManager.uploadFile(createTemporaryPlaceCommand.imageFile());
-
+        
         try {
             final Long playerId = createTemporaryPlaceCommand.playerId();
             final Player registeredPlayer = playerService.findPlayerById(playerId);
@@ -46,15 +49,18 @@ public class TemporaryPlaceService {
                     registeredPlayer);
             return temporaryPlaceRepository.save(temporaryPlace);
         } catch (final RuntimeException exception) {
-            awsS3FileManager.deleteFile();
+            awsS3FileManager.deleteFile(imageUrl);
             throw exception;
         }
     }
-
+    
     public void deleteById(final Long id) {
+        TemporaryPlace temporaryPlace = temporaryPlaceRepository.findById(id)
+                                                                .orElseThrow(() -> new TemporaryPlaceException(NOT_EXIST));
         temporaryPlaceRepository.deleteById(id);
+        awsS3FileManager.deleteFile(temporaryPlace.getImageUrl());
     }
-
+    
     @Transactional(readOnly = true)
     public List<TemporaryPlace> findAllTemporaryPlace() {
         final List<TemporaryPlace> temporaryPlaces = temporaryPlaceRepository.findAll();
