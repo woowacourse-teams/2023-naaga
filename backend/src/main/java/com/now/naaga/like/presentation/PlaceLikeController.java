881eaf73e7856c0e73d44d37e7ad4668d4807d5e
kokodak <kokodakadokok@gmail.com>
2023-10-15 05:48:23 +0900

    feat: 나의 좋아요 여부 조회 API 구현

--- backend/src/main/java/com/now/naaga/like/presentation/PlaceLikeController.java
+++ backend/src/main/java/com/now/naaga/like/presentation/PlaceLikeController.java
@@ -3,10 +3,14 @@ package com.now.naaga.like.presentation;
 import com.now.naaga.auth.presentation.annotation.Auth;
 import com.now.naaga.like.application.PlaceLikeService;
 import com.now.naaga.like.application.dto.CancelLikeCommand;
+import com.now.naaga.like.application.dto.CheckMyPlaceLikeCommand;
+import com.now.naaga.like.domain.PlaceLikeType;
+import com.now.naaga.like.presentation.dto.CheckMyPlaceLikeResponse;
 import com.now.naaga.player.presentation.dto.PlayerRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +33,16 @@ public class PlaceLikeController {
         return ResponseEntity
                 .status(HttpStatus.NO_CONTENT)
                 .build();
+    }
+
+    @GetMapping("/my")
+    public ResponseEntity<CheckMyPlaceLikeResponse> checkMyPlaceLike(@Auth final PlayerRequest playerRequest,
+                                                                     @PathVariable final Long placeId) {
+        final CheckMyPlaceLikeCommand command = CheckMyPlaceLikeCommand.of(playerRequest, placeId);
+        final PlaceLikeType placeLikeType = placeLikeService.checkMyLike(command);
+        final CheckMyPlaceLikeResponse response = CheckMyPlaceLikeResponse.from(placeLikeType);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
     }
 }
--- backend/src/test/java/com/now/naaga/like/presentation/PlaceLikeControllerTest.java
+++ backend/src/test/java/com/now/naaga/like/presentation/PlaceLikeControllerTest.java
@@ -1,5 +1,9 @@
 package com.now.naaga.like.presentation;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.*;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+
 import com.now.naaga.auth.domain.AuthToken;
 import com.now.naaga.auth.infrastructure.AuthType;
 import com.now.naaga.auth.infrastructure.jwt.AuthTokenGenerator;
@@ -8,11 +12,11 @@ import com.now.naaga.common.builder.PlaceBuilder;
 import com.now.naaga.common.builder.PlaceLikeBuilder;
 import com.now.naaga.common.builder.PlaceStatisticsBuilder;
 import com.now.naaga.common.builder.PlayerBuilder;
-import com.now.naaga.common.exception.ExceptionResponse;
 import com.now.naaga.like.domain.PlaceLike;
+import com.now.naaga.like.domain.PlaceLikeType;
+import com.now.naaga.like.presentation.dto.CheckMyPlaceLikeResponse;
 import com.now.naaga.member.domain.Member;
 import com.now.naaga.place.domain.Place;
-import com.now.naaga.placestatistics.domain.PlaceStatistics;
 import com.now.naaga.player.domain.Player;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -25,10 +29,7 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
-import static com.now.naaga.like.exception.PlaceLikeExceptionType.NOT_EXIST;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.http.HttpStatus.NO_CONTENT;
+import org.springframework.http.MediaType;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -66,13 +67,13 @@ class PlaceLikeControllerTest extends CommonControllerTest {
     void 좋아요_삭제를_성공하면_204응답을_한다() {
         //given
         final Place place = placeBuilder.init()
-                .build();
+                                        .build();
         final PlaceLike placeLike = placeLikeBuilder.init()
-                .place(place)
-                .build();
+                                                    .place(place)
+                                                    .build();
         placeStatisticsBuilder.init()
-                .place(place)
-                .build();
+                              .place(place)
+                              .build();
         final Member member = placeLike.getPlayer().getMember();
         final AuthToken generate = authTokenGenerator.generate(member, member.getId(), AuthType.KAKAO);
         final String accessToken = generate.getAccessToken();
@@ -91,5 +92,81 @@ class PlaceLikeControllerTest extends CommonControllerTest {
         //then
         final int statusCode = extract.statusCode();
         assertThat(statusCode).isEqualTo(NO_CONTENT.value());
+    }
+
+    @Test
+    void 좋아요가_눌려있을_때_나의_좋아요_여부를_조회하면_해당_좋아요_타입_응답과_200_상태코드를_반환한다() {
+        // given
+        final Player player = playerBuilder.init()
+                                           .build();
+
+        final Place place = placeBuilder.init()
+                                        .build();
+
+        final PlaceLikeType myType = PlaceLikeType.LIKE;
+
+        placeLikeBuilder.init()
+                        .player(player)
+                        .place(place)
+                        .placeLikeType(myType)
+                        .build();
+
+        final Member member = player.getMember();
+        final AuthToken generate = authTokenGenerator.generate(member, member.getId(), AuthType.KAKAO);
+        final String accessToken = generate.getAccessToken();
+
+        // when
+        final ExtractableResponse<Response> extract = RestAssured
+                .given().log().all()
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/places/{placeId}/likes/my", place.getId())
+                .then().log().all()
+                .extract();
+
+        // then
+        final int statusCode = extract.statusCode();
+        final CheckMyPlaceLikeResponse actual = extract.as(CheckMyPlaceLikeResponse.class);
+        final CheckMyPlaceLikeResponse expected = new CheckMyPlaceLikeResponse(myType);
+        assertSoftly(softAssertions -> {
+            softAssertions.assertThat(statusCode).isEqualTo(HttpStatus.OK.value());
+            softAssertions.assertThat(actual).usingRecursiveComparison()
+                          .isEqualTo(expected);
+        });
+    }
+
+    @Test
+    void 아무것도_눌려있지_않을_때_나의_좋아요_여부를_조회하면_NONE_타입_응답과_200_상태코드를_반환한다() {
+        // given
+        final Player player = playerBuilder.init()
+                                           .build();
+
+        final Place place = placeBuilder.init()
+                                        .build();
+
+        final Member member = player.getMember();
+        final AuthToken generate = authTokenGenerator.generate(member, member.getId(), AuthType.KAKAO);
+        final String accessToken = generate.getAccessToken();
+
+        // when
+        final ExtractableResponse<Response> extract = RestAssured
+                .given().log().all()
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/places/{placeId}/likes/my", place.getId())
+                .then().log().all()
+                .extract();
+
+        // then
+        final int statusCode = extract.statusCode();
+        final CheckMyPlaceLikeResponse actual = extract.as(CheckMyPlaceLikeResponse.class);
+        final CheckMyPlaceLikeResponse expected = new CheckMyPlaceLikeResponse(PlaceLikeType.NONE);
+        assertSoftly(softAssertions -> {
+            softAssertions.assertThat(statusCode).isEqualTo(HttpStatus.OK.value());
+            softAssertions.assertThat(actual).usingRecursiveComparison()
+                          .isEqualTo(expected);
+        });
     }
 }
