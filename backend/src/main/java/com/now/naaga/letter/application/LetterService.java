b66ea057bcce53f3a4bd7b02bd9948a69754628c
chaewon121 <cw121@naver.com>
2023-10-16 02:14:01 +0900

    refactor: id로 찾는 로직 제거

--- backend/src/main/java/com/now/naaga/letter/application/LetterService.java
+++ backend/src/main/java/com/now/naaga/letter/application/LetterService.java
@@ -20,28 +20,22 @@ public class LetterService {
 
     private final LetterRepository letterRepository;
     private final ReadLetterLogService readLetterLogService;
-    private final GameService gameService;
     private final PlayerService playerService;
 
     public LetterService(final LetterRepository letterRepository,
                          final ReadLetterLogService readLetterLogService,
-                         final GameService gameService, final PlayerService playerService) {
+                         final PlayerService playerService) {
         this.letterRepository = letterRepository;
         this.readLetterLogService = readLetterLogService;
-        this.gameService = gameService;
         this.playerService = playerService;
     }
 
     public Letter findLetter(final LetterReadCommand letterReadCommand) {
         final Player player = playerService.findPlayerById(letterReadCommand.playerId());
-        final Letter foundLetter = findLetterById(letterReadCommand.letterId());
+        final Letter foundLetter = letterRepository.findById(letterReadCommand.letterId())
+                .orElseThrow(() -> new LetterException(NO_EXIST));
+
         readLetterLogService.log(new LetterLogCreateCommand(player.getId(), foundLetter));
         return foundLetter;
-    }
-
-    @Transactional(readOnly = true)
-    public Letter findLetterById(final Long id) {
-        return letterRepository.findById(id)
-                .orElseThrow(() -> new LetterException(NO_EXIST));
     }
 }
