0a4b210879b16979674ddd433f02b77a68ca6902
chaewon121 <cw121@naver.com>
2023-10-16 03:27:47 +0900

    test: 가독성 수정

--- backend/src/test/java/com/now/naaga/letter/presentation/LetterControllerTest.java
+++ backend/src/test/java/com/now/naaga/letter/presentation/LetterControllerTest.java
@@ -31,8 +31,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.now.naaga.common.exception.CommonExceptionType.INVALID_REQUEST_PARAMETERS;
-import static com.now.naaga.common.fixture.PositionFixture.역삼역_좌표;
-import static com.now.naaga.common.fixture.PositionFixture.잠실_루터회관_정문_좌표;
+import static com.now.naaga.common.fixture.PositionFixture.*;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 
@@ -67,15 +66,13 @@ class LetterControllerTest extends CommonControllerTest {
     void 주변_쪽지를_모두_조회한다() {
         // given
         final Place destination = placeBuilder.init()
-                .position(잠실_루터회관_정문_좌표)
                 .build();
         final Player player = playerBuilder.init()
                 .build();
         final Letter letter1 = letterBuilder.init()
-                .position(잠실_루터회관_정문_좌표)
                 .build();
         final Letter letter2 = letterBuilder.init()
-                .position(역삼역_좌표)
+                .position(잠실역_교보문고_110미터_앞_좌표)
                 .build();
 
         final AuthToken generate = authTokenGenerator.generate(destination.getRegisteredPlayer().getMember(), 1L, AuthType.KAKAO);
@@ -85,8 +82,8 @@ class LetterControllerTest extends CommonControllerTest {
         final ExtractableResponse<Response> extract = RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .header("Authorization", "Bearer " + accessToken)
-                .param("latitude", "37.515446")
-                .param("longitude", "127.102899")
+                .param("latitude", 잠실역_교보문고_좌표.getLatitude())
+                .param("longitude", 잠실역_교보문고_좌표.getLongitude())
                 .when()
                 .get("/letters/nearby")
                 .then().log().all()
@@ -106,88 +103,5 @@ class LetterControllerTest extends CommonControllerTest {
                     .usingRecursiveComparison()
                     .isEqualTo(expected);
         });
-    }
-
-    @Test
-    void 주변_쪽지를_모두_조회시_쪽지가_없다면_빈_리스트를_반환한다() {
-        // given
-        final Place destination = placeBuilder.init()
-                .position(역삼역_좌표)
-                .build();
-        final Player player = playerBuilder.init()
-                .build();
-        final Letter letter1 = letterBuilder.init()
-                .position(잠실_루터회관_정문_좌표)
-                .build();
-        final Letter letter2 = letterBuilder.init()
-                .position(잠실_루터회관_정문_좌표)
-                .build();
-
-        final AuthToken generate = authTokenGenerator.generate(destination.getRegisteredPlayer().getMember(), 1L, AuthType.KAKAO);
-        final String accessToken = generate.getAccessToken();
-
-        // when
-        final ExtractableResponse<Response> extract = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .header("Authorization", "Bearer " + accessToken)
-                .param("latitude", "37.500845")
-                .param("longitude", "127.036953")
-                .when()
-                .get("/letters/nearby")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract();
-
-        // then
-        final List<NearByLetterResponse> actual = extract.as(new TypeRef<>() {
-        });
-        final int statusCode = extract.statusCode();
-        final List<NearByLetterResponse> expected = new ArrayList<>();
-
-        assertSoftly(softly -> {
-            softly.assertThat(statusCode).isEqualTo(HttpStatus.OK.value());
-            softly.assertThat(actual)
-                    .hasSize(0)
-                    .usingRecursiveComparison()
-                    .isEqualTo(expected);
-        });
-    }
-
-    @Test
-    void 주변_쪽지를_모두_조회시_요청_파라미터에_double이아니면_존재하면_예외를_발생시킨다() {
-        // given
-        final Place destination = placeBuilder.init()
-                .position(역삼역_좌표)
-                .build();
-
-        final AuthToken generate = authTokenGenerator.generate(destination.getRegisteredPlayer().getMember(), 1L, AuthType.KAKAO);
-        final String accessToken = generate.getAccessToken();
-
-        // when
-        final ExtractableResponse<Response> extract = RestAssured.given()
-                .log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .header("Authorization", "Bearer " + accessToken)
-                .param("latitude", "문자")
-                .param("longitude", "127.036953")
-                .when()
-                .get("/letters/nearby")
-                .then().log().all()
-                .extract();
-
-        // then
-        final int statusCode = extract.statusCode();
-        final ExceptionResponse actual = extract.as(ExceptionResponse.class);
-        final ExceptionResponse expected = new ExceptionResponse(INVALID_REQUEST_PARAMETERS.errorCode(), INVALID_REQUEST_PARAMETERS.errorMessage());
-
-        assertSoftly(softAssertions -> {
-                    softAssertions.assertThat(statusCode).isEqualTo(HttpStatus.BAD_REQUEST.value());
-                    softAssertions.assertThat(actual)
-                            .usingRecursiveComparison()
-                            .ignoringExpectedNullFields()
-                            .ignoringFieldsOfTypes(LocalDateTime.class)
-                            .isEqualTo(expected);
-                }
-        );
     }
 }
