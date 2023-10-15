d1b92f8ffbb2e942f92bfb322b5b5390755ea0af
krrong <khk9664@naver.com>
2023-10-15 19:20:00 +0900

    feat: 편지 관련 컴파운드 뷰 추가

---
+++ android/app/src/main/java/com/now/naaga/presentation/custom/LetterView.kt
@@ -0,0 +1,16 @@
+package com.now.naaga.presentation.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.now.naaga.databinding.CustomLetterViewBinding
+
+class LetterView(context: Context, attrs: AttributeSet?) : ConstraintLayout(context, attrs) {
+    private val binding: CustomLetterViewBinding
+
+    init {
+        val layoutInflater = LayoutInflater.from(context)
+        binding = CustomLetterViewBinding.inflate(layoutInflater, this, true)
+    }
+}
