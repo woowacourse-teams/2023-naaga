c05f749e73841461e59d042820a7d8686556d0bd
dooboocookie <79090478+dooboocookie@users.noreply.github.com>
2023-10-16 00:58:20 +0900

    feat: 장소의 좋아요/싫어요 개수 조회 API (#465)
    * feat: 장소통계를 장소아이디로 조회하는 기능 추가
    
    * feat: 특정 장소를 좋아요의 집계를 볼 수 있는 서비스 로직 추가
    
    * feat: 특정 장소의 좋아요 수를 조회하는 API 구현
    
    * test: 불분명한 메서드명 수정
    
    * fix: 좋아요 수 보는 API 인가처리하지 않도록 수정
    
    * test: SoftAssertions 에서 체이닝해서 assertThat 사용하도록 변경
    
    * chore: 임포트문 수정

--- backend/src/test/java/com/now/naaga/like/presentation/PlaceLikeControllerTest.java
+++ backend/src/test/java/com/now/naaga/like/presentation/PlaceLikeControllerTest.java
@@ -9,10 +9,13 @@ import com.now.naaga.common.builder.PlaceLikeBuilder;
 import com.now.naaga.common.builder.PlaceStatisticsBuilder;
 import com.now.naaga.common.builder.PlayerBuilder;
 import com.now.naaga.common.exception.ExceptionResponse;
+import com.now.naaga.like.application.dto.CountPlaceLikeCommand;
 import com.now.naaga.like.domain.PlaceLike;
+import com.now.naaga.like.presentation.dto.PlaceLikeCountResponse;
 import com.now.naaga.member.domain.Member;
 import com.now.naaga.place.domain.Place;
 import com.now.naaga.placestatistics.domain.PlaceStatistics;
+import com.now.naaga.placestatistics.exception.PlaceStatisticsExceptionType;
 import com.now.naaga.player.domain.Player;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -28,13 +31,12 @@ import org.springframework.http.HttpStatus;
 
 import static com.now.naaga.like.exception.PlaceLikeExceptionType.NOT_EXIST;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.springframework.http.HttpStatus.*;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class PlaceLikeControllerTest extends CommonControllerTest {
-
-    private final PlayerBuilder playerBuilder;
 
     private final PlaceBuilder placeBuilder;
 
@@ -45,12 +47,10 @@ class PlaceLikeControllerTest extends CommonControllerTest {
     private final AuthTokenGenerator authTokenGenerator;
 
     @Autowired
-    public PlaceLikeControllerTest(final PlayerBuilder playerBuilder,
-                                   final PlaceBuilder placeBuilder,
+    public PlaceLikeControllerTest(final PlaceBuilder placeBuilder,
                                    final PlaceLikeBuilder placeLikeBuilder,
                                    final PlaceStatisticsBuilder placeStatisticsBuilder,
                                    final AuthTokenGenerator authTokenGenerator) {
-        this.playerBuilder = playerBuilder;
         this.placeBuilder = placeBuilder;
         this.placeLikeBuilder = placeLikeBuilder;
         this.placeStatisticsBuilder = placeStatisticsBuilder;
@@ -91,5 +91,53 @@ class PlaceLikeControllerTest extends CommonControllerTest {
         //then
         final int statusCode = extract.statusCode();
         assertThat(statusCode).isEqualTo(NO_CONTENT.value());
+    }
+
+    @Test
+    void 특정_장소의_좋아요_수를_응답하고_응답의_상태코드는_200이다() {
+        //given
+        final Long expected = 123L;
+        final PlaceStatistics placeStatistics = placeStatisticsBuilder.init()
+                .likeCount(expected)
+                .build();
+        final Long placeId = placeStatistics.getPlace().getId();
+
+        //when
+        final ExtractableResponse<Response> extract = RestAssured
+                .given().log().all()
+                .pathParam("placeId", placeId)
+                .when()
+                .get("/places/{placeId}/likes/count")
+                .then().log().all()
+                .extract();
+
+        //then
+        final int statusCode = extract.statusCode();
+        final PlaceLikeCountResponse actual = extract.as(PlaceLikeCountResponse.class);
+        assertSoftly(softAssertions -> {
+            softAssertions.assertThat(statusCode).isEqualTo(OK.value());
+            softAssertions.assertThat(actual.placeLikeCount()).isEqualTo(expected);
+        });
+    }
+
+    @Test
+    void 좋아요_수를_조회할_때_장소_통계가_없으면_400_예외를_발생한다() {
+        //given & when
+        final ExtractableResponse<Response> extract = RestAssured
+                .given().log().all()
+                .pathParam("placeId", 100)
+                .when()
+                .get("/places/{placeId}/likes/count")
+                .then().log().all()
+                .extract();
+
+        //then
+        final int statusCode = extract.statusCode();
+        final ExceptionResponse actual = extract.as(ExceptionResponse.class);
+        assertSoftly(softAssertions -> {
+            softAssertions.assertThat(statusCode).isEqualTo(NOT_FOUND.value());
+            softAssertions.assertThat(actual.getCode()).isEqualTo(PlaceStatisticsExceptionType.NOT_FOUND.errorCode());
+            softAssertions.assertThat(actual.getMessage()).isEqualTo(PlaceStatisticsExceptionType.NOT_FOUND.errorMessage());
+        });
     }
 }
--- backend/src/test/java/com/now/naaga/placestatistics/application/PlaceStatisticsServiceTest.java
+++ backend/src/test/java/com/now/naaga/placestatistics/application/PlaceStatisticsServiceTest.java
@@ -1,10 +1,16 @@
 package com.now.naaga.placestatistics.application;
 
 import com.now.naaga.common.builder.PlaceStatisticsBuilder;
+import com.now.naaga.common.exception.BaseExceptionType;
 import com.now.naaga.place.domain.Place;
+import com.now.naaga.placestatistics.application.dto.FindPlaceStatisticsByPlaceIdCommand;
 import com.now.naaga.placestatistics.application.dto.SubtractLikeCommand;
 import com.now.naaga.placestatistics.domain.PlaceStatistics;
+import com.now.naaga.placestatistics.exception.PlaceStatisticsException;
+import com.now.naaga.placestatistics.exception.PlaceStatisticsExceptionType;
 import com.now.naaga.placestatistics.repository.PlaceStatisticsRepository;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -12,8 +18,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @Sql("/truncate.sql")
@@ -52,5 +62,36 @@ class PlaceStatisticsServiceTest {
         //then
         final PlaceStatistics findPlaceStatistics = placeStatisticsRepository.findByPlaceId(place.getId()).get();
         assertThat(findPlaceStatistics.getLikeCount()).isEqualTo(beforeLikeCount - 1);
+    }
+
+    @Transactional
+    @Test
+    void 장소통계를_장소아이디로_조회한다() {
+        // given
+        final PlaceStatistics expected = placeStatisticsBuilder.init()
+                .likeCount(123L)
+                .build();
+
+        // when
+        final Long placeId = expected.getPlace().getId();
+        final FindPlaceStatisticsByPlaceIdCommand findPlaceStatisticsByPlaceIdCommand = new FindPlaceStatisticsByPlaceIdCommand(placeId);
+        final PlaceStatistics actual = placeStatisticsService.findPlaceStatisticsByPlaceId(findPlaceStatisticsByPlaceIdCommand);
+
+        assertThat(actual)
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
+    }
+
+    @Test
+    void 장소통계를_장소아이디로_조회_시_없으면_예외를_발생한다() {
+        // given & when
+        final FindPlaceStatisticsByPlaceIdCommand findPlaceStatisticsByPlaceIdCommand = new FindPlaceStatisticsByPlaceIdCommand(1L);
+
+        // then
+        assertAll(() -> {
+            final BaseExceptionType baseExceptionType = assertThrows(PlaceStatisticsException.class, () -> placeStatisticsService.findPlaceStatisticsByPlaceId(findPlaceStatisticsByPlaceIdCommand))
+                    .exceptionType();
+            assertThat(baseExceptionType).isEqualTo(PlaceStatisticsExceptionType.NOT_FOUND);
+        });
     }
 }
