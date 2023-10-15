4f6cbafed8943dcafe772d70fb4db3c26bcedceb
krrong <khk9664@naver.com>
2023-10-15 22:05:45 +0900

    feat: 쪽지 등록 기능 구현

--- android/app/src/main/java/com/now/naaga/data/repository/DefaultLetterRepository.kt
+++ android/app/src/main/java/com/now/naaga/data/repository/DefaultLetterRepository.kt
@@ -4,13 +4,17 @@ import com.now.domain.model.letter.ClosedLetter
 import com.now.domain.model.letter.OpenLetter
 import com.now.domain.model.type.LogType
 import com.now.domain.repository.LetterRepository
+import com.now.naaga.data.mapper.toDomain
+import com.now.naaga.data.remote.dto.post.PostLetterDto
 import com.now.naaga.data.remote.retrofit.service.LetterService
+import com.now.naaga.util.extension.getValueOrThrow
 
 class DefaultLetterRepository(
     private val letterService: LetterService,
 ) : LetterRepository {
     override suspend fun postLetter(message: String, latitude: Double, longitude: Double): OpenLetter {
-        TODO("Not yet implemented")
+        val response = letterService.registerLetter(PostLetterDto(message, latitude, longitude)).getValueOrThrow()
+        return response.toDomain()
     }
 
     override suspend fun fetchNearbyLetters(latitude: Double, longitude: Double): List<ClosedLetter> {
