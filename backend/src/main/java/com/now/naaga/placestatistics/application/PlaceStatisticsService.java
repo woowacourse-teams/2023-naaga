2d0d015a081af6149b75e70b639af7f7f5803d94
kokodak <kokodakadokok@gmail.com>
2023-10-15 19:21:06 +0900

    feat: 장소 통계 생성 서비스 로직 구현

--- backend/src/main/java/com/now/naaga/placestatistics/application/PlaceStatisticsService.java
+++ backend/src/main/java/com/now/naaga/placestatistics/application/PlaceStatisticsService.java
@@ -1,10 +1,16 @@
 package com.now.naaga.placestatistics.application;
 
-import com.now.naaga.placestatistics.domain.PlaceStatistics;
+import static com.now.naaga.placestatistics.domain.PlaceStatistics.LIKE_COUNT_DEFAULT_VALUE;
+
+import com.now.naaga.place.application.PlaceService;
+import com.now.naaga.place.application.dto.FindPlaceByIdCommand;
+import com.now.naaga.place.domain.Place;
+import com.now.naaga.placestatistics.application.dto.CreatePlaceStatisticsCommand;
 import com.now.naaga.placestatistics.application.dto.SubtractLikeCommand;
+import com.now.naaga.placestatistics.domain.PlaceStatistics;
+import com.now.naaga.placestatistics.exception.PlaceStatisticsException;
 import com.now.naaga.placestatistics.exception.PlaceStatisticsExceptionType;
 import com.now.naaga.placestatistics.repository.PlaceStatisticsRepository;
-import com.now.naaga.placestatistics.exception.PlaceStatisticsException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,15 +20,27 @@ public class PlaceStatisticsService {
 
     private final PlaceStatisticsRepository placeStatisticsRepository;
 
-    public PlaceStatisticsService(final PlaceStatisticsRepository placeStatisticsRepository) {
+    private final PlaceService placeService;
+
+    public PlaceStatisticsService(final PlaceStatisticsRepository placeStatisticsRepository,
+                                  final PlaceService placeService) {
         this.placeStatisticsRepository = placeStatisticsRepository;
+        this.placeService = placeService;
+    }
+
+    public PlaceStatistics createPlaceStatistics(final CreatePlaceStatisticsCommand createPlaceStatisticsCommand) {
+        final Long placeId = createPlaceStatisticsCommand.placeId();
+        final Place place = placeService.findPlaceById(new FindPlaceByIdCommand(placeId));
+
+        final PlaceStatistics placeStatistics = new PlaceStatistics(place, LIKE_COUNT_DEFAULT_VALUE);
+        return placeStatisticsRepository.save(placeStatistics);
     }
 
     public void subtractLike(final SubtractLikeCommand subtractLikeCommand) {
         final Long placeId = subtractLikeCommand.placeId();
 
         final PlaceStatistics placeStatistics = placeStatisticsRepository.findByPlaceId(placeId)
-                .orElseThrow(() -> new PlaceStatisticsException(PlaceStatisticsExceptionType.NOT_FOUND));
+                                                                         .orElseThrow(() -> new PlaceStatisticsException(PlaceStatisticsExceptionType.NOT_FOUND));
 
         placeStatistics.subtractLike();
     }
--- backend/src/test/java/com/now/naaga/placestatistics/application/PlaceStatisticsServiceTest.java
+++ backend/src/test/java/com/now/naaga/placestatistics/application/PlaceStatisticsServiceTest.java
@@ -1,39 +1,58 @@
 package com.now.naaga.placestatistics.application;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.now.naaga.common.builder.PlaceBuilder;
 import com.now.naaga.common.builder.PlaceStatisticsBuilder;
 import com.now.naaga.place.domain.Place;
+import com.now.naaga.placestatistics.application.dto.CreatePlaceStatisticsCommand;
 import com.now.naaga.placestatistics.application.dto.SubtractLikeCommand;
 import com.now.naaga.placestatistics.domain.PlaceStatistics;
 import com.now.naaga.placestatistics.repository.PlaceStatisticsRepository;
 import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(ReplaceUnderscores.class)
 @Sql("/truncate.sql")
 @ActiveProfiles("test")
 @SpringBootTest
 class PlaceStatisticsServiceTest {
 
-    PlaceStatisticsService placeStatisticsService;
-
-    PlaceStatisticsBuilder placeStatisticsBuilder;
-
-    PlaceStatisticsRepository placeStatisticsRepository;
+    @Autowired
+    private PlaceStatisticsRepository placeStatisticsRepository;
 
     @Autowired
-    public PlaceStatisticsServiceTest(final PlaceStatisticsService placeStatisticsService,
-                                      final PlaceStatisticsBuilder placeStatisticsBuilder,
-                                      final PlaceStatisticsRepository placeStatisticsRepository) {
-        this.placeStatisticsService = placeStatisticsService;
-        this.placeStatisticsBuilder = placeStatisticsBuilder;
-        this.placeStatisticsRepository = placeStatisticsRepository;
+    private PlaceStatisticsService placeStatisticsService;
+
+    @Autowired
+    private PlaceStatisticsBuilder placeStatisticsBuilder;
+
+    @Autowired
+    private PlaceBuilder placeBuilder;
+
+    @Transactional
+    @Test
+    void 장소통계를_생성한다() {
+        // given
+        final Place place = placeBuilder.init()
+                                        .build();
+
+        // when
+        final CreatePlaceStatisticsCommand command = new CreatePlaceStatisticsCommand(place.getId());
+        final PlaceStatistics actual = placeStatisticsService.createPlaceStatistics(command);
+
+        // then
+        final PlaceStatistics expected = new PlaceStatistics(place, PlaceStatistics.LIKE_COUNT_DEFAULT_VALUE);
+        assertThat(actual).usingRecursiveComparison()
+                          .ignoringExpectedNullFields()
+                          .isEqualTo(expected);
     }
 
     @Test
@@ -41,8 +60,8 @@ class PlaceStatisticsServiceTest {
         //given
         final long beforeLikeCount = 10L;
         final PlaceStatistics placeStatistics = placeStatisticsBuilder.init()
-                .likeCount(beforeLikeCount)
-                .build();
+                                                                      .likeCount(beforeLikeCount)
+                                                                      .build();
         final Place place = placeStatistics.getPlace();
 
         //when
