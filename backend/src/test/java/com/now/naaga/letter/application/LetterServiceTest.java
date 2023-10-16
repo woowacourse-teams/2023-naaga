b411085cfe309589fbd8957ba8854905f888c8fc
zillionme <beatus96@naver.com>
2023-10-16 13:37:22 +0900

    test: LetterService 테스트 수정

--- backend/src/test/java/com/now/naaga/letter/application/LetterServiceTest.java
+++ backend/src/test/java/com/now/naaga/letter/application/LetterServiceTest.java
@@ -2,12 +2,14 @@ package com.now.naaga.letter.application;
 
 import com.now.naaga.common.builder.GameBuilder;
 import com.now.naaga.common.builder.PlayerBuilder;
+import com.now.naaga.common.exception.BaseExceptionType;
 import com.now.naaga.game.exception.GameException;
+import com.now.naaga.game.exception.GameExceptionType;
 import com.now.naaga.letter.application.letterlog.dto.LetterCreateCommand;
 import com.now.naaga.letter.domain.Letter;
 import com.now.naaga.place.domain.Position;
 import com.now.naaga.player.domain.Player;
-import com.now.naaga.player.exception.PlayerException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -16,12 +18,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
-import static com.now.naaga.common.fixture.MemberFixture.MEMBER;
-import static com.now.naaga.common.fixture.PlayerFixture.NICKNAME;
-import static com.now.naaga.common.fixture.PlayerFixture.SCORE;
 import static com.now.naaga.common.fixture.PositionFixture.잠실_루터회관_정문_좌표;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @ActiveProfiles("test")
@@ -43,12 +44,12 @@ class LetterServiceTest {
     void 쪽지를_정상적으로_생성하고_게임중_등록한_쪽지를_기록으로_남긴다() {
         //given
         final Player savedPlayer = playerBuilder.init()
-                .build();
+                                                .build();
         final String message = "날씨가 선선해요.";
         final Position position = 잠실_루터회관_정문_좌표;
         gameBuilder.init()
-                .player(savedPlayer)
-                .build();
+                   .player(savedPlayer)
+                   .build();
         
         //when
         final LetterCreateCommand letterCreateCommand = new LetterCreateCommand(
@@ -68,7 +69,7 @@ class LetterServiceTest {
     void 쪽지를_등록할_때_현재_진행_중인_게임이_존재하지_않으면_예외가_발생한다() {
         //given
         final Player savedPlayer = playerBuilder.init()
-                .build();
+                                                .build();
         final String message = "날씨가 선선해요.";
         final Position position = 잠실_루터회관_정문_좌표;
         final LetterCreateCommand letterCreateCommand = new LetterCreateCommand(
@@ -79,25 +80,10 @@ class LetterServiceTest {
         //when&then
         assertThatThrownBy(() -> letterService.writeLetter(letterCreateCommand))
                 .isInstanceOf(GameException.class);
-    }
-    
-    @Test
-    void 쪽지를_등록할_때_쪽지를_쓴_플레이어가_존재하지_않으면_예외가_발생한다() {
-        //given
-        final Player player = new Player(1L,
-                NICKNAME,
-                SCORE,
-                MEMBER(),
-                false);
-        final String message = "날씨가 선선해요.";
-        final Position position = 잠실_루터회관_정문_좌표;
-        final LetterCreateCommand letterCreateCommand = new LetterCreateCommand(
-                player.getId(),
-                message,
-                position);
-        
-        //when&then
-        assertThatThrownBy(() -> letterService.writeLetter(letterCreateCommand))
-                .isInstanceOf(PlayerException.class);
+        Assertions.assertAll(() -> {
+            final BaseExceptionType baseExceptionType = assertThrows(GameException.class, () -> letterService.writeLetter(letterCreateCommand))
+                    .exceptionType();
+            assertThat(baseExceptionType).isEqualTo(GameExceptionType.NOT_EXIST_IN_PROGRESS);
+        });
     }
 }
