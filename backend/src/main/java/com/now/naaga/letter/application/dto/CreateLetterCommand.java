46f26bb64629e2e0292f35de462a28764aa3d931
zillionme <beatus96@naver.com>
2023-10-16 15:24:31 +0900

    chore: letter dto 클래스명 변경

--- backend/src/main/java/com/now/naaga/letter/application/dto/CreateLetterCommand.java
+++ backend/src/main/java/com/now/naaga/letter/application/dto/CreateLetterCommand.java
@@ -1,18 +1,18 @@
-package com.now.naaga.letter.application.letterlog.dto;
+package com.now.naaga.letter.application.dto;
 
 import com.now.naaga.letter.presentation.dto.LetterRequest;
 import com.now.naaga.place.domain.Position;
 import com.now.naaga.player.presentation.dto.PlayerRequest;
 
-public record LetterCreateCommand(Long playerId,
+public record CreateLetterCommand(Long playerId,
                                   String message,
                                   Position position) {
 
-    public static LetterCreateCommand of(final PlayerRequest playerRequest,
+    public static CreateLetterCommand of(final PlayerRequest playerRequest,
                                          final LetterRequest letterRequest) {
         final Long playerId = playerRequest.playerId();
         final Position position = Position.of(letterRequest.latitude(), letterRequest.longitude());
-        return new LetterCreateCommand(playerId,
+        return new CreateLetterCommand(playerId,
                 letterRequest.message(),
                 position);
     }
