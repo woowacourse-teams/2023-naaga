96c762f86ac64fd016e5dafca8c3df84d59939a8
krrong <khk9664@naver.com>
2023-10-17 02:36:56 +0900

    feat: 뷰페이저, 탭레이아웃 연결

--- android/app/src/main/java/com/now/naaga/presentation/adventuredetail/AdventureDetailActivity.kt
+++ android/app/src/main/java/com/now/naaga/presentation/adventuredetail/AdventureDetailActivity.kt
@@ -2,6 +2,7 @@ package com.now.naaga.presentation.adventuredetail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import com.now.naaga.databinding.ActivityAdventureDetailBinding
 import com.now.naaga.presentation.adventuredetail.viewpager.ViewPagerAdapter
 import com.now.naaga.presentation.uimodel.model.OpenLetterUiModel
@@ -24,5 +25,12 @@ class AdventureDetailActivity : AppCompatActivity() {
                 List(10) { OpenLetterUiModel("krrong", "today", "meesage") },
             ),
         )
+
+        TabLayoutMediator(binding.tlAdventureDetail, binding.vpAdventureDetail) { tab, position ->
+            when (position) {
+                0 -> tab.text = "읽은 편지"
+                1 -> tab.text = "등록한 편지"
+            }
+        }.attach()
     }
 }
