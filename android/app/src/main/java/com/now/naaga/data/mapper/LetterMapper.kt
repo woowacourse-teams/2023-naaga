cb617b568c35665336fca1abc9f0ec6f4c8404ac
hyunji1203 <hj27720@gmail.com>
2023-10-16 16:39:41 +0900

    feat: openLetter Mapper 생성

--- android/app/src/main/java/com/now/naaga/data/mapper/LetterMapper.kt
+++ android/app/src/main/java/com/now/naaga/data/mapper/LetterMapper.kt
@@ -1,11 +1,23 @@
 package com.now.naaga.data.mapper
 
 import com.now.domain.model.letter.ClosedLetter
+import com.now.domain.model.letter.OpenLetter
 import com.now.naaga.data.remote.dto.ClosedLetterDto
+import com.now.naaga.data.remote.dto.OpenLetterDto
 
 fun ClosedLetterDto.toDomain(): ClosedLetter {
     return ClosedLetter(
         id = id,
         coordinate = coordinateDto.toDomain(),
+    )
+}
+
+fun OpenLetterDto.toDomain(): OpenLetter {
+    return OpenLetter(
+        id = id,
+        player = player.toDomain(),
+        coordinate = coordinateDto.toDomain(),
+        message = message,
+        registerDate = registerDate,
     )
 }
