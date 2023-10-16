c7803a76618b1d9e2e2cf7386477738d88099c8f
zillionme <beatus96@naver.com>
2023-10-16 13:10:26 +0900

    refactor: 진행중인 게임 조회 기능 게임 서비스로 이동

--- backend/src/main/java/com/now/naaga/game/repository/GameRepository.java
+++ backend/src/main/java/com/now/naaga/game/repository/GameRepository.java
@@ -2,13 +2,18 @@ package com.now.naaga.game.repository;
 
 import com.now.naaga.game.domain.Game;
 import com.now.naaga.game.domain.GameStatus;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameRepository extends JpaRepository<Game, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface GameRepository extends JpaRepository<Game, Long> {
+    
     List<Game> findByPlayerIdAndGameStatus(final Long playerId,
                                            final GameStatus gameStatus);
-
+    
     List<Game> findByPlayerId(Long playerId);
+    
+    Optional<Game> findByPlayerIdAndInProgress(final Long playerId,
+                                               final GameStatus inProgress);
 }
--- backend/src/main/java/com/now/naaga/letter/application/letterlog/WriteLetterLogService.java
+++ backend/src/main/java/com/now/naaga/letter/application/letterlog/WriteLetterLogService.java
@@ -1,10 +1,8 @@
 package com.now.naaga.letter.application.letterlog;
 
 import com.now.naaga.game.application.GameService;
-import com.now.naaga.game.application.dto.FindGameByStatusCommand;
+import com.now.naaga.game.application.dto.FindGameInProgressCommand;
 import com.now.naaga.game.domain.Game;
-import com.now.naaga.game.exception.GameException;
-import com.now.naaga.game.exception.GameExceptionType;
 import com.now.naaga.letter.application.LetterService;
 import com.now.naaga.letter.application.WriteLetterLogCreateService;
 import com.now.naaga.letter.application.letterlog.dto.WriteLetterLogCreateCommand;
@@ -13,10 +11,6 @@ import com.now.naaga.letter.domain.letterlog.WriteLetterLog;
 import com.now.naaga.letter.repository.letterlog.WriteLetterLogRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-import static com.now.naaga.game.domain.GameStatus.IN_PROGRESS;
 
 @Transactional
 @Service
@@ -45,11 +39,7 @@ public class WriteLetterLogService implements WriteLetterLogCreateService {
     }
     
     private Game getGameInProgress(final Long playerId) {
-        final FindGameByStatusCommand findGameByStatusCommand = new FindGameByStatusCommand(playerId, IN_PROGRESS);
-        final List<Game> gamesInProgress = gameService.findGamesByStatus(findGameByStatusCommand);
-        if (gamesInProgress.isEmpty()) {
-            throw new GameException(GameExceptionType.NOT_EXIST_IN_PROGRESS);
-        }
-        return gamesInProgress.get(0);
+        final FindGameInProgressCommand findGameByStatusCommand = new FindGameInProgressCommand(playerId);
+        return gameService.findGameInProgress(findGameByStatusCommand);
     }
 }
