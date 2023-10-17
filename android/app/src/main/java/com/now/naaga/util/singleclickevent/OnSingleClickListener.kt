e07512e46d0632dd84f8523d38b7d1fccd88087c
hyunji1203 <hj27720@gmail.com>
2023-10-17 15:40:54 +0900

    feat: 쪽지 더블 클릭 시 다이얼로그가 여러번 뜨지 않도록 더블 이벤트 발생 막아주는 클릭리스너 생성

---
+++ android/app/src/main/java/com/now/naaga/util/singleclickevent/OnSingleClickListener.kt
@@ -0,0 +1,25 @@
+package com.now.naaga.util.singleclickevent
+
+import com.naver.maps.map.overlay.Overlay
+
+class OnSingleClickListener(private val onSingleClick: (overlay: Overlay) -> Unit) : Overlay.OnClickListener {
+    private var lastClickTime = DEFAULT_LAST_CLICK_TIME
+
+    override fun onClick(overlay: Overlay): Boolean {
+        if (isValidate()) {
+            onSingleClick(overlay)
+        }
+        lastClickTime = System.currentTimeMillis()
+        return true
+    }
+
+
+    private fun isValidate(): Boolean {
+        return System.currentTimeMillis() - lastClickTime > CLICK_INTERVAL_TIME
+    }
+
+    companion object {
+        private const val DEFAULT_LAST_CLICK_TIME = 0L
+        private const val CLICK_INTERVAL_TIME = 1000L
+    }
+}
