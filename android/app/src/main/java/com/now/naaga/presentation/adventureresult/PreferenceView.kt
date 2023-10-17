098208a9bf4f1728bb335d850fbf60a413b9bb3b
briandr97 <briandr@naver.com>
2023-10-17 16:37:34 +0900

    feat: 좋아요 싫어요 기능을 담당할 커스텀 뷰 제작

---
+++ android/app/src/main/java/com/now/naaga/presentation/adventureresult/PreferenceView.kt
@@ -0,0 +1,84 @@
+package com.now.naaga.presentation.adventureresult
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.now.domain.model.PreferenceState
+import com.now.naaga.R
+import com.now.naaga.databinding.CustomPreferenceViewBinding
+
+class PreferenceView(context: Context, attrs: AttributeSet? = null) : ConstraintLayout(context, attrs) {
+    private val binding: CustomPreferenceViewBinding
+    private val layoutInflater = LayoutInflater.from(this.context)
+    private var preferenceStateListener: PreferenceStateListener? = null
+    var likeCount: Int = 0
+        set(value) {
+            field = value
+            binding.tvPreferenceLikeCount.text = value.toString()
+        }
+
+    init {
+        binding = CustomPreferenceViewBinding.inflate(layoutInflater, this, true)
+        setClickListeners()
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.PreferenceView,
+            0,
+            0,
+        ).apply {
+            val isLikeVisible = getBoolean(R.styleable.PreferenceView_isLikeVisible, true)
+            val isDislikeVisible = getBoolean(R.styleable.PreferenceView_isDislikeVisible, true)
+            val isLikeCountVisible = getBoolean(R.styleable.PreferenceView_isLikeCountVisible, true)
+            setComponentVisibility(isLikeVisible, isDislikeVisible, isLikeCountVisible)
+        }
+    }
+
+    private fun setClickListeners() {
+        binding.ivPreferenceLike.setOnClickListener {
+            preferenceStateListener?.onClick(PreferenceState.LIKE)
+        }
+        binding.ivPreferenceDislike.setOnClickListener {
+            preferenceStateListener?.onClick(PreferenceState.DISLIKE)
+        }
+    }
+
+    private fun setComponentVisibility(
+        isLikeVisible: Boolean,
+        isDislikeVisible: Boolean,
+        isLikeCountVisible: Boolean,
+    ) {
+        val setVisibility: (Boolean) -> Int = { isVisible: Boolean -> if (isVisible) View.VISIBLE else View.GONE }
+        binding.ivPreferenceLike.visibility = setVisibility(isLikeVisible)
+        binding.ivPreferenceDislike.visibility = setVisibility(isDislikeVisible)
+        binding.tvPreferenceLikeCount.visibility = setVisibility(isLikeCountVisible)
+    }
+
+    fun setPreferenceStateListener(listener: PreferenceStateListener) {
+        preferenceStateListener = listener
+    }
+
+    fun updatePreference(preferenceState: PreferenceState) {
+        when (preferenceState) {
+            PreferenceState.LIKE -> {
+                binding.ivPreferenceLike.isSelected = true
+                binding.ivPreferenceDislike.isSelected = false
+            }
+
+            PreferenceState.DISLIKE -> {
+                binding.ivPreferenceLike.isSelected = false
+                binding.ivPreferenceDislike.isSelected = true
+            }
+
+            PreferenceState.NONE -> {
+                binding.ivPreferenceLike.isSelected = false
+                binding.ivPreferenceDislike.isSelected = false
+            }
+        }
+    }
+
+    fun interface PreferenceStateListener {
+        fun onClick(preferenceState: PreferenceState)
+    }
+}
