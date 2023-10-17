9ae1136d33ad738034cb62fa83298ff809cfbafb
hyunji1203 <hj27720@gmail.com>
2023-10-17 11:26:29 +0900

    refator: 쪽지와의 거리 계산 로직 Coordinate로 이동

--- android/domain/src/main/java/com/now/domain/model/letter/ClosedLetter.kt
+++ android/domain/src/main/java/com/now/domain/model/letter/ClosedLetter.kt
@@ -1,10 +1,6 @@
 package com.now.domain.model.letter
 
 import com.now.domain.model.Coordinate
-import kotlin.math.acos
-import kotlin.math.cos
-import kotlin.math.roundToInt
-import kotlin.math.sin
 
 data class ClosedLetter(
     val id: Long,
@@ -12,16 +8,7 @@ data class ClosedLetter(
 ) {
     var isNearBy = false
 
-    fun isNearBy(myCoordinate: Coordinate) {
-        val rad = Math.PI / 180
-        val radLat1 = rad * myCoordinate.latitude
-        val radLat2 = rad * coordinate.latitude
-        val radDist = rad * (myCoordinate.longitude - coordinate.longitude)
-
-        var distance = sin(radLat1) * sin(radLat2)
-        distance += cos(radLat1) * cos(radLat2) * cos(radDist)
-        val ret = 6371000.0 * acos(distance)
-
-        isNearBy = ret.roundToInt() <= 50
+    fun isNearBy(other: Coordinate) {
+        coordinate.isNearBy(other)
     }
 }
