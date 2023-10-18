a94c48a1cd71894ecc3c5d41960b65cf3e35b330
chaewon121 <80631952+chaewon121@users.noreply.github.com>
2023-10-18 17:37:09 +0900

    refactor: 토큰 주기 수정 (#505)

--- backend/src/main/java/com/now/naaga/auth/infrastructure/jwt/AuthTokenGenerator.java
+++ backend/src/main/java/com/now/naaga/auth/infrastructure/jwt/AuthTokenGenerator.java
@@ -14,8 +14,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class AuthTokenGenerator {
 
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30; // 30분
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 14;  // 14일
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 365 * 100;  // 100년
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 365 * 100;  // 100년
 
     private final JwtProvider jwtProvider;
 
