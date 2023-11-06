908b79667b449c36466921df127c51b2fd7000ee
zillionme <beatus96@naver.com>
2023-11-06 20:36:51 +0900

    feat: 이미 읽은 쪽지는 로깅하지 않는 기능 추가

--- backend/src/main/java/com/now/naaga/letter/application/letterlog/ReadLetterLogService.java
+++ backend/src/main/java/com/now/naaga/letter/application/letterlog/ReadLetterLogService.java
@@ -10,37 +10,53 @@ import com.now.naaga.game.exception.GameException;
 import com.now.naaga.game.exception.GameExceptionType;
 import com.now.naaga.letter.application.letterlog.dto.LetterByGameCommand;
 import com.now.naaga.letter.application.letterlog.dto.LetterLogCreateCommand;
+import com.now.naaga.letter.domain.Letter;
 import com.now.naaga.letter.domain.letterlog.ReadLetterLog;
 import com.now.naaga.letter.repository.letterlog.ReadLetterLogRepository;
+
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Service
 public class ReadLetterLogService {
-
+    
     private final ReadLetterLogRepository readLetterLogRepository;
-
+    
     private final GameService gameService;
-
+    
     public ReadLetterLogService(final ReadLetterLogRepository readLetterLogRepository, final GameService gameService) {
         this.readLetterLogRepository = readLetterLogRepository;
         this.gameService = gameService;
     }
-
+    
     public List<ReadLetterLog> findReadLettersByGameId(final LetterByGameCommand letterByGameCommand) {
         final FindGameByIdCommand findGameByIdCommand = new FindGameByIdCommand(letterByGameCommand.gameId(), letterByGameCommand.playerId());
         final Game game = gameService.findGameById(findGameByIdCommand);
         return readLetterLogRepository.findByGameId(game.getId());
     }
-
+    
+    //TODO : 채채 letter 패키지 수정되면 수정할 것1
     public void log(final LetterLogCreateCommand letterLogCreateCommand) {
         final Game gameInProgress = getGameInProgress(letterLogCreateCommand.playerId());
-        final ReadLetterLog readLetterLog = new ReadLetterLog(gameInProgress, letterLogCreateCommand.letter());
-        readLetterLogRepository.save(readLetterLog);
+        boolean isAlreadyReadLetter = isAlreadyReadLetter(gameInProgress, letterLogCreateCommand.letter());
+        if(!isAlreadyReadLetter) {
+            final ReadLetterLog readLetterLog = new ReadLetterLog(gameInProgress, letterLogCreateCommand.letter());
+            readLetterLogRepository.save(readLetterLog);
+        }
     }
-
+    
+    //TODO : 채채 letter 패키지 수정되면 수정할 것2
+    private boolean isAlreadyReadLetter(final Game game,
+                                     final Letter letter) {
+        Optional<ReadLetterLog> readLetterInGame = readLetterLogRepository
+                .findByGameIdAndLetterId(game.getId(), letter.getId());
+        return readLetterInGame.isPresent();
+    }
+    
     private Game getGameInProgress(final Long playerId) {
         final FindGameByStatusCommand findGameByStatusCommand = new FindGameByStatusCommand(playerId, IN_PROGRESS);
         final List<Game> gamesInProgress = gameService.findGamesByStatus(findGameByStatusCommand);
