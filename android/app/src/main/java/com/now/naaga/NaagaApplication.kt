ab482ed9526e58e2b6287ac5ffeb4277a69e8f11
krrong <khk9664@naver.com>
2023-11-16 15:39:40 +0900

    refactor: 불필요한 datasource 삭제

--- android/app/src/main/java/com/now/naaga/NaagaApplication.kt
+++ android/app/src/main/java/com/now/naaga/NaagaApplication.kt
@@ -2,8 +2,6 @@ package com.now.naaga
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
-import com.now.naaga.data.local.AuthDataSource
-import com.now.naaga.data.local.DefaultAuthDataSource
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -12,18 +10,9 @@ class NaagaApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         initKakaoSdk()
-        initDataSources()
     }
 
     private fun initKakaoSdk() {
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
-    }
-
-    private fun initDataSources() {
-        authDataSource = DefaultAuthDataSource(applicationContext)
-    }
-
-    companion object DependencyContainer {
-        lateinit var authDataSource: AuthDataSource
     }
 }
