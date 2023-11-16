27c839b62dcfdd526cf3385f23df2c150f168865
zillionme <100172683+zillionme@users.noreply.github.com>
2023-11-16 15:18:58 +0900

    feat: 플레이어 정보 조회 기능 구현 (#563)
    * chore: 플레이어 랭크 관련 컨트롤러명 변경
    
    * feat: 플레이어 정보 조회 기능 추가
    
    * test: 플레이어 정보 조회 기능 테스트
    
    * refactor: PlayerController로 병합
    
    * chore: 컨벤션 수정
    
    * chore: 컨벤션 수정
    
    * chore: 컨벤션 수정
    
    * chore: 테스트 최적화 수정
    
    * chore: 컨벤션 수정

--- backend/src/main/java/com/now/naaga/player/presentation/PlayerController.java
+++ backend/src/main/java/com/now/naaga/player/presentation/PlayerController.java
@@ -1,50 +1,57 @@
 package com.now.naaga.player.presentation;
 
-import static com.now.naaga.common.exception.CommonExceptionType.INVALID_REQUEST_PARAMETERS;
-
 import com.now.naaga.auth.presentation.annotation.Auth;
 import com.now.naaga.common.exception.CommonException;
 import com.now.naaga.player.application.PlayerService;
+import com.now.naaga.player.domain.Player;
 import com.now.naaga.player.domain.Rank;
 import com.now.naaga.player.presentation.dto.PlayerRequest;
+import com.now.naaga.player.presentation.dto.PlayerResponse;
 import com.now.naaga.player.presentation.dto.RankResponse;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/ranks")
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.now.naaga.common.exception.CommonExceptionType.INVALID_REQUEST_PARAMETERS;
+
 @RestController
 public class PlayerController {
-
+    
     private final PlayerService playerService;
-
+    
     public PlayerController(final PlayerService playerService) {
         this.playerService = playerService;
     }
-
-    @GetMapping("/my")
+    
+    @GetMapping("/profiles/my")
+    public ResponseEntity<PlayerResponse> findMyInfo(@Auth final PlayerRequest playerRequest) {
+        final Player player = playerService.findPlayerById(playerRequest.playerId());
+        final PlayerResponse playerResponse = PlayerResponse.from(player);
+        return ResponseEntity.ok(playerResponse);
+    }
+    
+    @GetMapping("/ranks/my")
     public ResponseEntity<RankResponse> findMyRank(@Auth final PlayerRequest playerRequest) {
         final Rank rank = playerService.getRankAndTopPercent(playerRequest);
         final RankResponse rankResponse = RankResponse.of(rank);
         return ResponseEntity.ok(rankResponse);
     }
-
-    @GetMapping
+    
+    @GetMapping("/ranks")
     public ResponseEntity<List<RankResponse>> findAllRank(@RequestParam(name = "sort-by") final String sortBy,
                                                           @RequestParam(name = "order") final String order) {
         if (!sortBy.equalsIgnoreCase("RANK") || !order.equalsIgnoreCase("ASCENDING")) {
             throw new CommonException(INVALID_REQUEST_PARAMETERS);
         }
-
+        
         final List<Rank> ranks = playerService.getAllPlayersByRanksAscending();
         final List<RankResponse> rankResponseList = ranks.stream()
-                .map(RankResponse::of)
-                .collect(Collectors.toList());
+                                                         .map(RankResponse::of)
+                                                         .collect(Collectors.toList());
         return ResponseEntity.ok(rankResponseList);
     }
-
 }
--- backend/src/test/java/com/now/naaga/player/presentation/PlayerControllerTest.java
+++ backend/src/test/java/com/now/naaga/player/presentation/PlayerControllerTest.java
@@ -1,27 +1,60 @@
 package com.now.naaga.player.presentation;
 
-import static com.now.naaga.common.exception.CommonExceptionType.INVALID_REQUEST_PARAMETERS;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
-
 import com.now.naaga.common.ControllerTest;
 import com.now.naaga.common.exception.ExceptionResponse;
 import com.now.naaga.player.domain.Player;
+import com.now.naaga.player.presentation.dto.PlayerResponse;
 import com.now.naaga.player.presentation.dto.RankResponse;
 import com.now.naaga.score.domain.Score;
 import io.restassured.RestAssured;
 import io.restassured.common.mapper.TypeRef;
+import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import java.time.LocalDateTime;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.now.naaga.common.exception.CommonExceptionType.INVALID_REQUEST_PARAMETERS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
 @SuppressWarnings("NonAsciiCharacters")
 public class PlayerControllerTest extends ControllerTest {
-
+    
+    @Test
+    void 플레이어_정보를_정상적으로_조회한다() {
+        //given
+        final Player player = playerBuilder.init()
+                                           .build();
+        
+        //when
+        final ExtractableResponse<Response> extract = RestAssured
+                .given().log().all()
+                .header("Authorization", authorizationForBearer(player))
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/profiles/my")
+                .then().log().all()
+                .extract();
+        
+        //then
+        final int statusCode = extract.statusCode();
+        final PlayerResponse actual = extract.as(PlayerResponse.class);
+        final PlayerResponse expected = PlayerResponse.from(player);
+        
+        assertSoftly(softAssertions -> {
+                    softAssertions.assertThat(statusCode).isEqualTo(HttpStatus.OK.value());
+                    softAssertions.assertThat(actual)
+                                  .usingRecursiveComparison()
+                                  .isEqualTo(expected);
+                }
+        );
+    }
+    
     @Test
     void 멤버의_랭크를_조회한다() {
         // given
@@ -100,7 +133,6 @@ public class PlayerControllerTest extends ControllerTest {
         assertThat(thirdRank.getRank()).isEqualTo(3);
     }
 
-    // TODO: 요청 파라미터가 잘못돼었을때(o)
     @Test
     void 모든_맴버의_랭크를_조회할때_요청_파라미터가_없으면_예외를_발생시킨다() {
         // given
