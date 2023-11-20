fa7d560b7c882fe71c585882b2ee68819ee3e815
dooboocookie <bkh3030@gmail.com>
2023-11-20 20:50:26 +0900

    test: ControllerTest와 ServiceTest에 MySql Container 사용한도록 하는 스프링 컨텍스트 구현

---
+++ backend/src/test/java/com/now/naaga/common/MySqlContainerServiceTest.java
@@ -0,0 +1,8 @@
+package com.now.naaga.common;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public abstract class MySqlContainerServiceTest extends MySqlContainerTest {
+
+}
